febdf3f70e50db5a1b53737dfe1938b0716c278e
Matt Seccafien <matt.seccafien@shopify.com>
2022-07-29 18:53:46 +0200

    Add servers

--- test/basic.test.ts
+++ test/basic.test.ts
@@ -1,7 +1,7 @@
 /* eslint-disable jest/no-standalone-expect */
 import {it, expect} from './test-framework';
 
-it('renders a basic page', async ({fs, instance, ...ctx}) => {
+it('renders a basic page', async ({fs, instance}) => {
   await fs.write(
     'src/index.ts',
     `
@@ -61,9 +61,9 @@ it('renders a basic page', async ({fs, instance, ...ctx}) => {
     )
   );
 
-  const page = await instance.start();
+  const page = await instance.dev();
 
-  await page.goto('http://localhost:3000');
+  await page.goto(instance.url());
   const button = page.locator('#btn');
   await expect(button).toBeDefined();
 
@@ -71,5 +71,69 @@ it('renders a basic page', async ({fs, instance, ...ctx}) => {
 
   await button.click();
   await expect(await button.textContent()).toBe('Clicked 1 time(s)');
+});
+
+it('builds a site', async ({fs, instance}) => {
+  await fs.write(
+    'src/index.ts',
+    `
+        const root = document.createElement('div')
+        const button = document.createElement('button')
+        button.id = 'btn'
+  
+        let count = 0
+  
+        button.textContent = \`Clicked $\{count} time(s)\`
+  
+        button.onclick = () => {
+          count++
+          button.textContent = \`Clicked $\{count} time(s)\`
+        }
+  
+        root.appendChild(button)
+        document.body.appendChild(root)
+      `
+  );
+  await fs.write(
+    'index.html',
+    `
+        <!DOCTYPE html>
+        <html>
+  
+        <head>
+          <meta charset="utf-8">
+          <title>Playwright Vitest Test Page</title>
+          <script type="module" src="/src/index.ts"></script>
+        </head>
+  
+        <body>
+        </body>
+  
+        </html>
+      `
+  );
+  await fs.write(
+    'vite.config.ts',
+    `
+        import { defineConfig } from 'vite'
+        export default defineConfig({})
+      `
+  );
+
+  await fs.write(
+    'tsconfig.json',
+    JSON.stringify(
+      {
+        compilerOptions: {
+          jsx: 'react',
+        },
+      },
+      null,
+      2
+    )
+  );
+
+  await instance.build();
+  await instance.start();
 });
 /* eslint-enable jest/no-standalone-expect */
--- test/test-framework.ts
+++ test/test-framework.ts
@@ -12,15 +12,26 @@
  * 2. This structure also makes integration tests hard to follow -requiring
  *    developers to traverse all the files in the sub-folder to understand what
  *    is unique to this project. The test definition code file itself is often
- *    not enough to describe what is being tested.
+ *    not enough to describe what is being tested, and worse, we often layer a
+ *    number of tests together in one sub-folder. This makes it even harder to
+ *    know what each sub-folder is responsible for and easy for different tests
+ *    to bleed into each other.
  *
- * 3. We also run these suites in indirect and inconsistent way where we split
- *    tests into multiple files for different environments (worker, node,
- *    etc...) and pass in configuration for modes (dev, prod, etc...). This
- *    makes tests hard to follow, but the granularity at which we can run a
- *    single tests and limits share-ability between multiple tests.
+ * 3. We also run these tests in indirect and inconsistent way where we separate
+ *    out the the test definitions into a single file that is wrapped in a
+ *    function that takes in environment configuration. We then have multiple
+ *    similar files that import and call the test function, passing in a
+ *    configuration for different environments (worker, node, etc...) and pass
+ *    in configuration for modes (dev, prod, etc...). This makes tests hard to
+ *    follow, and limits the granularity at which we can run a single tests and
+ *    the share-ability between multiple tests.
  *
- * 4. We use jest to run the tests, but we are migrating to vitest.
+ * 4. We use jest to run the tests, but we are migrating to vitest. We have a
+ *    lot of plumbing that is tucked away in various setups scripts. These are
+ *    not very discoverable and very tied to the jest way of doing things.
+ *
+ * 5. We do hacky stuff to extend and support playwright for RSC and we lack
+ *    primitives for testing HMR.
  *
  * Each of the above can be improved upon in the following framework with the
  * overall goal to make integration tests easier to follow and easier to write.
@@ -28,27 +39,30 @@
  *
  * 1. Each tests should create a sandbox directory with a specific set of files
  *    that exist only for the lifecycle of the test. This creates an isolated
- *    enviroment where we can create an instance of a HydrogenApp to ensure
- *    nothing in the monorepo is relied on accidentally nor do we polute the
+ *    environment where we can create an instance of a HydrogenApp to ensure
+ *    nothing in the monorepo is relied on accidentally nor do we pollute the
  *    repository with extra files that are only needed for e2e tests.
  *
  */
 
 import {paramCase} from 'change-case';
-import {resolve, dirname, join} from 'path';
+import {resolve, dirname, join, extname} from 'path';
+import {createServer as createNodeServer} from 'http';
+import type {Server as NodeServer} from 'http';
+import {readFile, mkdirp, writeFile, pathExists} from 'fs-extra';
+import {it as vitestIt, TestContext} from 'vitest';
 import {
-  readFile,
-  mkdirp,
-  writeFile,
-  pathExists,
-  emptyDir,
-  remove,
-} from 'fs-extra';
-import {it as vitestIt, TestContext, TestFunction} from 'vitest';
-import {createServer} from 'vite';
+  build,
+  createServer as createViteServer,
+  InlineConfig,
+  UserConfig,
+} from 'vite';
 import type {ViteDevServer} from 'vite';
 import {chromium} from 'playwright';
-import type {Browser, Page} from 'playwright';
+import type {Browser} from 'playwright';
+import {format as prettierFormat} from 'prettier';
+import getPort from 'get-port';
+import sirv from 'sirv';
 
 interface Context {
   fs: SandboxFileSystem;
@@ -122,10 +136,13 @@ class SandboxFileSystem {
     }
 
     const filePath = this.resolvePath(file);
+    const formattedContent = await this.format(filePath, contents);
 
     await mkdirp(dirname(filePath));
-    await writeFile(filePath, contents, {encoding: 'utf8'});
+    await writeFile(filePath, formattedContent, {encoding: 'utf8'});
   }
+
+  async edit(file: string) {}
 
   async read(file: string) {
     const filePath = this.resolvePath(file);
@@ -138,8 +155,37 @@ class SandboxFileSystem {
   }
 
   async cleanup() {
-    await emptyDir(this.root);
-    await remove(this.root);
+    // await emptyDir(this.root);
+    // await remove(this.root);
+  }
+
+  async format(path, content) {
+    const ext = extname(path);
+    const prettierConfig = {
+      arrowParens: 'always' as const,
+      singleQuote: true,
+      bracketSpacing: false,
+      trailingComma: 'all' as const,
+      parser: 'babel',
+    };
+
+    switch (ext) {
+      case '.md':
+        prettierConfig.parser = 'markdown';
+        break;
+      case '.html':
+      case '.svg':
+        prettierConfig.parser = 'html';
+        break;
+      case '.json':
+      case '.css':
+        prettierConfig.parser = ext.slice(1);
+        break;
+    }
+
+    const formattedContent = await prettierFormat(content, prettierConfig);
+
+    return formattedContent;
   }
 }
 
@@ -150,32 +196,74 @@ async function createInstance(root: string) {
 
 class SandboxInstance {
   browser: Browser;
-  server: ViteDevServer;
+  devServer: ViteDevServer;
+  startServer: NodeServer;
 
   constructor(public readonly root: string) {}
 
-  async start() {
-    this.server = await createServer({
+  url() {
+    return `http://localhost:${this.server.config.server.port}`;
+  }
+
+  async config(): Promise<InlineConfig> {
+    return {
       root: this.root,
+      logLevel: 'silent',
       server: {
-        port: 3000,
+        watch: {
+          usePolling: true,
+          interval: 100,
+        },
+        host: true,
+        port: await getPort(),
       },
+      build: {
+        target: 'esnext',
+      },
+    };
+  }
+
+  async build() {
+    const output = await build(await this.config());
+
+    return output;
+  }
+
+  async start() {
+    const serve = sirv(resolve(this.root, 'dist'));
+    this.startServer = createNodeServer(serve);
+    const port = await getPort();
+
+    return new Promise((resolve, reject) => {
+      this.startServer.on('error', reject);
+
+      this.startServer.listen(port, () => {
+        this.startServer.removeListener('error', reject);
+        resolve(`http://localhost:${port}`);
+      });
     });
+  }
 
-    await this.server.listen();
+  async dev() {
+    this.devServer = await createViteServer(await this.config());
 
-    this.browser = await chromium.launch();
+    await this.devServer.listen();
+
+    this.browser = await chromium.launch({
+      headless: !process.env.VITE_DEBUG_SERVE,
+
+      args: process.env.CI
+        ? ['--no-sandbox', '--disable-setuid-sandbox']
+        : undefined,
+    });
     const page = await this.browser.newPage();
 
     return page;
   }
 
   async destroy() {
-    await this.browser.close();
-    await new Promise<void>((resolve, reject) => {
-      this.server.httpServer.close((error) =>
-        error ? reject(error) : resolve()
-      );
-    });
+    await this.browser?.close();
+    await this.devServer?.httpServer.close();
+    await this.startServer?.close();
   }
 }
