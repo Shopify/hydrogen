50e35361b9bc94caf1d06116181781b40d796ad7
Benjamin Sehl <ben.sehl@shopify.com>
2022-09-19 18:21:14 -0400

    Updates root file boilerplate

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,4 @@
-import type { MetaFunction } from "@remix-run/cloudflare";
+import type { LinksFunction, MetaFunction } from "@remix-run/cloudflare";
 import {
   Links,
   LiveReload,
@@ -8,9 +8,26 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 
+import styles from "./styles/app.css";
+
+export const links: LinksFunction = () => {
+  return [
+    { rel: "stylesheet", href: styles },
+    {
+      rel: "preconnect",
+      href: "https://cdn.shopify.com",
+    },
+    {
+      rel: "preconnect",
+      href: "https://shop.app",
+    },
+    { rel: "icon", type: "image/svg+xml", href: "/favicon.svg" },
+  ];
+};
+
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "New Remix App",
+  title: "Hydrogen",
   viewport: "width=device-width,initial-scale=1",
 });
 
