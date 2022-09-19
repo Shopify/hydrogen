077aeee66e69442706a6b4592e3b2d745e62a38c
Benjamin Sehl <ben.sehl@shopify.com>
2022-09-19 18:16:37 -0400

    Adds basic boilerplate and dependencies

---
+++ postcss.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    plugins: {
+      "postcss-import": {},
+      "tailwindcss/nesting": {},
+      tailwindcss: {},
+      "postcss-preset-env": {
+        features: { "nesting-rules": false },
+      },
+    },
+  };
+  
