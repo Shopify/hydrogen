f459ac73c8dc16bb67ba5b9f4039e5516576a4ea
Josh Larson <josh.larson@shopify.com>
2022-09-20 10:50:12 -0500

    Add Layout and header

---
+++ app/components/index.ts
@@ -0,0 +1,6 @@
+export { Layout } from "./Layout";
+export { Drawer, useDrawer } from "./Drawer";
+export { Heading, Text } from "./Text";
+export { Input } from "./Input";
+// Sue me
+export * from "./Icon";
---
+++ app/lib/utils.ts
@@ -0,0 +1,42 @@
+import type {
+  MenuItem,
+  Menu,
+} from "@shopify/hydrogen-ui-alpha/storefront-api-types";
+
+// @ts-expect-error types not available
+import typographicBase from "typographic-base";
+
+export interface EnhancedMenuItem extends MenuItem {
+  to: string;
+  target: string;
+  isExternal?: boolean;
+  items: EnhancedMenuItem[];
+}
+
+export interface EnhancedMenu extends Menu {
+  items: EnhancedMenuItem[];
+}
+
+export function missingClass(string?: string, prefix?: string) {
+  if (!string) {
+    return true;
+  }
+
+  const regex = new RegExp(` ?${prefix}`, "g");
+  return string.match(regex) === null;
+}
+
+export function formatText(input?: string | React.ReactNode) {
+  if (!input) {
+    return;
+  }
+
+  if (typeof input !== "string") {
+    return input;
+  }
+
+  return typographicBase(input, { locale: "en-us" }).replace(
+    /\s([^\s<]+)\s*$/g,
+    "\u00A0$1"
+  );
+}
