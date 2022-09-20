28d80b03dbc8bd9f9e4005a1494bb858d23f3d06
Josh Larson <josh.larson@shopify.com>
2022-09-20 10:03:33 -0500

    slug -> handle

--- app/routes/products/$productHandle.tsx
+++ app/routes/products/$productHandle.tsx
@@ -1,6 +1,6 @@
 import { useParams } from "@remix-run/react";
 
 export default function Product() {
-  const { productSlug } = useParams();
-  return <h1>Product: {productSlug}</h1>;
+  const { productHandle } = useParams();
+  return <h1>Product: {productHandle}</h1>;
 }
