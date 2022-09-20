c0ffe8f869a310d424d8a5baf46b76f7818c4111
Josh Larson <josh.larson@shopify.com>
2022-09-20 15:27:55 -0500

    Get PDP working! And refactor to data query objects

--- app/root.tsx
+++ app/root.tsx
@@ -13,16 +13,9 @@ import {
   useLoaderData,
 } from "@remix-run/react";
 import { Layout } from "~/components";
-import {
-  getPublicTokenHeaders,
-  getStorefrontApiUrl,
-} from "./lib/shopify-client";
-import invariant from "tiny-invariant";
+import { getLayoutData } from "~/data";
 
 import styles from "./styles/app.css";
-import { StorefrontApiResponseOk } from "@shopify/hydrogen-ui-alpha/dist/types/storefront-api-response.types";
-import { Menu, Shop } from "@shopify/hydrogen-ui-alpha/storefront-api-types";
-import { parseMenu } from "./lib/utils";
 
 export const links: LinksFunction = () => {
   return [
@@ -46,56 +39,7 @@ export const meta: MetaFunction = () => ({
 });
 
 export const loader: LoaderFunction = async function loader() {
-  const languageCode = "EN";
-
-  const HEADER_MENU_HANDLE = "main-menu";
-  const FOOTER_MENU_HANDLE = "footer";
-
-  const headers = getPublicTokenHeaders();
-  headers["content-type"] = "application/json";
-
-  const response = await fetch(getStorefrontApiUrl(), {
-    body: JSON.stringify({
-      query: LAYOUT_QUERY,
-      variables: {
-        language: languageCode,
-        headerMenuHandle: HEADER_MENU_HANDLE,
-        footerMenuHandle: FOOTER_MENU_HANDLE,
-      },
-    }),
-    headers,
-    method: "POST",
-  });
-
-  const json: StorefrontApiResponseOk<{
-    headerMenu: Menu;
-    footerMenu: Menu;
-    shop: Shop;
-  }> = await response.json();
-
-  invariant(json && json.data, "No data returned from Shopify API");
-
-  const { data } = json;
-
-  /*
-    Modify specific links/routes (optional)
-    @see: https://shopify.dev/api/storefront/unstable/enums/MenuItemType
-    e.g here we map:
-      - /blogs/news -> /news
-      - /blog/news/blog-post -> /news/blog-post
-      - /collections/all -> /products
-  */
-  const customPrefixes = { BLOG: "", CATALOG: "products" };
-
-  const headerMenu = data?.headerMenu
-    ? parseMenu(data.headerMenu, customPrefixes)
-    : undefined;
-
-  const footerMenu = data?.footerMenu
-    ? parseMenu(data.footerMenu, customPrefixes)
-    : undefined;
-
-  return { shop: data.shop, headerMenu, footerMenu };
+  return getLayoutData();
 };
 
 export default function App() {
@@ -118,41 +62,3 @@ export default function App() {
     </html>
   );
 }
-
-const LAYOUT_QUERY = `
-  query layoutMenus(
-    $language: LanguageCode
-    $headerMenuHandle: String!
-    $footerMenuHandle: String!
-  ) @inContext(language: $language) {
-    shop {
-      name
-    }
-    headerMenu: menu(handle: $headerMenuHandle) {
-      id
-      items {
-        ...MenuItem
-        items {
-          ...MenuItem
-        }
-      }
-    }
-    footerMenu: menu(handle: $footerMenuHandle) {
-      id
-      items {
-        ...MenuItem
-        items {
-          ...MenuItem
-        }
-      }
-    }
-  }
-  fragment MenuItem on MenuItem {
-    id
-    resourceId
-    tags
-    title
-    type
-    url
-  }
-`;
