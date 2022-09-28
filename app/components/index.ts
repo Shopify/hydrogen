d21fb69d63152ee709ecbb3f1c0a21157c79aed0
Josh Larson <josh.larson@shopify.com>
2022-09-28 10:47:05 -0500

    Update add to cart button to point to action

--- app/components/index.ts
+++ app/components/index.ts
@@ -3,7 +3,6 @@ export { Drawer, useDrawer } from "./Drawer";
 export { Heading, Section, Text, PageHeader } from "./Text";
 export { Input } from "./Input";
 export { ProductGallery } from "./ProductGallery";
-export { ProductOptions } from "./ProductOptions";
 export { ProductCard } from "./ProductCard";
 export { ProductSwimlane } from "./ProductSwimlane";
 export { ProductGrid } from "./ProductGrid";
