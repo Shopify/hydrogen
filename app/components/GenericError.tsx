379a0cc36220ff394502fee8a0154f5b17bae42d
Fran Dios <fran.dios@shopify.com>
2022-10-04 19:02:12 +0900

    Add root ErrorBoundary

--- app/components/GenericError.tsx
+++ app/components/GenericError.tsx
@@ -2,9 +2,19 @@ import { Button } from "./Button";
 import { FeaturedSection } from "./FeaturedSection";
 import { PageHeader, Text } from "./Text";
 
-export function GenericError() {
+export function GenericError({
+  error,
+}: {
+  error?: { message: string; stack?: string };
+}) {
   const heading = `Something’s wrong here.`;
-  const description = `We found an error while loading this page.`;
+  let description = `We found an error while loading this page.`;
+
+  // TODO hide error in prod?
+  if (error) {
+    description += `\n${error.message}`;
+    console.error(error);
+  }
 
   return (
     <>
@@ -12,6 +22,19 @@ export function GenericError() {
         <Text width="narrow" as="p">
           {description}
         </Text>
+        {error?.stack && (
+          <pre
+            style={{
+              padding: "2rem",
+              background: "hsla(10, 50%, 50%, 0.1)",
+              color: "red",
+              overflow: "auto",
+              maxWidth: "100%",
+            }}
+          >
+            {error.stack}
+          </pre>
+        )}
         <Button width="auto" variant="secondary" to={"/"}>
           Take me to the home page
         </Button>
