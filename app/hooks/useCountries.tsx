c1319694f5342f26fc97879fe323460d27216e60
Juan P. Prieto <juanpablo.prieto@shopify.com>
2022-10-11 12:36:05 -0700

    fix hooks

--- app/hooks/useCountries.tsx
+++ app/hooks/useCountries.tsx
@@ -4,13 +4,19 @@ import type {
   Country,
 } from "@shopify/hydrogen-ui-alpha/storefront-api-types";
 
-export function useCountries(): Array<Country> | undefined {
+/*
+  This is an experimental pattern that helps prevent props drilling
+*/
+export function useCountries(): Array<Country> | null {
   const rootData = useParentRouteData('/');
+
+  if (typeof rootData?.cart === 'undefined') {
+    return null
+  }
 
   if (rootData?.countries?._data) {
     return rootData?.countries?._data;
   }
 
-  throw rootData?.countries ?? new Promise(() => {})
+  throw rootData?.countries
 }
-
