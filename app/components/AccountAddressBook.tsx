87c771aefb054d73fdbb3016eee2a5bac87ef0fa
Josh Larson <josh.larson@shopify.com>
2022-10-11 11:11:44 -0500

    Build out address editing flow

--- app/components/AccountAddressBook.tsx
+++ app/components/AccountAddressBook.tsx
@@ -1,10 +1,9 @@
-import { Outlet, useOutlet } from "@remix-run/react";
+import { Form } from "@remix-run/react";
 import type {
   Customer,
   MailingAddress,
 } from "@shopify/hydrogen-ui-alpha/storefront-api-types";
-import { Button, Text, Modal } from "~/components";
-import type { EditAddressContext } from "~/routes/account/address/$addressId";
+import { Button, Link, Text } from "~/components";
 
 export function AccountAddressBook({
   customer,
@@ -13,30 +12,16 @@ export function AccountAddressBook({
   customer: Customer;
   addresses: MailingAddress[];
 }) {
-  const editingAddress = useOutlet();
-
   return (
     <>
-      {editingAddress && (
-        <Modal cancelLink=".">
-          <Outlet
-            context={
-              {
-                addresses,
-                defaultAddress: customer.defaultAddress,
-              } as EditAddressContext
-            }
-          />
-        </Modal>
-      )}
       <div className="grid w-full gap-4 p-4 py-6 md:gap-8 md:p-8 lg:p-12">
         <h3 className="font-bold text-lead">Address Book</h3>
         <div>
-          {!addresses?.length ? (
+          {!addresses?.length && (
             <Text className="mb-1" width="narrow" as="p" size="copy">
               You haven&apos;t saved any addresses yet.
             </Text>
-          ) : null}
+          )}
           <div className="w-48">
             <Button
               to="address/add"
@@ -46,7 +31,7 @@ export function AccountAddressBook({
               Add an Address
             </Button>
           </div>
-          {addresses?.length ? (
+          {Boolean(addresses?.length) && (
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
               {customer.defaultAddress && (
                 <Address address={customer.defaultAddress} defaultAddress />
@@ -57,48 +42,54 @@ export function AccountAddressBook({
                   <Address key={address.id} address={address} />
                 ))}
             </div>
-          ) : null}
+          )}
         </div>
       </div>
     </>
   );
 }
+
 function Address({
   address,
   defaultAddress,
 }: {
-  address: any;
+  address: MailingAddress;
   defaultAddress?: boolean;
 }) {
   return (
     <div className="lg:p-8 p-6 border border-gray-200 rounded flex flex-col">
-      {defaultAddress ? (
+      {defaultAddress && (
         <div className="mb-3 flex flex-row">
           <span className="px-3 py-1 text-xs font-medium rounded-full bg-primary/20 text-primary/50">
             Default
           </span>
         </div>
-      ) : null}
+      )}
       <ul className="flex-1 flex-row">
-        {address.firstName || address.lastName ? (
+        {(address.firstName || address.lastName) && (
           <li>
-            {(address.firstName && address.firstName + " ") + address.lastName}
+            {"" +
+              (address.firstName && address.firstName + " ") +
+              address?.lastName}
           </li>
-        ) : (
-          <></>
         )}
-        {address.formatted ? (
-          address.formatted.map((line: string) => <li key={line}>{line}</li>)
-        ) : (
-          <></>
-        )}
+        {address.formatted &&
+          address.formatted.map((line: string) => <li key={line}>{line}</li>)}
       </ul>
 
       <div className="flex flex-row font-medium mt-6">
-        <button className="text-left underline text-sm">Edit</button>
-        <button className="text-left text-primary/50 ml-6 text-sm">
-          Remove
-        </button>
+        <Link
+          to={`address/${encodeURIComponent(address.id)}`}
+          className="text-left underline text-sm"
+        >
+          Edit
+        </Link>
+        <Form action="address/delete" method="delete">
+          <input type="hidden" name="addressId" value={address.id} />
+          <button className="text-left text-primary/50 ml-6 text-sm">
+            Remove
+          </button>
+        </Form>
       </div>
     </div>
   );
