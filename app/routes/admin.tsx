0db11a07a0964fb31ea6e3d2111439d87d003fea
Juan P. Prieto <juanpablo.prieto@shopify.com>
2022-10-12 09:15:47 -0700

    /admin

---
+++ app/routes/admin.tsx
@@ -0,0 +1,18 @@
+import { redirect } from "@remix-run/cloudflare";
+import invariant from "tiny-invariant";
+import { getPrimaryShopDomain } from '~/data';
+
+/*
+  This route redirects you to your Shopify Admin
+  by querying for your myshopify.com domain.
+  Learn more about the redirect method here:
+  https://developer.mozilla.org/en-US/docs/Web/API/Response/redirect
+*/
+export async function loader() {
+  const shop = await getPrimaryShopDomain();
+  console.log({shop})
+  invariant(shop.primaryDomain, "Missing primary domain url");
+
+  const adminUrl = `${shop.primaryDomain.url}/admin`
+  return redirect(adminUrl)
+}
