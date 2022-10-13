ca39c28a391ef3f3e747f7b4f5b46be94564105b
Juan P. Prieto <juanpablo.prieto@shopify.com>
2022-10-13 14:04:51 -0700

    add infinite loading to ProductGrid

--- app/components/ProductGrid.tsx
+++ app/components/ProductGrid.tsx
@@ -1,18 +1,16 @@
-import { Button, Grid, ProductCard, LinkI18n } from "~/components";
-import { getImageLoadingPriority } from "~/lib/const";
-import type { Collection } from "@shopify/hydrogen-ui-alpha/storefront-api-types";
-import { useSearchParams, useTransition } from "@remix-run/react";
+import { LinkI18n } from "~/components";
+import type { ProductConnection } from "@shopify/hydrogen-ui-alpha/storefront-api-types";
+import { ProductGridPaginated } from './ProductGridPaginated';
+import { ProductGridInfinite } from "./ProductGridInfinite";
 
 export function ProductGrid({
-  collection,
+  paginated = false,
+  products,
 }: {
-  url: string;
-  collection: Collection;
+  paginated?: boolean
+  products: ProductConnection;
 }) {
-  const transition = useTransition();
-  const [searchParams] = useSearchParams();
-
-  if (!collection?.products?.nodes) {
+  if (!products?.nodes) {
     return (
       <>
         <p>No products found on this collection</p>
@@ -23,62 +21,17 @@ export function ProductGrid({
     );
   }
 
-  /* TODO:
-    - Load More -> Passes pages in location state via Link component
-    - Infinite Scroll -> Opt-in to trigger load more when in view
-    - Optional consideration: Virtualization if very long list (need to consider impact of scroll position, etc.)
-
-    - product grid prepends location state products if found
-    -- if found, never render a previous page link (because you'll already have it since page 1)
-*/
-
-  const { hasNextPage, hasPreviousPage, startCursor, endCursor } =
-    collection?.products?.pageInfo;
-
-  const products = collection?.products?.nodes;
+    /* TODO:
+      - Optional consideration: Virtualization if very long list (need to consider impact of scroll position, etc.)
+    */
 
   return (
-    <>
-      {hasPreviousPage && (
-        <div className="flex items-center justify-center mt-6">
-          <Button
-            to={`.?cursor=${startCursor}&direction=previous`}
-            disabled={transition.state !== "idle"}
-            variant="secondary"
-            width="full"
-            prefetch="intent"
-          >
-            {transition.state !== "idle"
-              ? "Loading..."
-              : "Load previous products"}
-          </Button>
-        </div>
-      )}
-
-      <Grid layout="products">
-        {products.map((product, i) => (
-          <ProductCard
-            key={product.id}
-            product={product}
-            loading={getImageLoadingPriority(i)}
-          />
-        ))}
-      </Grid>
-
-      {hasNextPage && (
-        <div className="flex items-center justify-center mt-6">
-          <Button
-            to={`.?cursor=${endCursor}&direction=next`}
-            disabled={transition.state !== "idle"}
-            variant="secondary"
-            width="full"
-            preventScrollReset={true} // not working 🧐
-            prefetch="intent"
-          >
-            {transition.state !== "idle" ? "Loading..." : "Load more products"}
-          </Button>
-        </div>
-      )}
-    </>
+    paginated
+      ? <ProductGridPaginated
+          products={products}
+        />
+      : <ProductGridInfinite
+          products={products}
+        />
   );
 }
