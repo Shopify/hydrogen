b01ee839f921b2fa0b0d1c2b8368f70881e50f7f
Juan P. Prieto <juanpablo.prieto@shopify.com>
2022-10-13 20:58:24 -0700

    add collections grid support on MoreGridItems

--- app/components/MoreGridItems.tsx
+++ app/components/MoreGridItems.tsx
@@ -5,9 +5,8 @@ import { useFetcher } from "@remix-run/react";
 interface MoreGridItems {
   className?: string
   cursor: string,
-  pageBy: number,
-  placeholderItem: React.ReactElement<{ key: number; }, string | React.JSXElementConstructor<any>>
-  resource: "collection" | "products" | "blogs"
+  pageBy?: number,
+  placeholderItem?: React.ReactElement<{ key: number; }, string | React.JSXElementConstructor<any>>
   setCursor: (value: string | null) => void,
   setHasNextPage: (value: boolean) => void,
   setItems: (value: any) => any,
@@ -19,11 +18,10 @@ interface MoreGridItems {
   to enable infinite loading
 */
 export function MoreGridItems({
-  className = "grid grid-cols-4 gap-4 mt-6",
+  className = "grid-flow-row grid gap-2 gap-y-6 md:gap-4 lg:gap-6 grid-cols-2 md:grid-cols-2 lg:grid-cols-4",
   cursor,
   pageBy = 4,
-  placeholderItem = <div className="h-[500px] bg-gray-100"></div>,
-  resource = "products",
+  placeholderItem = <div className="bg-gray-400 h-[244px] sm:h-[280px] md:h-[500px] lg:h-[300px] xl:h-[348px] 2xl:h-[500px]"></div>,
   setCursor,
   setHasNextPage,
   setItems,
@@ -53,28 +51,17 @@ export function MoreGridItems({
     if (state !== "idle") return;
     if (!data) return;
 
-    let hasNextPage: boolean = false,
-        endCursor: string | null = null,
-        pageItems: [] = [];
+    const result = data?.collection?.products // /collection/$handle
+      || data?.products // /products
+      || data?.collections; // /collections
 
-    // handle all types of paginated queries
-    switch (resource) {
-      case 'products': {
-        const result = data?.collection?.products;
-        hasNextPage = result.pageInfo?.hasNextPage || false;
-        endCursor = result?.pageInfo?.endCursor || null;
-        pageItems = result?.nodes || null
-        break;
-      }
-
-      // TODO: collections
-      // case 'collections': {}
-
-      // TODO: blog
-      // case 'blog': {}
-
-      default:
+    if (!result) {
+      return console.warn('Paginated query data not supported', {data})
     }
+
+    const hasNextPage = result?.pageInfo?.hasNextPage || false;
+    const endCursor = result?.pageInfo?.endCursor || null;
+    const pageItems = result?.nodes || null
 
     if (!hasNextPage) {
       setCursor(null);
