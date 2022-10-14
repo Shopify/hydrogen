972409be2323843a2a4fb9a9091efd65cfd294e5
Juan P. Prieto <juanpablo.prieto@shopify.com>
2022-10-13 21:01:00 -0700

    cleanup collections and products routes

--- app/routes/products/index.tsx
+++ app/routes/products/index.tsx
@@ -1,35 +1,36 @@
-import { json, type LoaderArgs,type  MetaFunction } from "@remix-run/cloudflare";
-import { useLoaderData } from "@remix-run/react";
-import type { ProductConnection } from "@shopify/hydrogen-ui-alpha/storefront-api-types";
-import { PageHeader, Section, ProductGrid } from "~/components";
-import { getAllProducts } from "~/data";
+import {json, type LoaderArgs, type MetaFunction} from '@remix-run/cloudflare';
+import {useLoaderData} from '@remix-run/react';
+import type {ProductConnection} from '@shopify/hydrogen-ui-alpha/storefront-api-types';
+import {PageHeader, Section, ProductGrid} from '~/components';
+import {getAllProducts} from '~/data';
 
-export async function loader({ request, params }: LoaderArgs) {
-  const cursor = new URL(request.url).searchParams.get("cursor") ?? undefined;
+export async function loader({request, params}: LoaderArgs) {
+  const searchParams = new URL(request.url).searchParams;
+
+  const cursor = searchParams.get('cursor') ?? undefined;
+  const direction =
+    searchParams.get('direction') === 'previous' ? 'previous' : 'next';
+
   return json({
-    products: await getAllProducts({ cursor, params })
-  })
+    products: await getAllProducts({cursor, pageBy: 4, direction, params}),
+  });
 }
 
 export const meta: MetaFunction = () => {
   return {
-    title: "All Products",
-    description: "All Products",
+    title: 'All Products',
+    description: 'All Products',
   };
 };
 
 export default function AllProducts() {
   const {products} = useLoaderData<typeof loader>();
 
-
   return (
     <>
       <PageHeader heading="All Products" variant="allCollections" />
       <Section>
-        <ProductGrid
-          key="products"
-          products={products as ProductConnection}
-        />
+        <ProductGrid key="products" products={products as ProductConnection} />
       </Section>
     </>
   );
