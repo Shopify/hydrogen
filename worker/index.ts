f7b3af831ddc2fbe5a3fd3889217cf5a4c84e889
Fran Dios <fran.dios@shopify.com>
2022-10-17 16:28:41 +0900

    Replace Wrangler with mini-oxygen

--- worker/index.ts
+++ worker/index.ts
@@ -2,14 +2,9 @@ import { createRequestHandler } from "@remix-run/oxygen";
 // The build remix app provided by remix build
 import * as remixBuild from "remix-build";
 
-// Virtual module provided by wrangler -- TODO this shouldn't be needed here
-import manifestJSON from "__STATIC_CONTENT_MANIFEST";
-const assetManifest = JSON.parse(manifestJSON);
-
 const requestHandler = createRequestHandler({
   build: remixBuild,
   mode: process.env.NODE_ENV,
-  assetManifest,
 });
 
 export default {
