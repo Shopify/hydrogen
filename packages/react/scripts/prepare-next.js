41caf556819569ed254a208fe9b8bf676b68c92b
Anthony Frehner <frehner@users.noreply.github.com>
2022-10-17 15:32:47 -0600

    Try manually doing the publish now (#10)

---
+++ packages/react/scripts/prepare-next.js
@@ -0,0 +1,15 @@
+import {execSync} from 'child_process';
+import fs from 'fs';
+
+const shortHash = execSync('git rev-parse --short HEAD').toString().trim();
+const version = `0.0.0-next-${shortHash}`;
+const packageJson = JSON.parse(fs.readFileSync('../package.json'));
+packageJson.version = version;
+fs.writeFileSync('../package.json', JSON.stringify(packageJson, null, 2));
+
+try {
+  execSync('yarn publish --tag next');
+} catch (e) {
+  console.log(e);
+  console.log('Publish failed');
+}
