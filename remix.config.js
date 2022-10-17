92786851d2a42d13b9ac5bc69a23398d3ca2285f
Maxim Shirshin <maxim.shirshin@shopify.com>
2022-10-17 20:13:47 +0200

    Do not use await, omit passThroughOnException

--- remix.config.js
+++ remix.config.js
@@ -1,5 +1,7 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
+  // Should be replaced with process.env.HYDROGEN_ASSET_BASE_URL for the production build
+  publicPath: "/build/",
   serverModuleFormat: "esm",
   serverBuildPath: "build/index.mjs",
   devServerBroadcastDelay: 1000,
