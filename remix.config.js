3f67cbc1ebb06f17315cf849b93a5e5e589b85a7
Fran Dios <fran.dios@shopify.com>
2022-10-19 16:26:53 +0900

    Move all assets to build directory

--- remix.config.js
+++ remix.config.js
@@ -3,7 +3,8 @@ module.exports = {
   // eslint-disable-next-line no-undef
   publicPath: (process.env.HYDROGEN_ASSET_BASE_URL || "") + "/build/",
   serverModuleFormat: "esm",
-  serverBuildPath: "build/index.mjs",
+  serverBuildPath: "build/server/index.mjs",
+  assetsBuildDirectory: "build/client/build",
   devServerBroadcastDelay: 1000,
   devServerPort: 8002,
   ignoredRouteFiles: ["**/.*"],
