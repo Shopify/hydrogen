1bd59c69e8d7932eff2d490a3e706cf7d8cf1c87
Juan P. Prieto <juanpablo.prieto@shopify.com>
2022-10-24 10:00:50 -0700

    lint

--- app/hooks/useCountries.tsx
+++ app/hooks/useCountries.tsx
@@ -1,13 +1,11 @@
-import { useMatches } from '@remix-run/react';
-import { useDeferred } from './useDeferred';
-import type {
-  Country,
-} from "@shopify/hydrogen-ui-alpha/storefront-api-types";
+import {useMatches} from '@remix-run/react';
+import {useDeferred} from './useDeferred';
+import type {Country} from '@shopify/hydrogen-ui-alpha/storefront-api-types';
 
 /*
   This is an experimental pattern that helps prevent props drilling
 */
 export function useCountries(): Array<Country> | null {
   const [root] = useMatches();
-  return useDeferred('countries', root)
+  return useDeferred('countries', root);
 }
