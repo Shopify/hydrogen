164662e567cbc42e561acec1e1dac8e95c799ce8
Fran Dios <fran.dios@shopify.com>
2022-10-26 11:41:03 +0900

    Merge context variables

--- packages/@hydrogen/remix/server.ts
+++ packages/@hydrogen/remix/server.ts
@@ -14,6 +14,7 @@ export function createRequestHandler(
     request: Request,
     {
       storefront,
+      context,
       ...options
     }: Omit<Parameters<typeof handleRequest>[1], 'loadContext'> &
       HydrogenHandlerParams,
@@ -21,9 +22,10 @@ export function createRequestHandler(
     try {
       return handleRequest(request, {
         ...options,
-        loadContext: {storefront: createStorefrontClient(storefront)},
+        context: {...context, storefront: createStorefrontClient(storefront)},
       });
     } catch (e) {
+      // eslint-disable-next-line no-console
       console.error(e);
 
       return new Response('Internal Error', {status: 500});
--- worker/index.ts
+++ worker/index.ts
@@ -12,12 +12,12 @@ export default {
   async fetch(
     request: Request,
     env: Env,
-    ctx: ExecutionContext,
+    context: ExecutionContext,
   ): Promise<Response> {
     try {
       return await requestHandler(request, {
         env,
-        ctx,
+        context,
         storefront: {
           publicStorefrontToken: '3b580e70970c4528da70c98e097c2fa0',
           storeDomain: 'hydrogen-preview',
@@ -25,6 +25,7 @@ export default {
         },
       });
     } catch (error) {
+      // eslint-disable-next-line no-console
       console.error(error);
       return new Response('An unexpected error occurred', {status: 500});
     }
