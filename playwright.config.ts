08ab74c258f88ee7a37c3cff0964df2a074f2456
Fran Dios <fran.dios@shopify.com>
2022-10-31 19:17:53 +0900

    Change webServer command in playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -99,7 +99,7 @@ const config: PlaywrightTestConfig = {
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npm run start',
+    command: 'npm run preview',
     port: 3000,
   },
 };
