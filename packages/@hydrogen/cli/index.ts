cb4618fa5a05bd35158fc548ec1029030927bb64
Fran Dios <fran.dios@shopify.com>
2022-10-31 19:05:41 +0900

    Add temporary CLI with build command

---
+++ packages/@hydrogen/cli/index.ts
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+
+export {};
+
+const [, , ...args] = process.argv;
+
+const options = {} as Record<string, any>;
+
+for (let i = 0; i < args.length; i++) {
+  const arg = args[i];
+  const nextArg = args[i + 1];
+  if (arg.startsWith('--')) {
+    options[
+      arg.replace('--', '').replace(/-([a-z])/g, (_, m1) => m1.toUpperCase())
+    ] = !nextArg || nextArg.startsWith('--') ? true : nextArg;
+  }
+}
+
+const [command] = args;
+
+if (command === 'build') {
+  if (!options.entry) {
+    throw new Error('The flag --entry is required');
+  }
+
+  (async () => {
+    // @ts-ignore
+    const {runBuild} = await import('./commands/build');
+
+    await runBuild(options as any);
+
+    process.exit();
+  })();
+} else {
+  // eslint-disable-next-line no-console
+  console.log(`Command "${command}" not supported`);
+}
