e1de9938e29574ad8afaa10413151c8fbeb42ca0
Vincent Zwanenburg <vincent.zwanenburg@shopify.com>
2022-11-07 10:20:48 +0000

    pre-push hook script with warning

---
+++ packages/mini-oxygen/script/pre-push.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+protected_branch='main'
+current_branch=$(git rev-parse --abbrev-ref HEAD)
+
+if [ $protected_branch = current_branch ]; then
+  printf "WARNING! You're about to push to the ${protected_branch} branch. Continue?\n"
+  select yn in "Yes" "No"; do
+    case $yn in
+      Yes ) exit 0;;
+      No ) exit 1;;
+    esac
+  done
+fi
