f9d9b145e1e5f4fe2319d95cd798a2b7aee719cd
Vincent Zwanenburg <vincent.zwanenburg@shopify.com>
2022-11-08 08:31:50 +0000

    fix variable + test

--- packages/mini-oxygen/script/pre-push.sh
+++ packages/mini-oxygen/script/pre-push.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-protected_branch='main'
+protected_branch='pre-push-to-main-warning'
 current_branch=$(git rev-parse --abbrev-ref HEAD)
 
-if [ $protected_branch = current_branch ]; then
+if [ $protected_branch = $current_branch ]; then
   printf "WARNING! You're about to push to the ${protected_branch} branch. Continue?\n"
   select yn in "Yes" "No"; do
     case $yn in
