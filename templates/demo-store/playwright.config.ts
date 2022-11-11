1d5aaf4afde8c9a85b1383aa0092d04c69dabe84
Fran Dios <fran.dios@shopify.com>
2022-11-11 19:19:03 +0900

    Fix typecheck

--- templates/demo-store/playwright.config.ts
+++ templates/demo-store/playwright.config.ts
@@ -1,6 +1,8 @@
 import type {PlaywrightTestConfig} from '@playwright/test';
 import {devices} from '@playwright/test';
 
+declare const process: {env: {CI: boolean}};
+
 /**
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
