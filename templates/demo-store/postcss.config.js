8401b96bc9e059a6e4ba944a43293e99de6d0084
Fran Dios <fran.dios@shopify.com>
2022-11-15 00:45:51 +0900

    Compile CSS modules using PostCSS

--- templates/demo-store/postcss.config.js
+++ templates/demo-store/postcss.config.js
@@ -1,3 +1,6 @@
+const fs = require('fs-extra');
+const path = require('path');
+
 module.exports = {
   plugins: {
     'postcss-import': {},
@@ -5,6 +8,27 @@ module.exports = {
     tailwindcss: {},
     'postcss-preset-env': {
       features: {'nesting-rules': false},
+    },
+    'postcss-modules': {
+      async getJSON(cssFilepath, json, outputFilepath) {
+        if (!cssFilepath.includes('.module.')) return;
+
+        const jsFilepath = path.resolve(
+          './app/styles/' + path.basename(cssFilepath) + '.js',
+        );
+
+        let code = `export default '${outputFilepath.split('public')[1]}';\n\n`;
+
+        for (const [key, value] of Object.entries(json)) {
+          code += `export const ${key} = '${value}';\n`;
+        }
+
+        await fs
+          .mkdir(path.dirname(jsFilepath), {recursive: true})
+          .catch(() => null);
+
+        await fs.writeFile(jsFilepath, code);
+      },
     },
   },
 };
