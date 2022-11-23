801955b36c8a4322b03359c9b67997752bfed6f7
Matt Seccafien <mseccafien@gmail.com>
2022-11-23 15:30:26 +0100

    Sort UI (#233)
    * Add basic sort ui
    
    * Update Icon.tsx
    
    * Merge in applied filters

--- templates/demo-store/app/components/Icon.tsx
+++ templates/demo-store/app/components/Icon.tsx
@@ -237,3 +237,17 @@ export function IconRemove(props: IconProps) {
     </Icon>
   );
 }
+
+export function IconFilters(props: IconProps) {
+  return (
+    <Icon {...props} fill="transparent" stroke={props.stroke || 'currentColor'}>
+      <title>Filters</title>
+      <circle cx="4.5" cy="6.5" r="2" />
+      <line x1="6" y1="6.5" x2="14" y2="6.5" />
+      <line x1="4.37114e-08" y1="6.5" x2="3" y2="6.5" />
+      <line x1="4.37114e-08" y1="13.5" x2="8" y2="13.5" />
+      <line x1="11" y1="13.5" x2="14" y2="13.5" />
+      <circle cx="9.5" cy="13.5" r="2" />
+    </Icon>
+  );
+}
