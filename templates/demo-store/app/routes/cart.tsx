121866dbb822e551b78aee1ad977785bf1c57172
Juan P. Prieto <juanpablo.prieto@shopify.com>
2022-11-24 10:10:09 -0800

    add BuyerIdentityUpdate

--- templates/demo-store/app/routes/cart.tsx
+++ templates/demo-store/app/routes/cart.tsx
@@ -1,38 +1,6 @@
 import {CartLoading, Cart} from '~/components';
 import {Await, useMatches} from '@remix-run/react';
 import {Suspense} from 'react';
-import {type ActionFunction, json} from '@shopify/hydrogen-remix';
-import invariant from 'tiny-invariant';
-import {updateLineItem} from '~/data';
-
-export const action: ActionFunction = async ({request, context}) => {
-  let cart;
-
-  const formData = new URLSearchParams(await request.text());
-  const intent = formData.get('intent');
-  invariant(intent, 'Missing cart intent');
-
-  // 1. Grab the cart ID from the session
-  const cartId = await context.session.get('cartId');
-
-  switch (intent) {
-    case 'set-quantity': {
-      const lineId = formData.get('lineId');
-      invariant(lineId, 'Missing lineId');
-      invariant(cartId, 'Missing cartId');
-      const quantity = Number(formData.get('quantity'));
-      cart = await updateLineItem(context, {
-        cartId,
-        lineItem: {id: lineId, quantity},
-      });
-      return json({cart});
-    }
-
-    default: {
-      throw new Error(`Cart intent ${intent} not supported`);
-    }
-  }
-};
 
 export default function CartRoute() {
   const [root] = useMatches();
