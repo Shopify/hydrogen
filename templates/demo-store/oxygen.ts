e0e68b4d1029c8fee23fa22de8fbd965e9f1fb64
Fran Dios <fran.dios@shopify.com>
2022-11-25 14:16:27 +0900

    Extract credentials as environment variables

--- templates/demo-store/oxygen.ts
+++ templates/demo-store/oxygen.ts
@@ -33,8 +33,8 @@ export default {
           env,
           context,
           storefront: {
-            publicStorefrontToken: '3b580e70970c4528da70c98e097c2fa0',
-            storeDomain: 'hydrogen-preview',
+            publicStorefrontToken: env.PUBLIC_STOREFRONT_TOKEN,
+            storeDomain: env.STOREFRONT_DOMAIN,
             storefrontApiVersion: '2022-10',
             i18n: getLocaleFromRequest(request),
           },
