9a0ebd95ae742fdd8ade5ed2d07e38ab73ef15c5
Helen Lin <helen.lin@shopify.com>
2022-11-28 10:51:57 -0800

    use remix-run dev to create templates

--- scripts/compile-template-for-dist.mjs
+++ scripts/compile-template-for-dist.mjs
@@ -1,33 +1,22 @@
-import {compileTemplate} from './compile-template.mjs';
 import {resolve} from 'path';
 import fs from 'fs-extra';
+import {createApp} from "@remix-run/dev";
 
 (async () => {
   const [template] = process.argv.slice(2);
-  const destination = resolve(process.cwd(), 'templates', `${template}-js`);
   const source = resolve(process.cwd(), 'templates');
+  const templateDir = `${source}/${template}`;
 
-  await compileTemplate(template, {source, destination});
-  copy(`${source}/${template}`, `${source}/${template}-ts`);
+  await createNewApp(templateDir, `${templateDir}-ts`, true);
+  await createNewApp(templateDir, `${templateDir}-js`, false);
   fs.removeSync(`${source}/${template}`);
 })();
 
-function copyDir(srcDir, destDir) {
-  fs.mkdirSync(destDir, {recursive: true});
-  for (const file of fs.readdirSync(srcDir)) {
-    const newFile = file.startsWith('_') ? `.${file.substring(1)}` : file;
-    const srcFile = resolve(srcDir, file);
-    const destFile = resolve(destDir, newFile);
-    copy(srcFile, destFile);
-  }
-}
-
-function copy(src, dest) {
-  const stat = fs.statSync(src);
-
-  if (stat.isDirectory()) {
-    copyDir(src, dest);
-  } else {
-    fs.copyFileSync(src, dest);
-  }
+async function createNewApp(srcDir, destDir, useTypeScript) {
+  await createApp({
+    appTemplate: srcDir,
+    installDeps: false,
+    useTypeScript,
+    projectDir: destDir,
+  });
 }
--- templates/demo-store/app/routes/products/$productHandle.tsx
+++ templates/demo-store/app/routes/products/$productHandle.tsx
@@ -25,10 +25,10 @@ import {
 import {getExcerpt} from '~/lib/utils';
 import invariant from 'tiny-invariant';
 import clsx from 'clsx';
-import {
+import type {
   ProductVariant,
   SelectedOptionInput,
-  Product,
+  Product as ProductType,
   Shop,
   ProductConnection,
 } from '@shopify/hydrogen-react/storefront-api-types';
@@ -55,7 +55,7 @@ export async function loader({
   });
 
   const {shop, product} = await storefront.query<{
-    product: Product & {selectedVariant?: ProductVariant};
+    product: ProductType & {selectedVariant?: ProductVariant};
     shop: Shop;
   }>(PRODUCT_QUERY, {
     variables: {
@@ -215,7 +215,7 @@ function ProductOptions({
   options,
   searchParamsWithDefaults,
 }: {
-  options: Product['options'];
+  options: ProductType['options'];
   searchParamsWithDefaults: URLSearchParams;
 }) {
   const closeRef = useRef<HTMLButtonElement>(null);
@@ -558,7 +558,7 @@ async function getRecommendedProducts(
   productId: string,
 ) {
   const products = await storefront.query<{
-    recommended: Product[];
+    recommended: ProductType[];
     additional: ProductConnection;
   }>(RECOMMENDED_PRODUCTS_QUERY, {
     variables: {productId, count: 12},
@@ -574,7 +574,7 @@ async function getRecommendedProducts(
     );
 
   const originalProduct = mergedProducts
-    .map((item: Product) => item.id)
+    .map((item: ProductType) => item.id)
     .indexOf(productId);
 
   mergedProducts.splice(originalProduct, 1);
