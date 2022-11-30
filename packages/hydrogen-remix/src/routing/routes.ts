3e80bb6f102b076cb7b6444fa008311a8764aff6
Bret Little <bret.little@shopify.com>
2022-11-30 13:05:47 -0500

    Remix route conventions (#262)
    * Make routes within `.hydrogen` follow remix route conventions
    
    * Make .hydrogen/routes support remix file routing conventions

--- packages/hydrogen-remix/src/routing/routes.ts
+++ packages/hydrogen-remix/src/routing/routes.ts
@@ -21,38 +21,30 @@ export async function hydrogenRoutes(
   const hydrogenRouteFiles = await readDir(hydrogenRoutesPath);
   return defineRoutes((route: any) => {
     for (const hydrogenRoute of hydrogenRouteFiles) {
-      const hydrogenRoutePath = path.relative(process.cwd(), hydrogenRoute);
+      const routeFilePath = path.relative(process.cwd(), hydrogenRoute);
 
-      const hydrogenRouteUrl = hydrogenRoutePath.substring(
-        hydrogenRoutePath.lastIndexOf('/'),
-        hydrogenRoutePath.lastIndexOf('.'),
+      const routeUrlPath = createRoutePath(
+        path.relative(hydrogenRoutesPath, routeFilePath),
       );
 
-      route(hydrogenRouteUrl, '../' + hydrogenRoutePath);
+      route(routeUrlPath, '../' + routeFilePath);
     }
   });
 }
 
 async function copyTemplates() {
-  const templates = await readDir(path.resolve(__dirname, '../../templates'));
-
+  const templateDirectory = path.resolve(__dirname, '../../templates');
+  const templates = await readDir(templateDirectory);
   const hydrogenDirectory = path.resolve(process.cwd(), '.hydrogen');
-  const hydrogenRoutesPath = path.resolve(process.cwd(), '.hydrogen/routes');
-
-  if (!fs.existsSync(hydrogenDirectory)) {
-    fs.mkdirSync(hydrogenDirectory);
-  }
-
-  if (!fs.existsSync(hydrogenRoutesPath)) {
-    fs.mkdirSync(hydrogenRoutesPath);
-  }
 
   for (const template of templates) {
     const destination = path.resolve(
       hydrogenDirectory,
-      path.basename(path.dirname(template)),
+      path.relative(templateDirectory, path.dirname(template)),
       path.basename(template),
     );
+
+    fs.mkdirSync(path.dirname(destination), {recursive: true});
     fs.copyFileSync(template, destination);
   }
 }
@@ -98,4 +90,91 @@ async function buildLangRoutes() {
       )};\n`,
     );
   }
+}
+
+/**
+ * Generate a URL path based on remix file conventions. This is copied from:
+ * https://github.com/remix-run/remix/blob/09296128ca2f2d5e8932c631e777fbe8baaa192d/packages/remix-dev/config/routesConvention.ts#L116
+ */
+function createRoutePath(partialRouteId: string): string | undefined {
+  partialRouteId = partialRouteId.substring(0, partialRouteId.lastIndexOf('.'));
+  let escapeStart = '[';
+  let escapeEnd = ']';
+  let result = '';
+  let rawSegmentBuffer = '';
+
+  let inEscapeSequence = 0;
+  let skipSegment = false;
+  for (let i = 0; i < partialRouteId.length; i++) {
+    let char = partialRouteId.charAt(i);
+    let lastChar = i > 0 ? partialRouteId.charAt(i - 1) : undefined;
+    let nextChar =
+      i < partialRouteId.length - 1 ? partialRouteId.charAt(i + 1) : undefined;
+
+    function isNewEscapeSequence() {
+      return (
+        !inEscapeSequence && char === escapeStart && lastChar !== escapeStart
+      );
+    }
+
+    function isCloseEscapeSequence() {
+      return inEscapeSequence && char === escapeEnd && nextChar !== escapeEnd;
+    }
+
+    function isStartOfLayoutSegment() {
+      return char === '_' && nextChar === '_' && !rawSegmentBuffer;
+    }
+
+    if (skipSegment) {
+      if (char === '/' || char === '.' || char === path.win32.sep) {
+        skipSegment = false;
+      }
+      continue;
+    }
+
+    if (isNewEscapeSequence()) {
+      inEscapeSequence++;
+      continue;
+    }
+
+    if (isCloseEscapeSequence()) {
+      inEscapeSequence--;
+      continue;
+    }
+
+    if (inEscapeSequence) {
+      result += char;
+      continue;
+    }
+
+    if (char === '/' || char === path.win32.sep || char === '.') {
+      if (rawSegmentBuffer === 'index' && result.endsWith('index')) {
+        result = result.replace(/\/?index$/, '');
+      } else {
+        result += '/';
+      }
+      rawSegmentBuffer = '';
+      continue;
+    }
+
+    if (isStartOfLayoutSegment()) {
+      skipSegment = true;
+      continue;
+    }
+
+    rawSegmentBuffer += char;
+
+    if (char === '$') {
+      result += typeof nextChar === 'undefined' ? '*' : ':';
+      continue;
+    }
+
+    result += char;
+  }
+
+  if (rawSegmentBuffer === 'index' && result.endsWith('index')) {
+    result = result.replace(/\/?index$/, '');
+  }
+
+  return result || undefined;
 }
