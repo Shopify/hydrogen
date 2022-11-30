7d0b5a8c63d08d6ae2529dc7ec62fef1865f3e39
Helen Lin <helen.lin@shopify.com>
2022-11-30 15:26:53 -0800

    fix dist generation of tsconfig

--- scripts/compile-template-for-dist.mjs
+++ scripts/compile-template-for-dist.mjs
@@ -7,6 +7,11 @@ import {createApp} from "@remix-run/dev";
   const source = resolve(process.cwd(), 'templates');
   const templateDir = `${source}/${template}`;
 
+  // Update node_modules override for dist
+  let tsConfig = await fs.readFile(resolve(templateDir, 'tsconfig.json'), 'utf8');
+  tsConfig = tsConfig.replace('../../node_modules', 'node_modules');
+  await fs.writeFile(resolve(templateDir, 'tsconfig.json'), tsConfig);
+
   await createNewApp(templateDir, `${templateDir}-ts`, true);
   await createNewApp(templateDir, `${templateDir}-js`, false);
   fs.removeSync(`${source}/${template}`);
