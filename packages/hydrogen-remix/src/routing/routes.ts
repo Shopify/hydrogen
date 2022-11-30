190b8347bbfbca455234064bff6efdc68409886b
Bret Little <bret.little@shopify.com>
2022-11-30 11:15:30 -0500

    Make routes within `.hydrogen` follow remix route conventions

--- packages/hydrogen-remix/src/routing/routes.ts
+++ packages/hydrogen-remix/src/routing/routes.ts
@@ -29,6 +29,8 @@ export async function hydrogenRoutes(
       );
 
       route(hydrogenRouteUrl, '../' + hydrogenRoutePath);
+
+      console.log(hydrogenRouteUrl, createRoutePath(hydrogenRouteUrl));
     }
   });
 }
@@ -98,4 +100,90 @@ async function buildLangRoutes() {
       )};\n`,
     );
   }
+}
+
+/**
+ * Generate a URL path based on remix file conventions. This is copied from:
+ * https://github.com/remix-run/remix/blob/09296128ca2f2d5e8932c631e777fbe8baaa192d/packages/remix-dev/config/routesConvention.ts#L116
+ */
+function createRoutePath(partialRouteId: string): string | undefined {
+  let escapeStart = '[';
+  let escapeEnd = ']';
+  let result = '';
+  let rawSegmentBuffer = '';
+
+  let inEscapeSequence = 0;
+  let skipSegment = false;
+  for (let i = 0; i < partialRouteId.length; i++) {
+    let char = partialRouteId.charAt(i);
+    let lastChar = i > 0 ? partialRouteId.charAt(i - 1) : undefined;
+    let nextChar =
+      i < partialRouteId.length - 1 ? partialRouteId.charAt(i + 1) : undefined;
+
+    function isNewEscapeSequence() {
+      return (
+        !inEscapeSequence && char === escapeStart && lastChar !== escapeStart
+      );
+    }
+
+    function isCloseEscapeSequence() {
+      return inEscapeSequence && char === escapeEnd && nextChar !== escapeEnd;
+    }
+
+    function isStartOfLayoutSegment() {
+      return char === '_' && nextChar === '_' && !rawSegmentBuffer;
+    }
+
+    if (skipSegment) {
+      if (char === '/' || char === '.' || char === path.win32.sep) {
+        skipSegment = false;
+      }
+      continue;
+    }
+
+    if (isNewEscapeSequence()) {
+      inEscapeSequence++;
+      continue;
+    }
+
+    if (isCloseEscapeSequence()) {
+      inEscapeSequence--;
+      continue;
+    }
+
+    if (inEscapeSequence) {
+      result += char;
+      continue;
+    }
+
+    if (char === '/' || char === path.win32.sep || char === '.') {
+      if (rawSegmentBuffer === 'index' && result.endsWith('index')) {
+        result = result.replace(/\/?index$/, '');
+      } else {
+        result += '/';
+      }
+      rawSegmentBuffer = '';
+      continue;
+    }
+
+    if (isStartOfLayoutSegment()) {
+      skipSegment = true;
+      continue;
+    }
+
+    rawSegmentBuffer += char;
+
+    if (char === '$') {
+      result += typeof nextChar === 'undefined' ? '*' : ':';
+      continue;
+    }
+
+    result += char;
+  }
+
+  if (rawSegmentBuffer === 'index' && result.endsWith('index')) {
+    result = result.replace(/\/?index$/, '');
+  }
+
+  return result || undefined;
 }
