c9b84a5371b2a27af6ba4678592f6bbf2da6936c
Fran Dios <fran.dios@shopify.com>
2022-12-02 12:59:20 +0900

    Remove deprecated option

--- templates/demo-store/remix.config.js
+++ templates/demo-store/remix.config.js
@@ -4,7 +4,6 @@ const {hydrogenRoutes} = require('@shopify/h2-test-hydrogen-remix/build');
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
   ignoredRouteFiles: ['**/.*'],
-  serverBuildDirectory: 'dist',
   async routes(defineRoutes) {
     return await hydrogenRoutes(defineRoutes, {});
   },
