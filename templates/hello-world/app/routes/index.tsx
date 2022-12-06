21a6efc7809bd4db8c99761563e79c09be01aa0f
Matt Seccafien <matt.seccafien@shopify.com>
2022-12-06 19:41:22 +0100

    typo

--- templates/hello-world/app/routes/index.tsx
+++ templates/hello-world/app/routes/index.tsx
@@ -2,7 +2,7 @@ export default function Index() {
   return (
     <div>
       <p>
-        Edit this route is at <em>./app/routes/index.tsx</em>.
+        Edit this route in <em>app/routes/index.tsx</em>.
       </p>
     </div>
   );
