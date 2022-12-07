a09e674ff8b6df4dee33bc17ef33178e14e0145c
Fran Dios <fran.dios@shopify.com>
2022-12-07 17:54:28 +0900

    Fix hello-world template

--- templates/hello-world/remix.env.d.ts
+++ templates/hello-world/remix.env.d.ts
@@ -1,3 +1,14 @@
 /// <reference types="@remix-run/dev" />
-/// <reference types="@remix-run/cloudflare" />
-/// <reference types="@cloudflare/workers-types" />
+/// <reference types="@remix-run/oxygen" />
+/// <reference types="@shopify/oxygen-workers-types" />
+
+import {HydrogenSession} from '../server';
+
+/**
+ * Declare local additions to `AppLoadContext` to include the session utilities we injected in `server.ts`.
+ */
+declare module '@shopify/hydrogen-remix' {
+  export interface AppLoadContext {
+    session: HydrogenSession;
+  }
+}
