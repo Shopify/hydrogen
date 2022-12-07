317d91b76a96da29a8c6ab135e449a436727e9e2
Fran Dios <fran.dios@shopify.com>
2022-12-07 12:18:00 +0900

    Move request logs to CLI

--- packages/cli/src/utils/mini-oxygen.ts
+++ packages/cli/src/utils/mini-oxygen.ts
@@ -35,9 +35,68 @@ export async function startMiniOxygen({
     buildWatchPaths: watch
       ? [path.resolve(root, buildPathWorkerFile)]
       : undefined,
+    onResponse: (request, response) =>
+      // 'Request' type in MiniOxygen comes from Miniflare,
+      // which is slightly different from standard Request type.
+      logResponse(request as unknown as Request, response),
   });
 
-  console.log(
-    `🚥 MiniOxygen server started at http://localhost:${actualPort}\n`,
+  const listeningAt = `http://localhost:${actualPort}`;
+
+  output.info(
+    output.content`🚥 MiniOxygen server started at ${output.token.link(
+      listeningAt,
+      listeningAt,
+    )}\n`,
   );
+}
+
+export function logResponse(request: Request, response: Response) {
+  try {
+    const url = new URL(request.url);
+    const isProxy = !!response.url && response.url !== request.url;
+    const isDataRequest = !isProxy && url.searchParams.has('_data');
+    let route = request.url.replace(url.origin, '');
+    let info = '';
+    let type = 'render';
+
+    if (isProxy) {
+      type = 'proxy';
+      info = `[${response.url}]`;
+    }
+
+    if (isDataRequest) {
+      type = request.method === 'GET' ? 'loader' : 'action';
+      const dataParam = url.searchParams.get('_data')?.replace('routes/', '');
+      route = url.pathname;
+      info = `[${
+        dataParam?.includes('/.hydrogen/')
+          ? dataParam.replace(/^.*(\.hydrogen\/)/, '$1')
+          : dataParam
+      }]`;
+    }
+
+    const colorizeStatus =
+      response.status < 300
+        ? output.token.green
+        : response.status < 400
+        ? output.token.cyan
+        : output.token.errorText;
+
+    output.info(
+      output.content`${request.method.padStart(6)}  ${colorizeStatus(
+        String(response.status),
+      )}  ${output.token.italic(type.padEnd(7, ' '))} ${route}${
+        info ? ' ' + colors.dim(info) : ''
+      } ${
+        request.headers.get('purpose') === 'prefetch'
+          ? output.token.italic('(prefetch)')
+          : ''
+      }`,
+    );
+  } catch {
+    if (request && response) {
+      console.log(`${request.method} ${response.status} ${request.url}`);
+    }
+  }
 }
