847f1b2800fb07a8adf67969e80d73574420ca8e
Josh Larson <josh.larson@shopify.com>
2022-12-14 10:22:19 -0600

    Remove compiled templates

--- templates/hello-world-js/app/components/Layout.jsx
+++
@@ -1,9 +0,0 @@
-export function Layout({ children, title, description }) {
-  return (
-    <div className="Layout">
-      <h1>{title}</h1>
-      <h2>{description}</h2>
-      {children}
-    </div>
-  );
-}
