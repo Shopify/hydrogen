fd6c1af5ff8f7c5c72b41ce309a66d3e58885b2b
Josh Larson <josh.larson@shopify.com>
2022-12-14 10:22:19 -0600

    Remove compiled templates

--- templates/demo-store-js/app/lib/seo/index.js
+++
@@ -1,4 +0,0 @@
-export { Seo } from "./seo";
-export { Debugger } from "./debugger";
-
-export * from "./types";
--- templates/hello-world-js/.eslintrc.js
+++
@@ -1,6 +0,0 @@
-/**
- * @type {import("@types/eslint").Linter.BaseConfig}
- */
-module.exports = {
-  extends: ['plugin:hydrogen/recommended', 'plugin:hydrogen/typescript'],
-};
