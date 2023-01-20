a6275c6460dcb17c218f536d485bbbf020483930
Fran Dios <fran.dios@shopify.com>
2023-01-20 12:47:57 +0900

    Shadow routes

---
+++ packages/cli/src/shadow-routes/index.tsx
@@ -0,0 +1,8 @@
+export default function Index() {
+  return (
+    <>
+      <p>Run the following command to generate a home page:</p>
+      <pre>npx shopify hydrogen generate route index</pre>
+    </>
+  );
+}
--- packages/cli/tsup.config.ts
+++ packages/cli/tsup.config.ts
@@ -1,8 +1,6 @@
 import {defineConfig} from 'tsup';
 
-export default defineConfig({
-  entry: ['src/**/*.ts'],
-  outDir: 'dist',
+const commonConfig = {
   format: 'esm',
   minify: false,
   bundle: false,
@@ -11,4 +9,19 @@ export default defineConfig({
   sourcemap: false,
   // The CLI is not imported anywhere so we don't need to generate types:
   dts: false,
-});
+};
+
+export default defineConfig([
+  {
+    ...commonConfig,
+    entry: ['src/**/*.ts'],
+    outDir: 'dist',
+  },
+  {
+    ...commonConfig,
+    entry: ['src/shadow-routes/**/*.tsx'],
+    outDir: 'dist/shadow-routes',
+    clean: true,
+    outExtension: () => ({js: '.jsx'}),
+  },
+]);
