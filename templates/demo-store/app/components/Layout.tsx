aff0e6a641f2725523a363756f28f124b423bd44
Fran Dios <fran.dios@shopify.com>
2023-01-25 20:49:04 +0900

    Move root queries

--- templates/demo-store/app/components/Layout.tsx
+++ templates/demo-store/app/components/Layout.tsx
@@ -23,10 +23,10 @@ import {
 import {useParams, Form, Await, useMatches} from '@remix-run/react';
 import {useWindowScroll} from 'react-use';
 import {Disclosure} from '@headlessui/react';
-import type {LayoutData} from '~/data';
 import {Suspense, useEffect, useMemo} from 'react';
 import {useIsHydrated} from '~/hooks/useIsHydrated';
 import {useCartFetchers} from '~/hooks/useCartFetchers';
+import type {LayoutData} from '../root';
 
 export function Layout({
   children,
