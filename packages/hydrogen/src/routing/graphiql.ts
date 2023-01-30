e754bfc00b58d0fc9bc1b840a3377b3b40eb7be3
Josh Larson <josh.larson@shopify.com>
2023-01-30 13:57:54 -0600

    Use the new version of graphiql, which is dope

--- packages/hydrogen/src/routing/graphiql.ts
+++ packages/hydrogen/src/routing/graphiql.ts
@@ -21,34 +21,56 @@ export function graphiqlLoader(
   return new Response(
     `
 <!DOCTYPE html>
-<html>
-<head>
-  <meta charset=utf-8/>
-  <meta name="viewport" content="user-scalable=no, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, minimal-ui">
-  <title>Shopify Storefront API</title>
-  <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/graphql-playground-react/build/static/css/index.css" />
-  <link rel="shortcut icon" href="//cdn.jsdelivr.net/npm/graphql-playground-react/build/favicon.png" />
-  <script src="//cdn.jsdelivr.net/npm/graphql-playground-react/build/static/js/middleware.js"></script>
-</head>
-<body>
-  <div id="root"></div>
-  <script>window.addEventListener('load', function (event) {
-    GraphQLPlayground.init(document.getElementById('root'), {
-      endpoint: '${url}',
-      settings:{
-        'request.globalHeaders': {
-          Accept: 'application/json',
-          'Content-Type': 'application/graphql',
-          'X-Shopify-Storefront-Access-Token': '${accessToken}'
-        }
-      },
-      tabs: [{
-        endpoint: '${url}',
-        query: '{ shop { name } }'
-      }]
-    })
-  })</script>
-</body>
+<html lang="en">
+  <head>
+    <title>GraphiQL</title>
+    <style>
+      body {
+        height: 100%;
+        margin: 0;
+        width: 100%;
+        overflow: hidden;
+      }
+
+      #graphiql {
+        height: 100vh;
+      }
+    </style>
+
+    <script
+      src="https://unpkg.com/react@17/umd/react.development.js"
+      integrity="sha512-Vf2xGDzpqUOEIKO+X2rgTLWPY+65++WPwCHkX2nFMu9IcstumPsf/uKKRd5prX3wOu8Q0GBylRpsDB26R6ExOg=="
+      crossorigin="anonymous"
+    ></script>
+    <script
+      src="https://unpkg.com/react-dom@17/umd/react-dom.development.js"
+      integrity="sha512-Wr9OKCTtq1anK0hq5bY3X/AvDI5EflDSAh0mE9gma+4hl+kXdTJPKZ3TwLMBcrgUeoY0s3dq9JjhCQc7vddtFg=="
+      crossorigin="anonymous"
+    ></script>
+    <link rel="stylesheet" href="https://unpkg.com/graphiql/graphiql.min.css" />
+  </head>
+
+  <body>
+    <div id="graphiql">Loading...</div>
+    <script
+      src="https://unpkg.com/graphiql/graphiql.min.js"
+      type="application/javascript"
+    ></script>
+    <script>
+      ReactDOM.render(
+        React.createElement(GraphiQL, {
+          fetcher: GraphiQL.createFetcher({
+            url: '${url}',
+            headers: {
+              'X-Shopify-Storefront-Access-Token': '${accessToken}',
+            }
+          }),
+          defaultEditorToolsVisibility: true,
+        }),
+        document.getElementById('graphiql'),
+      );
+    </script>
+  </body>
 </html>
   `,
     {status: 200, headers: {'content-type': 'text/html'}},
