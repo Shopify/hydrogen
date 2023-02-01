f203cbb2d4ce5cc919597f2cf255387e413f8c56
Fran Dios <fran.dios@shopify.com>
2023-02-01 20:43:03 +0900

    Add error and progress logs

--- packages/cli/src/utils/template-downloader.ts
+++ packages/cli/src/utils/template-downloader.ts
@@ -14,7 +14,7 @@ export async function getLatestReleaseDownloadUrl() {
   const response = await fetch(REPO_RELEASES_URL);
   if (!response.ok || response.status >= 400) {
     throw new Error(
-      `Failed to fetch latest release. Status ${response.status} ${response.statusText}}`,
+      `Failed to fetch the latest release information. Status ${response.status} ${response.statusText}}`,
     );
   }
 
@@ -31,7 +31,7 @@ export async function downloadTarball(url: string, storageDir: string) {
   const response = await fetch(url);
   if (!response.ok || response.status >= 400) {
     throw new Error(
-      `Failed to fetch latest release. Status ${response.status} ${response.statusText}}`,
+      `Failed to download the latest release files. Status ${response.status} ${response.statusText}}`,
     );
   }
 
@@ -50,21 +50,30 @@ export async function downloadTarball(url: string, storageDir: string) {
 }
 
 export async function getLatestTemplates() {
-  const {version, url} = await getLatestReleaseDownloadUrl();
-  const templateStoragePath = new URL('../starter-templates', import.meta.url)
-    .pathname;
+  try {
+    const {version, url} = await getLatestReleaseDownloadUrl();
+    const templateStoragePath = new URL('../starter-templates', import.meta.url)
+      .pathname;
 
-  if (!fs.existsSync(templateStoragePath)) {
-    fs.mkdirSync(templateStoragePath);
+    if (!fs.existsSync(templateStoragePath)) {
+      fs.mkdirSync(templateStoragePath);
+    }
+
+    const templateStorageVersionPath = path.join(templateStoragePath, version);
+    if (!fs.existsSync(templateStorageVersionPath)) {
+      await downloadTarball(url, templateStorageVersionPath);
+    }
+
+    return {
+      version,
+      templatesDir: path.join(templateStorageVersionPath, 'templates'),
+    };
+  } catch (e) {
+    const error = e as Error;
+    error.message =
+      `Could not download Hydrogen templates from GitHub.\nPlease check your internet connection and the following error:\n\n` +
+      error.message;
+
+    throw error;
   }
-
-  const templateStorageVersionPath = path.join(templateStoragePath, version);
-  if (!fs.existsSync(templateStorageVersionPath)) {
-    await downloadTarball(url, templateStorageVersionPath);
-  }
-
-  return {
-    version,
-    templatesDir: path.join(templateStorageVersionPath, 'templates'),
-  };
 }
