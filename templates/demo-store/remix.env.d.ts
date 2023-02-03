2649245d868f06f3518ee9594e0cb9bc986d922a
Bret Little <bret.little@shopify.com>
2023-02-03 15:47:46 -0500

    Add storefront-id header to API requests (#440)

--- templates/demo-store/remix.env.d.ts
+++ templates/demo-store/remix.env.d.ts
@@ -20,6 +20,7 @@ declare global {
     PRIVATE_STOREFRONT_API_TOKEN: string;
     PUBLIC_STOREFRONT_API_VERSION: string;
     PUBLIC_STORE_DOMAIN: string;
+    PUBLIC_STOREFRONT_ID: string;
   }
 }
 
--- templates/demo-store/server.ts
+++ templates/demo-store/server.ts
@@ -40,6 +40,7 @@ export default {
         privateStorefrontToken: env.PRIVATE_STOREFRONT_API_TOKEN,
         storeDomain: `https://${env.PUBLIC_STORE_DOMAIN}`,
         storefrontApiVersion: env.PUBLIC_STOREFRONT_API_VERSION || '2023-01',
+        storefrontId: env.PUBLIC_STOREFRONT_ID,
       });
 
       /**
