57b66129f805d0b2ae0c24a20950e6a9c7f2727a
Fran Dios <fran.dios@shopify.com>
2023-02-06 22:50:55 +0900

    Validate remix.config.js and remove --entry flag

--- packages/cli/src/utils/config.ts
+++ packages/cli/src/utils/config.ts
@@ -1,4 +1,8 @@
 import type {ServerMode} from '@remix-run/dev/dist/config/serverModes.js';
+import type {RemixConfig} from '@remix-run/dev/dist/config.js';
+import {renderFatalError} from '@shopify/cli-kit/node/ui';
+import {output, file} from '@shopify/cli-kit';
+import {createRequire} from 'module';
 import path from 'path';
 import fs from 'fs/promises';
 
@@ -12,11 +16,9 @@ export function getProjectPaths(appPath?: string, entry?: string) {
   const buildPath = path.join(root, BUILD_DIR);
   const buildPathClient = path.join(buildPath, CLIENT_SUBDIR);
   const buildPathWorkerFile = path.join(buildPath, WORKER_SUBDIR, 'index.js');
-  const entryFile = entry ? path.join(root, entry) : '';
 
   return {
     root,
-    entryFile,
     buildPath,
     buildPathClient,
     buildPathWorkerFile,
@@ -26,43 +28,114 @@ export function getProjectPaths(appPath?: string, entry?: string) {
 
 export async function getRemixConfig(
   root: string,
-  entryFile: string,
-  publicPath: string,
   mode = process.env.NODE_ENV as ServerMode,
 ) {
   const {readConfig} = await import('@remix-run/dev/dist/config.js');
-  const config = await readConfig(root, mode);
+  const config = (await readConfig(root, mode)) as RemixConfig & {
+    serverConditions?: string[];
+    serverMainFields?: string[];
+    serverDependenciesToBundle?: string;
+  };
 
-  const hydrogenAssetBase = process.env.HYDROGEN_ASSET_BASE_URL;
-  if (hydrogenAssetBase) {
-    const suffix = config.publicPath?.replace(/\\/g, '/').replace(/^\//, '');
-    config.publicPath = hydrogenAssetBase + suffix;
+  // TODO: Remove when updating Remix
+  if (!config.serverConditions) {
+    const require = createRequire(import.meta.url);
+    const actualConfigFile = require(path.join(root, 'remix.config'));
+    config.serverBuildTarget = 'cloudflare-workers';
+    config.serverConditions = actualConfigFile.serverConditions;
+    config.serverMainFields = actualConfigFile.serverMainFields;
   }
 
-  config.serverEntryPoint ??= entryFile;
-  config.serverBuildTarget = 'cloudflare-workers';
-  config.serverModuleFormat = 'esm';
-  config.serverPlatform = 'neutral';
+  if (!config.serverEntryPoint) {
+    throwConfigError(
+      'Could not find a server entry point.',
+      'Please add a server option to your remix.config.js pointing to an Oxygen worker entry file.',
+    );
+  } else {
+    assertEntryFileExists(config.rootDirectory, config.serverEntryPoint);
+  }
 
-  config.serverBuildPath = path.resolve(
-    root,
-    path.join(BUILD_DIR, WORKER_SUBDIR, 'index.js'),
-  );
-  config.relativeAssetsBuildDirectory = path.join(
+  if (config.serverPlatform !== 'neutral') {
+    throwConfigError(
+      'The serverPlatform in remix.config.js must be "neutral".',
+    );
+  }
+
+  if (config.serverModuleFormat !== 'esm') {
+    throwConfigError(
+      'The serverModuleFormat in remix.config.js must be "esm".',
+    );
+  }
+
+  if (config.serverDependenciesToBundle !== 'all') {
+    throwConfigError(
+      'The serverDependenciesToBundle in remix.config.js must be "all".',
+    );
+  }
+
+  if (!config.serverConditions?.includes('worker')) {
+    throwConfigError(
+      'The serverConditions in remix.config.js must include "worker".',
+    );
+  }
+
+  if (
+    process.env.NODE_ENV === 'development' &&
+    !config.serverConditions?.includes('development')
+  ) {
+    output.warn(
+      'Add `process.env.NODE_ENV` value to serverConditions in remix.config.js to enable debugging features in development.',
+    );
+  }
+
+  const expectedServerMainFields = ['browser', 'module', 'main'];
+
+  if (
+    !config.serverMainFields ||
+    !expectedServerMainFields.every(
+      (v, i) => config.serverMainFields?.[i] === v,
+    )
+  ) {
+    throwConfigError(
+      `The serverMainFields in remix.config.js must be ${JSON.stringify(
+        expectedServerMainFields,
+      )}.`,
+    );
+  }
+
+  const cdnUrl = process.env.HYDROGEN_ASSET_BASE_URL;
+  if (cdnUrl && !config.publicPath.startsWith(cdnUrl)) {
+    throwConfigError(
+      'The publicPath in remix.config.js must be prepended with the value of `process.env.HYDROGEN_ASSET_BASE_URL`.',
+    );
+  }
+
+  const expectedServerBuildPath = path.join(
     BUILD_DIR,
-    CLIENT_SUBDIR,
-    'build',
+    WORKER_SUBDIR,
+    'index.js',
   );
-  config.assetsBuildDirectory = path.resolve(
-    root,
-    config.relativeAssetsBuildDirectory,
-  );
+  if (config.serverBuildPath !== path.resolve(root, expectedServerBuildPath)) {
+    throwConfigError(
+      `The serverBuildPath in remix.config.js must be "${expectedServerBuildPath}".`,
+    );
+  }
 
-  config.watchPaths = [publicPath];
+  const expectedAssetsBuildDirectory = path.join(BUILD_DIR, CLIENT_SUBDIR);
+  if (
+    !config.assetsBuildDirectory.startsWith(
+      path.resolve(root, expectedAssetsBuildDirectory),
+    )
+  ) {
+    throwConfigError(
+      `The assetsBuildDirectory in remix.config.js must be in "${expectedAssetsBuildDirectory}".`,
+    );
+  }
 
   if (process.env.LOCAL_DEV) {
     // Watch local packages when developing in Hydrogen repo
     const packagesPath = new URL('../../..', import.meta.url).pathname;
+    config.watchPaths ??= [];
 
     config.watchPaths.push(
       ...(await fs.readdir(packagesPath)).map((pkg) =>
@@ -76,4 +149,41 @@ export async function getRemixConfig(
   }
 
   return config;
+}
+
+function throwConfigError(message: string, tryMessage: string | null = null) {
+  renderFatalError({
+    name: 'ConfigError',
+    type: 0,
+    message,
+    tryMessage,
+  });
+
+  process.exit(1);
+}
+
+export async function assertEntryFileExists(
+  root: string,
+  fileRelative: string,
+) {
+  const fileAbsolute = path.resolve(root, fileRelative);
+  const exists = await file.exists(fileAbsolute);
+
+  if (!exists) {
+    if (!path.extname(fileAbsolute)) {
+      const {readdir} = await import('fs/promises');
+      const files = await readdir(path.dirname(fileAbsolute));
+      const exists = files.some((file) => {
+        const {name, ext} = path.parse(file);
+        return name === path.basename(fileAbsolute) && /^\.[jt]s$/.test(ext);
+      });
+
+      if (exists) return;
+    }
+
+    throwConfigError(
+      `Entry file "${fileRelative}" not found.`,
+      'Please ensure the file exists and that the path is correctly added to the `server` property in remix.config.js.',
+    );
+  }
 }
