7e2a0f6701443f9ad8bdd2139b565a5693db8b04
Benjamin Sehl <ben.sehl@shopify.com>
2023-02-06 20:47:54 -0500

    Update layout text

--- templates/demo-store/app/components/Layout.tsx
+++ templates/demo-store/app/components/Layout.tsx
@@ -415,8 +415,7 @@ function Footer({menu}: {menu?: EnhancedMenu}) {
       divider={isHome ? 'none' : 'top'}
       as="footer"
       role="contentinfo"
-      className={`grid min-h-[25rem] items-start grid-flow-row w-full gap-6 py-8 px-6 md:px-8 lg:px-12
-        border-b md:gap-8 lg:gap-12 grid-cols-1 md:grid-cols-2 lg:grid-cols-${itemsCount}
+      className={`grid min-h-[25rem] items-start grid-flow-row w-full gap-6 py-8 px-6 md:px-8 lg:px-12 md:gap-8 lg:gap-12 grid-cols-1 md:grid-cols-2 lg:grid-cols-${itemsCount}
         bg-primary dark:bg-contrast dark:text-primary text-contrast overflow-hidden`}
     >
       <FooterMenu menu={menu} />
@@ -425,7 +424,7 @@ function Footer({menu}: {menu?: EnhancedMenu}) {
         className={`self-end pt-8 opacity-50 md:col-span-2 lg:col-span-${itemsCount}`}
       >
         &copy; {new Date().getFullYear()} / Shopify, Inc. Hydrogen is an MIT
-        Licensed Open Source project. This website is carbon&nbsp;neutral.
+        Licensed Open Source project.
       </div>
     </Section>
   );
