dbf16d0f0ad3ae4d39ad0b073bc7c71c85ad7935
Fran Dios <frankdiox@gmail.com>
2023-02-09 07:30:34 +0900

    Add extension to server entry to ensure Remix compiler watches the file (#456)

--- templates/hello-world/remix.config.js
+++ templates/hello-world/remix.config.js
@@ -3,7 +3,7 @@ module.exports = {
   appDirectory: 'app',
   ignoredRouteFiles: ['**/.*'],
   watchPaths: ['./public'],
-  server: './server',
+  server: './server.ts',
   /**
    * The following settings are required to deploy Hydrogen apps to Oxygen:
    */
