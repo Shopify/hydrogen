d7408e488000f33415120a1618411b17dcbdb5ea
johncraigcole <johncraigcole@gmail.com>
2023-02-17 09:20:33 -0500

    Added an invariant check

--- templates/demo-store/app/routes/($lang)/$shopid/orders/$token/authenticate.ts
+++ templates/demo-store/app/routes/($lang)/$shopid/orders/$token/authenticate.ts
@@ -1,5 +1,6 @@
 import {Shop} from '@shopify/hydrogen/storefront-api-types';
 import {LoaderArgs, redirect} from '@shopify/remix-oxygen';
+import invariant from 'tiny-invariant';
 
 export async function loader({request, context: {storefront}}: LoaderArgs) {
   const {origin} = new URL(request.url);
@@ -8,5 +9,6 @@ export async function loader({request, context: {storefront}}: LoaderArgs) {
   }>(`query getShopPrimaryDomain { shop { primaryDomain{ url } } }`, {
     cache: storefront.CacheLong(),
   });
+  invariant(shop, 'No data returned from shop primary domain query');
   return redirect(request.url.replace(origin, shop.primaryDomain.url));
 }
