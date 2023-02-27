e5de938aee9398966304d7ff5f16629cd3e2d828
Daniel Rios Pavia <daniel.riospavia@shopify.com>
2023-02-27 13:14:15 +0100

    fix remix.config and server.js

--- examples/flyio/remix.config.js
+++ examples/flyio/remix.config.js
@@ -2,23 +2,9 @@
 module.exports = {
   appDirectory: 'app',
   ignoredRouteFiles: ['**/.*'],
-  // watchPaths: ['./public'],
-  // server: './server.js',
-  /**
-   * The following settings are required to deploy Hydrogen apps to Oxygen:
-   */
   publicPath: (process.env.HYDROGEN_ASSET_BASE_URL ?? '/') + 'build/',
   serverDependenciesToBundle: 'all',
   serverModuleFormat: 'cjs',
   serverPlatform: 'node',
   serverMinify: process.env.NODE_ENV === 'production',
 };
-
-// /** @type {import('@remix-run/dev').AppConfig} */
-// module.exports = {
-//   ignoredRouteFiles: ['**/.*'],
-//   // appDirectory: "app",
-//   // assetsBuildDirectory: "public/build",
-//   // serverBuildPath: "build/index.js",
-//   // publicPath: "/build/",
-// };
--- examples/flyio/server.js
+++ examples/flyio/server.js
@@ -36,14 +36,14 @@ app.all('*', (req, res, next) => {
    * Create Hydrogen's Storefront client.
    */
   const {storefront} = createStorefrontClient({
-    // buyerIp: getBuyerIp(request),
+    buyerIp: req.get('oxygen-buyer-ip') ?? undefined,
     i18n: {language: 'EN', country: 'US'},
     publicStorefrontToken: env.PUBLIC_STOREFRONT_API_TOKEN,
     privateStorefrontToken: env.PRIVATE_STOREFRONT_API_TOKEN,
     storeDomain: `https://${env.PUBLIC_STORE_DOMAIN}`,
     storefrontApiVersion: env.PUBLIC_STOREFRONT_API_VERSION || '2023-01',
     storefrontId: env.PUBLIC_STOREFRONT_ID,
-    // requestGroupId: req.headers.get('request-id'),
+    requestGroupId: req.get('request-id') ?? undefined,
   });
 
   /**
