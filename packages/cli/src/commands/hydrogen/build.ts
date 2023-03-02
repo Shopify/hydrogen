9bad2fad38cfc5e6c06c16f65956492a7f7087c5
Matt Seccafien <462077+cartogram@users.noreply.github.com>
2023-03-02 18:36:09 +0100

    add to dev too

--- packages/cli/src/commands/hydrogen/build.ts
+++ packages/cli/src/commands/hydrogen/build.ts
@@ -2,29 +2,18 @@ import path from 'path';
 import {output, file} from '@shopify/cli-kit';
 import colors from '@shopify/cli-kit/node/colors';
 import {getProjectPaths, getRemixConfig} from '../../utils/config.js';
-import {commonFlags, flagsToCamelObject} from '../../utils/flags.js';
+import {
+  deprecated,
+  commonFlags,
+  flagsToCamelObject,
+} from '../../utils/flags.js';
 import Command from '@shopify/cli-kit/node/base-command';
-import {renderInfo} from '@shopify/cli-kit/node/ui';
 import Flags from '@oclif/core/lib/flags.js';
 import {checkLockfileStatus} from '../../utils/check-lockfile.js';
 import {findMissingRoutes} from '../../utils/missing-routes.js';
 import {getPackageManager} from '@shopify/cli-kit/node/node-package-manager';
 
 const LOG_WORKER_BUILT = '📦 Worker built';
-
-const deprecated = (name: string) =>
-  Flags.custom<null>({
-    parse: () => {
-      renderInfo({
-        headline: `The ${colors.bold(
-          name,
-        )} flag is deprecated and will be removed in a future version of Shopify CLI.`,
-      });
-
-      return Promise.resolve(null);
-    },
-    hidden: true,
-  });
 
 export default class Build extends Command {
   static description = 'Builds a Hydrogen storefront for production.';
@@ -41,7 +30,6 @@ export default class Build extends Command {
     }),
     base: deprecated('--base')(),
     entry: deprecated('--entry')(),
-    client: deprecated('--client')(),
     target: deprecated('--target')(),
   };
 
