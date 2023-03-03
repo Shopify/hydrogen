aa5af6045369a91aefe758e9a502005518db7ead
Fran Dios <fran.dios@shopify.com>
2023-03-03 15:19:45 +0900

    Cache full-page responses in Oxygen request handler

--- packages/remix-oxygen/src/server.ts
+++ packages/remix-oxygen/src/server.ts
@@ -3,23 +3,63 @@ import {
   type AppLoadContext,
   type ServerBuild,
 } from '@remix-run/server-runtime';
+import {
+  cacheResponse,
+  getItemFromCache,
+  isPublicCacheControlHeader,
+  isStale,
+  shouldCacheRequest,
+} from './cache';
 
-export function createRequestHandler<Context = unknown>({
+type CreateOxygenRequestHandler = {
+  build: ServerBuild;
+  mode?: string;
+  getLoadContext?: (
+    request: Request,
+  ) => Promise<AppLoadContext> | AppLoadContext;
+  waitUntil?: ExecutionContext['waitUntil'];
+};
+
+export function createRequestHandler({
   build,
   mode,
   getLoadContext,
-}: {
-  build: ServerBuild;
-  mode?: string;
-  getLoadContext?: (request: Request) => Promise<Context> | Context;
-}) {
+  waitUntil,
+}: CreateOxygenRequestHandler) {
   const handleRequest = createRemixRequestHandler(build, mode);
+  const handleRequestWithContext = async (request: Request) =>
+    handleRequest(request, await getLoadContext?.(request));
 
   return async (request: Request) => {
-    return handleRequest(
-      request,
-      (await getLoadContext?.(request)) as AppLoadContext,
-    );
+    const cache =
+      !!waitUntil &&
+      shouldCacheRequest(request) &&
+      (await caches.open('oxygen'));
+
+    if (cache) {
+      const cachedResponse = await getItemFromCache(cache, request);
+
+      if (cachedResponse) {
+        if (isStale(request, cachedResponse.headers)) {
+          cacheResponse(
+            request,
+            () => handleRequestWithContext(request),
+            cache,
+            waitUntil,
+          );
+        }
+
+        return cachedResponse;
+      }
+    }
+
+    const response = await handleRequestWithContext(request);
+
+    if (cache && isPublicCacheControlHeader(response)) {
+      cacheResponse(request, () => response.clone(), cache, waitUntil);
+    }
+
+    return response;
   };
 }
 
