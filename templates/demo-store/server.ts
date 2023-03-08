9a7f908b2e690dd49dd5261931e0b2bd24be9dd5
Helen Lin <helen.lin@shopify.com>
2023-03-07 16:30:16 -0800

    test hard code domain

--- templates/demo-store/server.ts
+++ templates/demo-store/server.ts
@@ -39,7 +39,7 @@ export default {
         i18n: getLocaleFromRequest(request),
         publicStorefrontToken: env.PUBLIC_STOREFRONT_API_TOKEN,
         privateStorefrontToken: env.PRIVATE_STOREFRONT_API_TOKEN,
-        storeDomain: `https://${env.PUBLIC_STORE_DOMAIN}`,
+        storeDomain: `https://checkout.hydrogen.shop`, //`https://${env.PUBLIC_STORE_DOMAIN}`,
         storefrontApiVersion: env.PUBLIC_STOREFRONT_API_VERSION || '2023-01',
         storefrontId: env.PUBLIC_STOREFRONT_ID,
         requestGroupId: request.headers.get('request-id'),
