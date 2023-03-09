3e859f3dbb380da83d294e7ecd41d28b2d7c9197
Daniel Rios <daniel.riospavia@shopify.com>
2023-03-09 17:01:08 +0100

    ShopPayButton now accepts storeDomain prop (#645)
    * extract shopify default context
    
    * update ShopPayButton to also accept a storeDomain prop
    
    Co-authored-by: Juan P. Prieto <juanpablo.prieto@shopify.com>
    
    * fix shop pay button in demo store
    
    * add changeset
    
    * bust turbo cache
    
    * Revert "bust turbo cache"
    
    This reverts commit 6023e837dfb35bc03d5e3dfc857df2209e0b8c02.
    
    * Update docs
    
    * fix test and generate doc
    
    * fix name
    
    * remove turbo cache
    
    * revert tubro cache
    
    ---------
    
    Co-authored-by: Juan P. Prieto <juanpablo.prieto@shopify.com>
    Co-authored-by: Helen Lin <helen.lin@shopify.com>

--- packages/hydrogen-react/src/ShopPayButton.doc.ts
+++ packages/hydrogen-react/src/ShopPayButton.doc.ts
@@ -5,10 +5,10 @@ const data: ReferenceEntityTemplateSchema = {
   category: 'components',
   isVisualComponent: false,
   related: [],
-  description: `The \`ShopPayButton\` component renders a button that redirects to the Shop Pay checkout. It renders a [\`<shop-pay-button>\`](https://shopify.dev/custom-storefronts/tools/web-components) custom element, for which it will lazy-load the source code automatically. It relies on the \`<ShopProvider>\` context provider.`,
+  description: `The \`ShopPayButton\` component renders a button that redirects to the Shop Pay checkout. It renders a [\`<shop-pay-button>\`](https://shopify.dev/custom-storefronts/tools/web-components) custom element, for which it will lazy-load the source code automatically.`,
   type: 'component',
   defaultExample: {
-    description: 'I am the default example',
+    description: '<ShopPayButton> without <ShopifyProvider>',
     codeblock: {
       tabs: [
         {
@@ -22,8 +22,32 @@ const data: ReferenceEntityTemplateSchema = {
           language: 'tsx',
         },
       ],
-      title: 'Example code',
+      title: '<ShopPayButton> without <ShopifyProvider>',
     },
+  },
+  examples: {
+    description: '',
+    examples: [
+      {
+        description:
+          'If `<ShopifyProvider>` context provider is used in your app, you can use the `<ShopPayButton>` without supplying a `storeDomain` prop',
+        codeblock: {
+          tabs: [
+            {
+              title: 'JavaScript',
+              code: './ShopPayButton2.example.jsx',
+              language: 'jsx',
+            },
+            {
+              title: 'TypeScript',
+              code: './ShopPayButton2.example.tsx',
+              language: 'tsx',
+            },
+          ],
+          title: '<ShopPayButton> with <ShopifyProvider>',
+        },
+      },
+    ],
   },
   definitions: [
     {
---
+++ packages/hydrogen-react/src/ShopPayButton2.example.jsx
@@ -0,0 +1,19 @@
+import {ShopifyProvider, ShopPayButton} from '@shopify/hydrogen-react';
+
+export default function App() {
+  return (
+    <ShopifyProvider
+      storeDomain="my-store"
+      storefrontToken="abc123"
+      storefrontApiVersion="2023-01"
+      countryIsoCode="CA"
+      languageIsoCode="EN"
+    >
+      <AddVariantQuantity1 variantId="gid://shopify/ProductVariant/1" />
+    </ShopifyProvider>
+  );
+}
+
+export function AddVariantQuantity1({variantId}) {
+  return <ShopPayButton variantIds={[variantId]} />;
+}
