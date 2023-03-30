958aed55a2b5bfc412a003d760dd0c13a2286e20
Helen Lin <helen.lin@shopify.com>
2023-03-30 12:30:52 -0700

    Fix virtual route

--- packages/cli/src/virtual-routes/virtual-root.tsx
+++ packages/cli/src/virtual-routes/virtual-root.tsx
@@ -20,15 +20,12 @@ export const links: LinksFunction = () => {
   ];
 };
 
-export const meta: MetaFunction = () => ({
-  charset: 'utf-8',
-  viewport: 'width=device-width,initial-scale=1',
-});
-
 export default function App() {
   return (
     <html lang="en">
       <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
         <Meta />
         <Links />
       </head>
