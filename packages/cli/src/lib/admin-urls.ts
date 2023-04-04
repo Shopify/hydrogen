e2f6a521850ce464bfa45d7a22568732f037c116
Gray Gilmore <graygilmore@gmail.com>
2023-04-03 17:02:27 -0700

    Add utility for admin URLs

---
+++ packages/cli/src/lib/admin-urls.ts
@@ -0,0 +1,22 @@
+import type {AdminSession} from '@shopify/cli-kit/node/session';
+
+export function newHydrogenStorefrontUrl(session: AdminSession) {
+  const {storeFqdn} = session;
+  return `https://${storeFqdn}/admin/custom_storefronts/new`;
+}
+
+export function hydrogenStorefrontUrl(
+  session: AdminSession,
+  storefrontId: string,
+) {
+  const {storeFqdn} = session;
+  return `https://${storeFqdn}/admin/custom_storefronts/${storefrontId}`;
+}
+
+export function hydrogenStorefrontSettingsUrl(
+  session: AdminSession,
+  storefrontId: string,
+) {
+  const {storeFqdn} = session;
+  return `https://${storeFqdn}/admin/custom_storefronts/${storefrontId}/settings`;
+}
