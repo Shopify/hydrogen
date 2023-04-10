df5bf1257702f090fdec698fb2de131eb94c24c4
Anthony Frehner <frehner@users.noreply.github.com>
2023-04-10 14:56:18 -0600

    v1 and v2 ErrorBoundaries
    Also fix commenting style, and add a doc to guidelines.

--- templates/skeleton/app/routes/account/register.tsx
+++ templates/skeleton/app/routes/account/register.tsx
@@ -4,7 +4,12 @@ import {
   type ErrorBoundaryComponent,
   redirect,
 } from '@shopify/remix-oxygen';
-import {Form, useCatch} from '@remix-run/react';
+import {
+  Form,
+  useCatch,
+  useRouteError,
+  isRouteErrorResponse,
+} from '@remix-run/react';
 
 export async function loader({context, params}: LoaderArgs) {
   const customerAccessToken = await context.session.get('customerAccessToken');
@@ -67,16 +72,20 @@ export default function Register() {
   );
 }
 
-// Remix v1 implementation of ErrorBoundary; for Remix v2, this will need to be updated according to the following documentation:
-// https://remix.run/docs/en/1.15.0/pages/v2#catchboundary-and-errorboundary
-export const ErrorBoundary: ErrorBoundaryComponent = ({error}) => {
+/**
+ * Remix v1 implementation of ErrorBoundary
+ * https://remix.run/docs/en/1.15.0/pages/v2#catchboundary-and-errorboundary
+ */
+export const ErrorBoundaryV1: ErrorBoundaryComponent = ({error}) => {
   console.error(error);
 
   return <div>There was an error.</div>;
 };
 
-// Remix v1 implementation of CatchBoundary; for v2, you can remove `CatchBoundary` and implement the error handling in `ErrorBoundary`:
-// https://remix.run/docs/en/1.15.0/pages/v2#catchboundary-and-errorboundary
+/**
+ * Remix v1 implementation of CatchBoundary
+ * https://remix.run/docs/en/1.15.0/pages/v2#catchboundary-and-errorboundary
+ */
 export function CatchBoundary() {
   const caught = useCatch();
   console.error(caught);
@@ -87,4 +96,20 @@ export function CatchBoundary() {
       {caught.data?.message}
     </div>
   );
+}
+
+/**
+ * Remix v2 ErrorBoundary
+ * https://remix.run/docs/en/1.15.0/route/error-boundary-v2
+ */
+export function ErrorBoundary() {
+  const error = useRouteError();
+
+  if (isRouteErrorResponse(error)) {
+    console.error(error.status, error.statusText, error.data);
+    return <div>Route Error</div>;
+  } else {
+    console.error((error as Error).message);
+    return <div>Thrown Error</div>;
+  }
 }
