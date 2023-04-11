e2f88fe8d123441bc4418d8936f7240eb5315d7a
Anthony Frehner <frehner@users.noreply.github.com>
2023-04-11 11:06:44 -0600

    Updating templates based on guidelines

--- templates/skeleton/app/routes/collections/$collectionHandle.tsx
+++ templates/skeleton/app/routes/collections/$collectionHandle.tsx
@@ -1,9 +1,18 @@
-import {json, type LoaderArgs} from '@shopify/remix-oxygen';
-import {useLoaderData} from '@remix-run/react';
+import {
+  json,
+  type LoaderArgs,
+  type ErrorBoundaryComponent,
+} from '@shopify/remix-oxygen';
+import {
+  useLoaderData,
+  Link,
+  useCatch,
+  useRouteError,
+  isRouteErrorResponse,
+} from '@remix-run/react';
 import type {Collection as CollectionType} from '@shopify/hydrogen/storefront-api-types';
-import {Link} from '@remix-run/react';
 
-export async function loader({params, request, context}: LoaderArgs) {
+export async function loader({params, context}: LoaderArgs) {
   const {collectionHandle} = params;
 
   const {collection} = await context.storefront.query<{
@@ -38,8 +47,38 @@ export default function Collection() {
   );
 }
 
+export const ErrorBoundaryV1: ErrorBoundaryComponent = ({error}) => {
+  console.error(error);
+
+  return <div>There was an error.</div>;
+};
+
+export function CatchBoundary() {
+  const caught = useCatch();
+  console.error(caught);
+
+  return (
+    <div>
+      There was an error. Status: {caught.status}. Message:{' '}
+      {caught.data?.message}
+    </div>
+  );
+}
+
+export function ErrorBoundary() {
+  const error = useRouteError();
+
+  if (isRouteErrorResponse(error)) {
+    console.error(error.status, error.statusText, error.data);
+    return <div>Route Error</div>;
+  } else {
+    console.error((error as Error).message);
+    return <div>Thrown Error</div>;
+  }
+}
+
 const COLLECTION_QUERY = `#graphql
-  query CollectionDetails(
+  query collection_details(
     $handle: String!
     $country: CountryCode
     $language: LanguageCode
