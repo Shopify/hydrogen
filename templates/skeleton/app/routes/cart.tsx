082ce87667136c9c76f04dcce48e2921814bcbd5
Anthony Frehner <frehner@users.noreply.github.com>
2023-04-13 09:13:55 -0600

    Finish updating the templates to the guidelines (#783)

--- templates/skeleton/app/routes/cart.tsx
+++ templates/skeleton/app/routes/cart.tsx
@@ -1,7 +1,14 @@
-import {Await, useMatches} from '@remix-run/react';
+import {
+  Await,
+  useMatches,
+  useCatch,
+  useRouteError,
+  isRouteErrorResponse,
+} from '@remix-run/react';
 import {Suspense} from 'react';
 import {flattenConnection} from '@shopify/hydrogen';
 import type {Cart as CartType} from '@shopify/hydrogen/storefront-api-types';
+import {type ErrorBoundaryComponent} from '@shopify/remix-oxygen';
 
 export async function action() {
   // @TODO implement cart action
@@ -11,7 +18,10 @@ export default function CartRoute() {
   const [root] = useMatches();
   return (
     <Suspense fallback="loading">
-      <Await resolve={root.data?.cart as CartType}>
+      <Await
+        resolve={root.data?.cart as CartType}
+        errorElement={<div>An error occurred</div>}
+      >
         {(cart) => {
           const linesCount = Boolean(cart?.lines?.edges?.length || 0);
           if (!linesCount) {
@@ -38,4 +48,34 @@ export default function CartRoute() {
       </Await>
     </Suspense>
   );
+}
+
+export const ErrorBoundaryV1: ErrorBoundaryComponent = ({error}) => {
+  console.error(error);
+
+  return <div>There was an error.</div>;
+};
+
+export function CatchBoundary() {
+  const caught = useCatch();
+  console.error(caught);
+
+  return (
+    <div>
+      There was an error. Status: {caught.status}. Message:{' '}
+      {caught.data?.message}
+    </div>
+  );
+}
+
+export function ErrorBoundary() {
+  const error = useRouteError();
+
+  if (isRouteErrorResponse(error)) {
+    console.error(error.status, error.statusText, error.data);
+    return <div>Route Error</div>;
+  } else {
+    console.error((error as Error).message);
+    return <div>Thrown Error</div>;
+  }
 }
