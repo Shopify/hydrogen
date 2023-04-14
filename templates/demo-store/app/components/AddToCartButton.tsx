aacd0b46d4a935706e972c7aa35bec25c30fb701
Helen Lin <helen.lin@shopify.com>
2023-04-13 17:29:00 -0700

    clean up

--- templates/demo-store/app/components/AddToCartButton.tsx
+++ templates/demo-store/app/components/AddToCartButton.tsx
@@ -54,23 +54,3 @@ export function AddToCartButton({
     </CartForm>
   );
 }
-
-// return (
-//   <fetcher.Form action="/cart" method="post">
-//     <input type="hidden" name="cartAction" value={CartAction.ADD_TO_CART} />
-//     <input type="hidden" name="countryCode" value={selectedLocale.country} />
-//     <input type="hidden" name="lines" value={JSON.stringify(lines)} />
-// <input type="hidden" name="analytics" value={JSON.stringify(analytics)} />
-// <Button
-//   as="button"
-//   type="submit"
-//   width={width}
-//   variant={variant}
-//   className={className}
-//   disabled={disabled ?? fetcherIsNotIdle}
-//   {...props}
-// >
-//   {children}
-// </Button>
-//   </fetcher.Form>
-// );
