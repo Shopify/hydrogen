a9528701d567b63ab4e66df623e852c1d11e2b3c
Gray Gilmore <graygilmore@gmail.com>
2023-04-18 15:30:27 -0700

    Add unlink command

---
+++ packages/cli/src/commands/hydrogen/unlink.ts
@@ -0,0 +1,40 @@
+import Command from '@shopify/cli-kit/node/base-command';
+import {outputSuccess, outputWarn} from '@shopify/cli-kit/node/output';
+
+import {commonFlags} from '../../lib/flags.js';
+import {getConfig, unsetStorefront} from '../../lib/shopify-config.js';
+
+export default class Unlink extends Command {
+  static description = 'Unlink a local project from a Hydrogen storefront.';
+
+  static hidden = true;
+
+  static flags = {
+    path: commonFlags.path,
+  };
+
+  async run(): Promise<void> {
+    const {flags} = await this.parse(Unlink);
+    await unlinkStorefront(flags);
+  }
+}
+
+export interface LinkFlags {
+  path?: string;
+}
+
+export async function unlinkStorefront({path}: LinkFlags) {
+  const actualPath = path ?? process.cwd();
+  const {storefront: configStorefront} = await getConfig(actualPath);
+
+  if (!configStorefront) {
+    outputWarn("This project isn't linked to a Hydrogen storefront.");
+    return;
+  }
+
+  const storefrontTitle = configStorefront.title;
+
+  await unsetStorefront(actualPath);
+
+  outputSuccess(`You are no longer linked to ${storefrontTitle}`);
+}
