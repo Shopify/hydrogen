e7fb600f7198130e4c82cc51a7391275df200c53
Graham F. Scott <gfscott@gmail.com>
2023-04-28 17:09:24 -0400

    Add comment on env object

--- packages/hydrogen/docs/staticPages/authenticate.server.js
+++ packages/hydrogen/docs/staticPages/authenticate.server.js
@@ -12,6 +12,7 @@ const {storefront} = createStorefrontClient({
   cache,
   waitUntil,
   i18n: {language: 'EN', country: 'US'},
+  // `env` provides access to runtime data, including environment variables
   publicStorefrontToken: env.PUBLIC_STOREFRONT_API_TOKEN,
   privateStorefrontToken: env.PRIVATE_STOREFRONT_API_TOKEN,
   storeDomain: `https://${env.PUBLIC_STORE_DOMAIN}`,
