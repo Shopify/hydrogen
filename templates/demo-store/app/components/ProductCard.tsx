f7ae998a290625eea9b4fbaec4b86e76dae2d535
aydenja <79268298+aydenja@users.noreply.github.com>
2023-04-30 23:20:29 -0500

    Changed cart naming to be consistent (#822)
    * Changed cart naming to be consistent
    
    * retrigger checks

--- templates/demo-store/app/components/ProductCard.tsx
+++ templates/demo-store/app/components/ProductCard.tsx
@@ -120,7 +120,7 @@ export function ProductCard({
           }}
         >
           <Text as="span" className="flex items-center justify-center gap-2">
-            Add to Bag
+            Add to Cart
           </Text>
         </AddToCartButton>
       )}
