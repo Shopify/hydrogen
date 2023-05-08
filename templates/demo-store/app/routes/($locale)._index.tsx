96dad7aa6a6933ad7d7cc6f6a099fb5bd902ef6b
Benjamin Sehl <ben.sehl@shopify.com>
2023-05-08 16:14:34 -0400

    Rename ($lang) paths to ($locale) in Demo Store (#860)
    * Rename () paths to ()
    
    * Updates import statements
    
    * Updates params references
    
    * Fix missed references to lang
    
    * Fix broken policies route

--- templates/demo-store/app/routes/($locale)._index.tsx
+++ templates/demo-store/app/routes/($locale)._index.tsx
@@ -26,11 +26,11 @@ export async function loader({params, context}: LoaderArgs) {
   const {language, country} = context.storefront.i18n;
 
   if (
-    params.lang &&
-    params.lang.toLowerCase() !== `${language}-${country}`.toLowerCase()
+    params.locale &&
+    params.locale.toLowerCase() !== `${language}-${country}`.toLowerCase()
   ) {
-    // If the lang URL param is defined, yet we still are on `EN-US`
-    // the the lang param must be invalid, send to the 404 page
+    // If the locale URL param is defined, yet we still are on `EN-US`
+    // the the locale param must be invalid, send to the 404 page
     throw new Response(null, {status: 404});
   }
 
