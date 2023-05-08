b015f7ef3600ab6c7f982454b5f2bef79f5212d4
Fran Dios <fran.dios@shopify.com>
2023-05-08 17:53:06 +0900

    Use stable features in demo-store

--- templates/demo-store/remix.config.js
+++ templates/demo-store/remix.config.js
@@ -16,9 +16,9 @@ module.exports = {
   serverModuleFormat: 'esm',
   serverPlatform: 'neutral',
   serverMinify: process.env.NODE_ENV === 'production',
+  postcss: true,
+  tailwind: true,
   future: {
-    unstable_postcss: true,
-    unstable_tailwind: true,
     v2_meta: true,
     v2_routeConvention: true,
     v2_errorBoundary: true,
