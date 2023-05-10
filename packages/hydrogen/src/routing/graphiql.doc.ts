3bc3ab05edc5b6719bf52e52131188da23561aff
Bret Little <bret.little@shopify.com>
2023-05-10 16:05:43 -0400

    Fixes

--- packages/hydrogen/src/routing/graphiql.doc.ts
+++ packages/hydrogen/src/routing/graphiql.doc.ts
@@ -5,7 +5,7 @@ const data: ReferenceEntityTemplateSchema = {
   category: 'utilities',
   isVisualComponent: false,
   related: [],
-  description: `This function creates an instance of [GraphiQL](https://graphql.org/swapi-graphql) in your Hydrogen app when running on a development server. This enables you to explore, write, and test GraphQL queries using your store's live data from the Storefront API. You can visit the GraphiQL app at your storefront route /graphiql. This function accepts [Remix LoaderArgs](https://remix.run/docs/en/v1/route/loader#type-safety) if you need to customize GraphiQL's behavior. Learn more about [using GraphiQL in Hydrogen](/docs/custom-storefronts/hydrogen/data-fetching/graphiql).`,
+  description: `This function creates an instance of [GraphiQL](https://graphql.org/swapi-graphql) in your Hydrogen app when running on a development server. This enables you to explore, write, and test GraphQL queries using your store's live data from the Storefront API. You can visit the GraphiQL app at your storefront route /graphiql. Learn more about [using GraphiQL in Hydrogen](/docs/custom-storefronts/hydrogen/data-fetching/graphiql).`,
   type: 'utility',
   defaultExample: {
     description: 'I am the default example',
