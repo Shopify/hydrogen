808b8c47281a61fe1179477d799995a47e989fd7
Bret Little <bret.little@shopify.com>
2023-05-11 14:47:15 -0400

    Add hydrogen reference docs (#820)

---
+++ packages/hydrogen/src/cache/CacheShort.example.js
@@ -0,0 +1,19 @@
+import {json} from '@shopify/remix-oxygen';
+import {CacheShort} from '@shopify/hydrogen';
+
+export async function loader({context}) {
+  const data = await context.storefront.query(
+    `#grahpql
+  {
+    shop {
+      name
+      description
+    }
+  }`,
+    {
+      cache: CacheShort(),
+    },
+  );
+
+  return json(data);
+}
