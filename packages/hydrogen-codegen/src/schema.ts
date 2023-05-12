620e3c9f3b38fa9040add316a839f3f9639922cd
Fran Dios <fran.dios@shopify.com>
2023-05-12 14:41:56 +0900

    Use JSON schema from Hydrogen package

--- packages/hydrogen-codegen/src/schema.ts
+++ packages/hydrogen-codegen/src/schema.ts
@@ -1,5 +1,5 @@
 // This comment is used during ESM build:
 //! import {createRequire} from 'module'; const require = createRequire(import.meta.url);
 export const schema = require.resolve(
-  '@shopify/hydrogen-react/storefront.schema.json',
+  '@shopify/hydrogen/storefront.schema.json',
 );
