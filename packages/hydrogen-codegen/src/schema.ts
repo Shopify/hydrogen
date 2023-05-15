a83cabd3b1e4dfb1c5be401639dfc0ecf5a8fe5d
Fran Dios <fran.dios@shopify.com>
2023-05-15 20:26:50 +0900

    Make schema accessible without building Hydrogen first

--- packages/hydrogen-codegen/src/schema.ts
+++ packages/hydrogen-codegen/src/schema.ts
@@ -1,14 +1,14 @@
 // This comment is used during ESM build:
 //! import {createRequire} from 'module'; const require = createRequire(import.meta.url);
 export const getSchema = () =>
-  require.resolve('@shopify/hydrogen/storefront.schema.json');
+  require.resolve('@shopify/hydrogen-react/storefront.schema.json');
 
 let staticSchema = '';
 
 try {
   staticSchema = getSchema();
 } catch {
-  // This can happen at build time or when '@shopify/hydrogen' is not found.
+  // This can happen at build time or when '@shopify/hydrogen-react' is not found.
   // Generally this shouldn't be an issue in real apps so let's ignore the error.
 }
 
