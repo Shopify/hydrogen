6025ddffe7e2213e897dffa2884a75cdb1d09013
Bret Little <bret.little@shopify.com>
2023-05-16 13:06:55 -0400

    Update packages/hydrogen/src/pagination/Pagination.doc.ts
    Co-authored-by: Anthony Frehner <frehner@users.noreply.github.com>

--- packages/hydrogen/src/pagination/Pagination.doc.ts
+++ packages/hydrogen/src/pagination/Pagination.doc.ts
@@ -5,7 +5,7 @@ const data: ReferenceEntityTemplateSchema = {
   category: 'components',
   isVisualComponent: false,
   related: [],
-  description: `The \`<Pagination />\` component makes it easy to pagination lists using cursors from the Storefront API. It is important for paginatino state to be maintained in the URL, so that the user can navigate to a product and return back to the same scrolled position in a list. It is also important that the list state is shareable via URL. The \`<Pagination>\` componenent provides a render prop with properties to load more elements into your list.`,
+  description: `The \`<Pagination />\` component makes it easy to pagination lists using cursors from the Storefront API. It is important for pagination state to be maintained in the URL, so that the user can navigate to a product and return back to the same scrolled position in a list. It is also important that the list state is shareable via URL. The \`<Pagination>\` component provides a render prop with properties to load more elements into your list.`,
   type: 'component',
   defaultExample: {
     description: 'I am the default example',
