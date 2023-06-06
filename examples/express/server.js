3a1d0d1b303d207b36a8fad3f8f5ac4b1ce6d5d9
Fran Dios <fran.dios@shopify.com>
2023-06-06 20:15:10 +0900

    Make storefrontApiVersion optional and remove it by default in examples

--- examples/express/server.js
+++ examples/express/server.js
@@ -88,7 +88,6 @@ async function getContext(req) {
     publicStorefrontToken: env.PUBLIC_STOREFRONT_API_TOKEN,
     privateStorefrontToken: env.PRIVATE_STOREFRONT_API_TOKEN,
     storeDomain: `https://${env.PUBLIC_STORE_DOMAIN}`,
-    storefrontApiVersion: env.PUBLIC_STOREFRONT_API_VERSION || '2023-04',
     storefrontId: env.PUBLIC_STOREFRONT_ID,
     storefrontHeaders: {
       requestGroupId: req.get('request-id'),
