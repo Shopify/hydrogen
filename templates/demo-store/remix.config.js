da87c5f3f32a9c85fbe28ec06aefb9b8a4e6c670
Fran Dios <fran.dios@shopify.com>
2023-06-09 15:59:26 +0900

    Enable v2_headers flag

--- templates/demo-store/remix.config.js
+++ templates/demo-store/remix.config.js
@@ -20,8 +20,9 @@ module.exports = {
   tailwind: true,
   future: {
     v2_meta: true,
-    v2_routeConvention: true,
+    v2_headers: true,
     v2_errorBoundary: true,
+    v2_routeConvention: true,
     v2_normalizeFormMethod: true,
   },
 };
