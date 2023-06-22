0dd08dbbc3a997ac0978c8e7c83e0423b8ea7fa4
Bret Little <bret.little@shopify.com>
2023-06-22 17:09:29 -0400

    Add comment

--- templates/demo-store/app/routes/($locale).products.$productHandle.tsx
+++ templates/demo-store/app/routes/($locale).products.$productHandle.tsx
@@ -52,6 +52,11 @@ export async function loader({params, request, context}: LoaderArgs) {
     },
   });
 
+  // In order to show which variants are available in the UI, we need to query
+  // all of them. But there might be a *lot*, so instead separate the variants
+  // into it's own separate query that is deferred. So there's a brief moment
+  // where variant options might show as available when they're not, but after
+  // this deffered query resolves, the UI will update.
   const variants = context.storefront.query(VARIANTS_QUERY, {
     variables: {
       handle: productHandle,
