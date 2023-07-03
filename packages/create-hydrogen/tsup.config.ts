300da1cbc5eded3db0caf56cb55daabd4783f5c7
Fran Dios <fran.dios@shopify.com>
2023-07-03 16:04:34 +0900

    Do not overwrite version added by changeset

--- packages/create-hydrogen/tsup.config.ts
+++ packages/create-hydrogen/tsup.config.ts
@@ -1,6 +1,4 @@
 import {defineConfig} from 'tsup';
-import {createRequire} from 'module';
-import fs from 'fs/promises';
 
 export default defineConfig({
   entry: ['src/**/*.ts'],
@@ -12,18 +10,4 @@ export default defineConfig({
   treeshake: true,
   sourcemap: false,
   dts: false,
-  async onSuccess() {
-    const require = createRequire(import.meta.url);
-    const {version} = require('@shopify/cli-hydrogen/package.json');
-
-    const content = await fs.readFile('./package.json', 'utf-8');
-    await fs.writeFile(
-      './package.json',
-      content.replace(
-        /"(@shopify\/cli-hydrogen)":\s+".*"/gm,
-        `"$1": "^${version}"`,
-      ),
-      'utf-8',
-    );
-  },
 });
