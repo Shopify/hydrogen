61d2d9804c9a9017ef9bd38b65b6a30d41a737d9
Bret Little <bret.little@shopify.com>
2023-07-06 11:33:10 -0400

    Product Form Abstraction (#1027)
    * Product form implementation
    
    https://github.com/Shopify/hydrogen-internal/issues/23

---
+++ packages/hydrogen/src/product/getFirstAvailableVariant.example.tsx
@@ -0,0 +1,32 @@
+import {getFirstAvailableVariant__unstable as getFirstAvailableVariant} from '@shopify/hydrogen';
+import {json, type LoaderArgs} from '@shopify/remix-oxygen';
+
+export async function loader({params, context}: LoaderArgs) {
+  const {product} = await context.storefront.query(PRODUCT_QUERY, {
+    variables: {
+      handle: params.productHandle,
+    },
+  });
+
+  const firstAvailableVariant = getFirstAvailableVariant(product.variants);
+
+  return json({product, firstAvailableVariant});
+}
+
+const PRODUCT_QUERY = `#graphql
+  query Product($handle: String!) {
+    product(handle: $handle) {
+      title
+      description
+      options {
+        name
+        values 
+      }
+      variants(first: 250) {
+        nodes {
+          ...ProductVariantFragment
+        }
+      }
+    }
+  }
+`;
