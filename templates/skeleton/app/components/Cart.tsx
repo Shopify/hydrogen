ec2e3dd037e555e65f7a8f58d393e06b20ef69ac
Juan P. Prieto <juanpablo.prieto@shopify.com>
2023-07-14 09:15:37 -0700

    fix cart-line click

--- templates/skeleton/app/components/Cart.tsx
+++ templates/skeleton/app/components/Cart.tsx
@@ -73,46 +73,45 @@ function CartLineItem({
   const lineItemUrl = useVariantUrl(product.handle, selectedOptions);
 
   return (
-    <li key={id}>
-      <Link
-        className="cart-line"
-        prefetch="intent"
-        to={lineItemUrl}
-        onClick={() => {
-          if (layout === 'aside') {
-            // close the drawer
-            window.location.href = lineItemUrl.to;
-          }
-        }}
-      >
-        {image && (
-          <Image
-            alt={title}
-            aspectRatio="1/1"
-            data={image}
-            height={100}
-            loading="lazy"
-            width={100}
-          />
-        )}
+    <li key={id} className="cart-line">
+      {image && (
+        <Image
+          alt={title}
+          aspectRatio="1/1"
+          data={image}
+          height={100}
+          loading="lazy"
+          width={100}
+        />
+      )}
 
-        <div>
+      <div>
+        <Link
+          prefetch="intent"
+          to={lineItemUrl}
+          onClick={() => {
+            if (layout === 'aside') {
+              // close the drawer
+              window.location.href = lineItemUrl.to;
+            }
+          }}
+        >
           <p>
             <strong>{product.title}</strong>
           </p>
-          <CartLinePrice line={line} as="span" />
-          <ul>
-            {selectedOptions.map((option) => (
-              <li key={option.name}>
-                <small>
-                  {option.name}: {option.value}
-                </small>
-              </li>
-            ))}
-          </ul>
-          <CartLineQuantity line={line} />
-        </div>
-      </Link>
+        </Link>
+        <CartLinePrice line={line} as="span" />
+        <ul>
+          {selectedOptions.map((option) => (
+            <li key={option.name}>
+              <small>
+                {option.name}: {option.value}
+              </small>
+            </li>
+          ))}
+        </ul>
+        <CartLineQuantity line={line} />
+      </div>
     </li>
   );
 }
