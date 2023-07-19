735dc08f7d140403eb31933307121334574a88ca
Fran Dios <frankdiox@gmail.com>
2023-07-19 15:24:41 +0200

    Improve Hydrogen errors logged in CLI (#1093)
    * Add h2 prefix to HR StorefrontClient errors and warnings
    
    * Replace errors and warnings with banners
    
    * Add log prefix to createStorefrontClient
    
    * Debounce logs
    
    * Add query and variables to GraphiQL
    
    * Use function name as the scope
    
    * Add prefix and graphql info to Storefront API errors
    
    * Show GraphiQL link on SFAPI errors
    
    * Add favicon to GraphiQL
    
    * Update to React 18 in GraphiQL
    
    * Update to GraphiQL v3
    
    * Automatically prettify query and variables in GraphiQL
    
    * Sanitize error stack and improve log format
    
    * Add log-type to prefix pattern
    
    * Changesets
    
    * Minor refactor
    
    * Unit test for h2 patterns

--- packages/cli/src/lib/log.ts
+++ packages/cli/src/lib/log.ts
@@ -1,5 +1,14 @@
 /* eslint-disable no-console */
 
+import {
+  renderInfo,
+  renderWarning,
+  renderFatalError,
+} from '@shopify/cli-kit/node/ui';
+import {BugError} from '@shopify/cli-kit/node/error';
+import {outputContent, outputToken} from '@shopify/cli-kit/node/output';
+import colors from '@shopify/cli-kit/node/colors';
+
 type ConsoleMethod = 'log' | 'warn' | 'error' | 'debug' | 'info';
 const originalConsole = {...console};
 const methodsReplaced = new Set<ConsoleMethod>();
@@ -8,6 +17,11 @@ type Matcher = (args: Array<any>) => boolean;
 type Replacer = (args: Array<any>) => void | string[];
 const addedReplacers = new Set<string>();
 const messageReplacers: Array<[Matcher, Replacer]> = [];
+
+export function resetAllLogs() {
+  Object.assign(console, originalConsole);
+  methodsReplaced.clear();
+}
 
 export function addMessageReplacers(
   key: string,
@@ -19,10 +33,35 @@ export function addMessageReplacers(
   }
 }
 
+const printedMessages = new Set<string | Object>();
+
+/**
+ * Certain messages like errors might be printed multiple times.
+ * This ensures they are only printed once per second.
+ */
+function debounceMessage(args: unknown[]) {
+  const key = args
+    .map((item) => {
+      const message = (item as Error)?.message ?? (item as string);
+      return typeof message === 'string' ? message : '';
+    })
+    .filter(Boolean)
+    .join('');
+
+  if (printedMessages.has(key)) return true;
+
+  printedMessages.add(key);
+  setTimeout(() => printedMessages.delete(key), 1000);
+
+  return false;
+}
+
 function injectLogReplacer(method: ConsoleMethod) {
   if (!methodsReplaced.has(method)) {
     methodsReplaced.add(method);
     console[method] = (...args: unknown[]) => {
+      if (debounceMessage(args)) return;
+
       const replacer = messageReplacers.find(([matcher]) => matcher(args))?.[1];
       if (!replacer) return originalConsole[method](...args);
 
@@ -32,6 +71,9 @@ function injectLogReplacer(method: ConsoleMethod) {
   }
 }
 
+/**
+ * Mute logs from Miniflare
+ */
 export function muteDevLogs({workerReload}: {workerReload?: boolean} = {}) {
   injectLogReplacer('log');
 
@@ -60,6 +102,9 @@ export function muteDevLogs({workerReload}: {workerReload?: boolean} = {}) {
 }
 
 const originalWrite = process.stdout.write;
+/**
+ * Modify logs from cli-kit related to authentication
+ */
 export function muteAuthLogs({
   onPressKey,
   onKeyTimeout,
@@ -120,6 +165,122 @@ export function muteAuthLogs({
   return () => {
     process.stdout.write = originalWrite;
   };
+}
+
+/**
+ * Modify logs from Hydrogen to use cli-kit banners
+ * Format: `[h2:type:scope] message`. Example: `[h2:error:storefront.query] Wrong query`
+ * Where the message can be multiline and the last line
+ * can contain links to docs or other resources.
+ */
+export function enhanceH2Logs(options: {
+  graphiqlUrl: string;
+  appDirectory: string;
+}) {
+  injectLogReplacer('warn');
+  injectLogReplacer('error');
+
+  addMessageReplacers('h2-warn', [
+    ([first]) => {
+      const message = first?.message ?? first;
+      return typeof message === 'string' && message.startsWith('[h2:');
+    },
+    (args: any[]) => {
+      const firstArg = args[0];
+      const errorObject =
+        typeof firstArg === 'object' && !!firstArg.stack
+          ? (firstArg as Error)
+          : undefined;
+
+      const stringArg = errorObject?.message ?? (firstArg as string);
+
+      const [, type, scope, message] =
+        stringArg.match(/^\[h2:([^:]+):([^\]]+)\]\s+(.*)$/ims) || [];
+
+      if (!type || !scope || !message) return args;
+
+      const headline = `In Hydrogen's \`${scope.trim()}\`:\n\n`;
+
+      const lines = message.split('\n');
+      const lastLine = lines.at(-1) ?? '';
+      const hasLinks = /https?:\/\//.test(lastLine);
+      if (hasLinks) lines.pop();
+
+      if (type === 'error' || errorObject) {
+        let tryMessage = hasLinks ? lastLine : undefined;
+        let stack = errorObject?.stack;
+        const cause = errorObject?.cause as
+          | {[key: string]: any; graphql?: {query: string; variables: string}}
+          | undefined;
+
+        if (!!cause?.graphql?.query) {
+          const {query, variables} = cause.graphql;
+          const link = `${options.graphiqlUrl}?query=${encodeURIComponent(
+            query,
+          )}${variables ? `&variables=${encodeURIComponent(variables)}` : ''}`;
+
+          const [, queryType, queryName] =
+            query.match(/(query|mutation)\s+(\w+)/) || [];
+
+          tryMessage =
+            (tryMessage ? `${tryMessage}\n\n` : '') +
+            outputContent`To debug the ${queryType || 'query'}${
+              queryName ? ` \`${colors.whiteBright(queryName)}\`` : ''
+            }, try it in ${outputToken.link(colors.bold('GraphiQL'), link)}.`
+              .value;
+
+          // Sanitize stack trace to only show app code
+          const stackLines = stack?.split('\n') ?? [];
+          const isAppLine = (line: string) =>
+            line.includes(options.appDirectory);
+          const firstAppLineIndex = stackLines.findIndex(isAppLine);
+          const lastAppLineIndex =
+            stackLines.length -
+            [...stackLines]
+              .reverse() // findLastIndex requires Node 18
+              .findIndex(isAppLine);
+
+          stack =
+            [
+              stackLines[0], // Error message
+              ...stackLines.slice(firstAppLineIndex, lastAppLineIndex), // App code
+            ]
+              .join('\n')
+              .trim() || undefined;
+        }
+
+        const error = new BugError(
+          headline +
+            colors.bold(lines.join('\n').replace(' - Request', '\nRequest')),
+          tryMessage,
+        );
+
+        error.cause = cause;
+        error.stack = stack;
+        renderFatalError(error);
+
+        return;
+      }
+
+      let reference: undefined | string[] = undefined;
+
+      if (hasLinks) {
+        reference = [];
+        for (const link of lastLine.matchAll(/https?:\/\/[^\s]+/g)) {
+          reference.push(link[0]);
+        }
+      }
+
+      const render = type === 'warn' ? renderWarning : renderInfo;
+
+      render({
+        body: headline + colors.bold(lines.join('\n')),
+        reference,
+      });
+
+      return;
+    },
+  ]);
 }
 
 const warnings = new Set<string>();
