1e73e7687589307fb8a1a19e8b3a6214d0a02a7a
Juan P. Prieto <juanpablo.prieto@shopify.com>
2023-07-25 07:10:12 -0700

    fix featured-collections image aspect-ratio

--- templates/skeleton/app/routes/_index.tsx
+++ templates/skeleton/app/routes/_index.tsx
@@ -43,8 +43,8 @@ function FeaturedCollection({
       to={`/collections/${collection.handle}`}
     >
       {image && (
-        <div>
-          <Image data={image} sizes="(min-width: 45em) 40vw, 100vw" />
+        <div className="featured-collection-image">
+          <Image data={image} />
         </div>
       )}
       <h1>{collection.title}</h1>
