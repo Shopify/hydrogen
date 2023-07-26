f73efa6d968566b11e49f404c09d7430f9aab403
Fran Dios <fran.dios@shopify.com>
2023-07-26 14:13:08 +0200

    Add g:c shortcut for generate:command

--- packages/cli/src/commands/hydrogen/g.ts
+++ packages/cli/src/commands/hydrogen/g.ts
@@ -1,7 +1,8 @@
 import Command from '@shopify/cli-kit/node/base-command';
 import GenerateRoute from './generate/route.js';
+import GenerateComponent from './generate/component.js';
 
-export default class GenerateRouteShortcut extends Command {
+export default class GenerateShortcut extends Command {
   static description =
     'Shortcut for `hydrogen generate`. See `hydrogen generate --help` for more information.';
 
@@ -14,6 +15,10 @@ export default class GenerateRouteShortcut extends Command {
 
     if (command === 'r' || command === 'route') {
       return new GenerateRoute(args, this.config).run();
+    }
+
+    if (command === 'c' || command === 'component') {
+      return new GenerateComponent(args, this.config).run();
     }
 
     throw new Error(`Invalid command argument "${command}".`);
