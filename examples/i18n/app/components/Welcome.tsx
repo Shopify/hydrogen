d50bc3b747de99ee77f8e4e28f397b5272278399
Juan P. Prieto <juanpablo.prieto@shopify.com>
2023-08-03 07:13:41 -0700

    add example Welcome comp

---
+++ examples/i18n/app/components/Welcome.tsx
@@ -0,0 +1,13 @@
+import {useTranslation} from 'react-i18next';
+
+export function Welcome() {
+  const {t} = useTranslation();
+  return (
+    <div>
+      <h1>{t('home.welcome.title')}</h1>
+      <h4>{t('home.welcome.subtitle')}</h4>
+      <br />
+      <p style={{maxWidth: 470}}>{t('home.welcome.description')}</p>
+    </div>
+  );
+}
