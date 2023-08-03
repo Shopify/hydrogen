7011f85d32b10a06d5647a8d6999b75e76f98605
Juan P. Prieto <juanpablo.prieto@shopify.com>
2023-08-03 07:17:06 -0700

    add i18n packages and base scaffold

---
+++ examples/i18n/remix.env.d.ts
@@ -0,0 +1,39 @@
+/// <reference types="@remix-run/dev" />
+/// <reference types="@shopify/remix-oxygen" />
+/// <reference types="@shopify/oxygen-workers-types" />
+/// <reference types="./@types/i18next.d.ts" />
+
+import type {Storefront, HydrogenCart} from '@shopify/hydrogen';
+import type {HydrogenSession} from './server';
+import type {i18nLocale} from './@types/i18next';
+
+declare global {
+  /**
+   * A global `process` object is only available during build to access NODE_ENV.
+   */
+  const process: {env: {NODE_ENV: 'production' | 'development'}};
+
+  /**
+   * Declare expected Env parameter in fetch handler.
+   */
+  interface Env {
+    SESSION_SECRET: string;
+    PUBLIC_STOREFRONT_API_TOKEN: string;
+    PRIVATE_STOREFRONT_API_TOKEN: string;
+    PUBLIC_STORE_DOMAIN: string;
+    PUBLIC_STOREFRONT_ID: string;
+  }
+}
+
+/**
+ * Declare local additions to `AppLoadContext` to include the session utilities we injected in `server.ts`.
+ */
+declare module '@shopify/remix-oxygen' {
+  export interface AppLoadContext {
+    cart: HydrogenCart;
+    env: Env;
+    session: HydrogenSession;
+    storefront: Storefront;
+    i18n: i18nLocale;
+  }
+}
