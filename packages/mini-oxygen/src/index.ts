f17bd49a6c653574e9a9da0048d800bf5a2c89ed
Fran Dios <fran.dios@shopify.com>
2023-08-04 16:40:26 +0200

    Expose MiniOxygenOptions

--- packages/mini-oxygen/src/index.ts
+++ packages/mini-oxygen/src/index.ts
@@ -1,1 +1,6 @@
-export {startServer} from './startServer';
+export type {Request, Response} from './mini-oxygen/server';
+export {
+  startServer,
+  createMiniOxygen,
+  type MiniOxygenOptions,
+} from './startServer.js';
