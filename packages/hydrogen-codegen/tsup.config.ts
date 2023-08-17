cb0d467c3f0f484192e4ba8be33abf3839d73be4
Fran Dios <frankdiox@gmail.com>
2023-08-17 11:19:12 +0900

    Fix build dependencies (#1229)
    * Import type from hydrogen-react
    
    * Remove cli/dist folder before building
    
    * Separate tsup configs
    
    * Copy hydrogen-react types in build config
    
    * Update to Tsup 7.2.0
    
    * Update Turbo
    
    * Fix turbo dev dependencies in monorepo
    
    * Stop serving demo-store in root dev
    
    * Update contributing guide

--- packages/hydrogen-codegen/tsup.config.ts
+++ packages/hydrogen-codegen/tsup.config.ts
@@ -1,6 +1,6 @@
+import path from 'node:path';
+import fs from 'node:fs/promises';
 import {defineConfig} from 'tsup';
-import fs from 'fs/promises';
-import path from 'path';
 
 const commonConfig = {
   minify: false,
