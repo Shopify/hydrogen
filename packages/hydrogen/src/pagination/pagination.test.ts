41440b624326b4642411ea5b37dd220a856dc7eb
Bret Little <bret.little@shopify.com>
2023-08-31 15:47:44 -0400

    Fix typescript

--- packages/hydrogen/src/pagination/pagination.test.ts
+++ packages/hydrogen/src/pagination/pagination.test.ts
@@ -103,7 +103,6 @@ describe('<Pagination>', () => {
       return function () {
         render(
           createElement(Pagination, {
-            // @ts-expect-error
             connection: {
               nodes: [1, 2, 3],
               pageInfo,
