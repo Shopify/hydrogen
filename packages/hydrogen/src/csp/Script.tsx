f65f5cf7e9a6b732cb9f485a0b70c2cebd800d87
Fran Dios <frankdiox@gmail.com>
2023-09-08 05:00:06 +0900

    Suppress Script hydration warning (#1312)
    * Suppress hydration warning in Script component

--- packages/hydrogen/src/csp/Script.tsx
+++ packages/hydrogen/src/csp/Script.tsx
@@ -6,6 +6,8 @@ type ScriptProps = JSX.IntrinsicElements['script'];
 export const Script = forwardRef<HTMLScriptElement, ScriptProps>(
   (props, ref) => {
     const nonce = useNonce();
-    return <script {...props} nonce={nonce} ref={ref} />;
+    return (
+      <script suppressHydrationWarning {...props} nonce={nonce} ref={ref} />
+    );
   },
 );
