75da2e6a81da57b4a4d782f978526faa64d6d53c
Fran Dios <fran.dios@shopify.com>
2023-09-14 15:28:16 +0900

    Cleanup

--- packages/cli/src/lib/transpile/file.ts
+++ packages/cli/src/lib/transpile/file.ts
@@ -6,6 +6,5 @@ export async function transpileFile(
   // This imports CJS dependencies. Do it dynamically
   // to avoid slowing down the CLI start-up time.
   const {transpileTs} = await import('./morph/index.js');
-  //   if (!filepath.includes('root.') && !filepath.includes('server.')) return '';
   return transpileTs(code, filepath, keepTypes);
 }
