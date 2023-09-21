64ddaf901bc68414021b5ec3997634fe0ab77d90
Fran Dios <fran.dios@shopify.com>
2023-09-21 12:49:19 +0900

    Remove future v2 flags

--- templates/hello-world/remix.config.js
+++ templates/hello-world/remix.config.js
@@ -16,12 +16,4 @@ module.exports = {
   serverModuleFormat: 'esm',
   serverPlatform: 'neutral',
   serverMinify: process.env.NODE_ENV === 'production',
-  future: {
-    v2_dev: true,
-    v2_meta: true,
-    v2_headers: true,
-    v2_errorBoundary: true,
-    v2_routeConvention: true,
-    v2_normalizeFormMethod: true,
-  },
 };
