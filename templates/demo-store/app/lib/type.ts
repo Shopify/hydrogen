e84cca8f3f3ddb8f4587322c8b0a3494d8f461e9
Helen Lin <helen.lin@shopify.com>
2023-10-19 15:18:54 -0700

    revert i18n fix

--- templates/demo-store/app/lib/type.ts
+++ templates/demo-store/app/lib/type.ts
@@ -1,14 +1,17 @@
+import type {Storefront as HydrogenStorefront} from '@shopify/hydrogen';
 import type {
-  Storefront as HydrogenStorefront,
-  I18nBase,
-} from '@shopify/hydrogen';
-import type {CurrencyCode} from '@shopify/hydrogen/storefront-api-types';
+  CountryCode,
+  CurrencyCode,
+  LanguageCode,
+} from '@shopify/hydrogen/storefront-api-types';
 
 export type NonNullableFields<T> = {
   [P in keyof T]: NonNullable<T[P]>;
 };
 
-export type Locale = I18nBase & {
+export type Locale = {
+  language: LanguageCode;
+  country: CountryCode;
   label: string;
   currency: CurrencyCode;
 };
