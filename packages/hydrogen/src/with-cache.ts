945c55aeb3b80f4b6c39d3527ec09c1bc6cd8477
Fran Dios <frankdiox@gmail.com>
2023-10-23 07:59:50 +0900

    Support `withCache` in debug-network for workerd (#1438)
    * Support withCache debug-network for workerd
    
    * Changesets

--- packages/hydrogen/src/with-cache.ts
+++ packages/hydrogen/src/with-cache.ts
@@ -1,12 +1,26 @@
 import {type CacheKey, runWithCache} from './cache/fetch';
 import type {CachingStrategy} from './cache/strategies';
 
+type CrossRuntimeRequest = {
+  headers: {
+    get?: (key: string) => string | null | undefined;
+    [key: string]: any;
+  };
+};
+
 type CreateWithCacheOptions = {
   /** An instance that implements the [Cache API](https://developer.mozilla.org/en-US/docs/Web/API/Cache) */
   cache: Cache;
   /** The `waitUntil` function is used to keep the current request/response lifecycle alive even after a response has been sent. It should be provided by your platform. */
   waitUntil: ExecutionContext['waitUntil'];
+  /** The `request` object is used to access certain headers for debugging */
+  request?: CrossRuntimeRequest;
 };
+
+function getHeader(key: string, request?: CrossRuntimeRequest) {
+  const value = request?.headers?.get?.(key) ?? request?.headers?.[key];
+  return typeof value === 'string' ? value : undefined;
+}
 
 /**
  * Creates a utility function that executes an asynchronous operation
@@ -15,10 +29,11 @@ type CreateWithCacheOptions = {
  * By default, it uses the `CacheShort` strategy.
  *
  */
-export function createWithCache<T = unknown>(
-  options: CreateWithCacheOptions,
-): CreateWithCacheReturn<T> {
-  const {cache, waitUntil} = options;
+export function createWithCache<T = unknown>({
+  cache,
+  waitUntil,
+  request,
+}: CreateWithCacheOptions): CreateWithCacheReturn<T> {
   return function withCache<T = unknown>(
     cacheKey: CacheKey,
     strategy: CachingStrategy,
@@ -28,7 +43,10 @@ export function createWithCache<T = unknown>(
       strategy,
       cacheInstance: cache,
       waitUntil,
-      debugInfo: {},
+      debugInfo: {
+        requestId: getHeader('request-id', request),
+        purpose: getHeader('purpose', request),
+      },
     });
   };
 }
