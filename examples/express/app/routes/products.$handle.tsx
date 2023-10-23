3c6982d21dc9f5c09873e913d30e0b8e92ca7c64
Fran Dios <fran.dios@shopify.com>
2023-10-23 10:41:42 +0900

    Add product page to express example to test

---
+++ examples/express/app/routes/products.$handle.tsx
@@ -0,0 +1,48 @@
+import {json, type LoaderArgs} from '@shopify/remix-oxygen';
+import {useLoaderData} from '@remix-run/react';
+
+export async function loader({params, context}: LoaderArgs) {
+  const {handle} = params;
+  const {storefront} = context;
+
+  if (!handle) {
+    throw new Error('Expected product handle to be defined');
+  }
+
+  const {product} = await storefront.query(
+    `#graphql
+    query Product( $handle: String!) {
+      product(handle: $handle) {
+        id
+        title
+        descriptionHtml
+      }
+    }`,
+    {variables: {handle}},
+  );
+
+  if (!product?.id) {
+    throw new Response(null, {status: 404});
+  }
+
+  return json({product});
+}
+
+export default function Product() {
+  const {
+    product: {title, descriptionHtml},
+  } = useLoaderData<typeof loader>();
+
+  return (
+    <div className="product">
+      <h1>{title}</h1>
+      <br />
+      <p>
+        <strong>Description</strong>
+      </p>
+      <br />
+      <div dangerouslySetInnerHTML={{__html: descriptionHtml}} />
+      <br />
+    </div>
+  );
+}
