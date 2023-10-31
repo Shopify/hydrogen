4f15fdfb63ed1e7445b0cfe90f94182b019e7954
Fran Dios <fran.dios@shopify.com>
2023-10-31 11:20:34 +0900

    Revert code changes

--- packages/cli/src/lib/codegen.ts
+++ packages/cli/src/lib/codegen.ts
@@ -17,7 +17,6 @@ if (isStandaloneProcess) {
     rootDirectory: process.argv[2]!,
     appDirectory: process.argv[3]!,
     configFilePath: process.argv[4],
-    isJSProject: process.argv[5] === 'js',
     watch: true,
   });
 }
@@ -100,7 +99,6 @@ type CodegenOptions = ProjectDirs & {
   watch?: boolean;
   configFilePath?: string;
   forceSfapiVersion?: string;
-  isJSProject?: boolean;
 };
 
 export async function codegen(options: CodegenOptions) {
@@ -167,13 +165,12 @@ async function generateTypes({
 async function generateDefaultConfig(
   {rootDirectory, appDirectory}: ProjectDirs,
   forceSfapiVersion?: string,
-  isJSProject?: boolean,
 ): Promise<LoadCodegenConfigResult> {
   const {schema, preset, pluckConfig} = await import(
     '@shopify/hydrogen-codegen'
   );
 
-  const defaultGlob = isJSProject ? '*!(*.d).{js,jsx}' : '*!(*.d).{ts,tsx}'; // No d.ts files
+  const defaultGlob = '*!(*.d).{ts,tsx}'; // No d.ts files
   const appDirRelative = relativePath(rootDirectory, appDirectory);
 
   return {
