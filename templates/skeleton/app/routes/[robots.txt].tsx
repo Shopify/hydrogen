a656c7d3bea21d99e2ba726f1953ed0333c156ce
Andrew Cohen <andrewcohen22@gmail.com>
2023-11-08 16:12:12 -0500

    Remove error boundary from robots.txt skeleton (#1492)
    * Remove error boundary from robots.txt skeleton
    
    * Add changeset

--- templates/skeleton/app/routes/[robots.txt].tsx
+++ templates/skeleton/app/routes/[robots.txt].tsx
@@ -20,33 +20,6 @@ export async function loader({request, context}: LoaderFunctionArgs) {
   });
 }
 
-export function ErrorBoundary() {
-  const error = useRouteError();
-
-  if (isRouteErrorResponse(error)) {
-    return (
-      <div>
-        <h1>Oops</h1>
-        <p>Status: {error.status}</p>
-        <p>{error.data.message}</p>
-      </div>
-    );
-  }
-
-  let errorMessage = 'Unknown error';
-  if (error instanceof Error) {
-    errorMessage = error.message;
-  }
-
-  return (
-    <div>
-      <h1>Uh oh ...</h1>
-      <p>Something went wrong.</p>
-      <pre>{errorMessage}</pre>
-    </div>
-  );
-}
-
 function robotsTxtData({url, shopId}: {shopId?: string; url?: string}) {
   const sitemapUrl = url ? `${url}/sitemap.xml` : undefined;
 
