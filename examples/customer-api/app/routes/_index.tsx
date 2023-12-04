c8e8f6fd233e52cf5570b1904af710d6b907aae5
Bret Little <bret.little@shopify.com>
2023-12-04 16:02:37 -0500

    Update the return types of the Customer Account API query (#1537)
    * Update the return types of the Customer Account API query and mutation methods. Also change the Customer Account API to default to 2024-01.
    
    * Warn when the customer account api version doesn't match the expected version
    
    Co-authored-by: Michelle Chen <michelle.chen@shopify.com>
    
    ---------
    
    Co-authored-by: Michelle Chen <michelle.chen@shopify.com>

--- examples/customer-api/app/routes/_index.tsx
+++ examples/customer-api/app/routes/_index.tsx
@@ -3,9 +3,9 @@ import {type LoaderFunctionArgs, json} from '@shopify/remix-oxygen';
 
 export async function loader({context}: LoaderFunctionArgs) {
   if (await context.customer.isLoggedIn()) {
-    const {customer} = await context.customer.query<{
+    const {data} = await context.customer.query<{
       customer: {firstName: string; lastName: string};
-    }>(`#graphql
+    }>(`#graphql:customer
       query getCustomer {
         customer {
           firstName
@@ -16,7 +16,7 @@ export async function loader({context}: LoaderFunctionArgs) {
 
     return json(
       {
-        customer,
+        customer: data.customer,
       },
       {
         headers: {
