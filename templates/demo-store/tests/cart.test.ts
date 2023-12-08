fc4e89dc095059a130fd59ab00f3dc92c2ccd6dc
Fran Dios <fran.dios@shopify.com>
2023-12-08 17:20:49 +0100

    Fix cart test

--- templates/demo-store/tests/cart.test.ts
+++ templates/demo-store/tests/cart.test.ts
@@ -11,10 +11,18 @@ test.describe('Cart', () => {
     await page.locator(`[data-test=product-grid] a  >> nth=0`).click();
     await page.locator(`[data-test=add-to-cart]`).click();
 
+    const firstItemPrice = await waitForLoaders(page, () =>
+      page.locator('[data-test=subtotal]').textContent(),
+    );
+
+    // Add an extra unit by increasing quantity
     await waitForLoaders(page, () =>
       page.locator(`button :text-is("+")`).click({clickCount: 1, delay: 600}),
     );
-
+    await expect(
+      page.locator('[data-test=subtotal]'),
+      'should double the price',
+    ).toContainText(usdFormatter.format(2 * normalizePrice(firstItemPrice)));
     await expect(
       page.locator('[data-test=item-quantity]'),
       'should increase quantity',
@@ -25,16 +33,19 @@ test.describe('Cart', () => {
     await page.locator(`header nav a:text-is("Products")`).click();
     await page.locator(`[data-test=product-grid] a  >> nth=0`).click();
 
+    // Add another unit by adding to cart the same item
     await waitForLoaders(page, () =>
       page.locator(`[data-test=add-to-cart]`).click(),
     );
-
+    await expect(
+      page.locator('[data-test=subtotal]'),
+      'should triple the price',
+    ).toContainText(formatPrice(3 * normalizePrice(firstItemPrice)));
     const quantities = await page
       .locator('[data-test=item-quantity]')
       .allTextContents();
-
     await expect(
-      quantities.reduce((a, b) => a + Number(b), 0),
+      quantities.reduce((a, b) => Number(a) + Number(b), 0),
       'should have the correct item quantities',
     ).toEqual(3);
 
@@ -45,11 +56,12 @@ test.describe('Cart', () => {
     await page.locator('a :text("Checkout")').click();
 
     await expect(page.url(), 'should navigate to checkout').toMatch(
-      /[\w\d-]+\.myshopify\.com\/\d+\/checkouts\/[\d\w]+/,
+      /checkout\.hydrogen\.shop\/checkouts\/[\d\w]+/,
     );
 
     const priceInCheckout = await page
-      .locator('[data-checkout-subtotal-price-target]')
+      .locator('[role=cell] > span')
+      .getByText(/^\$\d/)
       .textContent();
 
     await expect(
@@ -59,13 +71,24 @@ test.describe('Cart', () => {
   });
 });
 
+const usdFormatter = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD',
+});
+
+function formatPrice(price: string | number) {
+  return usdFormatter.format(Number(price));
+}
+
 function normalizePrice(price: string | null) {
   if (!price) throw new Error('Price was not found');
 
-  return price
-    .replace('$', '')
-    .trim()
-    .replace(/[.,](\d\d)$/, '-$1')
-    .replace(/[.,]/g, '')
-    .replace('-', '.');
+  return Number(
+    price
+      .replace('$', '')
+      .trim()
+      .replace(/[.,](\d\d)$/, '-$1')
+      .replace(/[.,]/g, '')
+      .replace('-', '.'),
+  );
 }
