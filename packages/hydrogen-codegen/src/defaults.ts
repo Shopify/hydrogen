a44f9c32345081ee42f2c3a6fb0c1441407ed264
Fran Dios <fran.dios@shopify.com>
2023-12-13 13:59:44 +0100

    Add customer account default values to codegen

---
+++ packages/hydrogen-codegen/src/defaults.ts
@@ -0,0 +1,32 @@
+import {
+  GENERATED_MUTATION_INTERFACE_NAME,
+  GENERATED_QUERY_INTERFACE_NAME,
+} from './plugin';
+
+const sfapiDefaultInterfaceExtensionCode = `
+declare module '@shopify/hydrogen' {
+  interface StorefrontQueries extends ${GENERATED_QUERY_INTERFACE_NAME} {}
+  interface StorefrontMutations extends ${GENERATED_MUTATION_INTERFACE_NAME} {}
+}`;
+
+const caapiDefaultInterfaceExtensionCode = `
+declare module '@shopify/hydrogen' {
+  interface CustomerAccountQueries extends ${GENERATED_QUERY_INTERFACE_NAME} {}
+  interface CustomerAccountMutations extends ${GENERATED_MUTATION_INTERFACE_NAME} {}
+}`;
+
+const defaultValues = {
+  sfapi: {
+    importTypesFrom: '@shopify/hydrogen/storefront-api-types',
+    namespacedImportName: 'StorefrontAPI',
+    interfaceExtensionCode: sfapiDefaultInterfaceExtensionCode,
+  },
+  caapi: {
+    importTypesFrom: '@shopify/hydrogen/caapi-types',
+    namespacedImportName: 'CustomerAccountAPI',
+    interfaceExtensionCode: caapiDefaultInterfaceExtensionCode,
+  },
+};
+
+export const getDefaultOptions = (target: keyof typeof defaultValues) =>
+  defaultValues[target];
