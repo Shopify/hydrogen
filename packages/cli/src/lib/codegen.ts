a14407d6b017c72d0fe8ec100817320100916650
Fran Dios <fran.dios@shopify.com>
2023-12-14 15:20:42 +0100

    Get codegen documents from user config if available

--- packages/cli/src/lib/codegen.ts
+++ packages/cli/src/lib/codegen.ts
@@ -2,11 +2,12 @@ import {spawn} from 'node:child_process';
 import {fileURLToPath} from 'node:url';
 import {formatCode, getCodeFormatOptions} from './format-code.js';
 import {renderFatalError, renderWarning} from '@shopify/cli-kit/node/ui';
-import {joinPath, relativePath} from '@shopify/cli-kit/node/path';
+import {joinPath, relativePath, basename} from '@shopify/cli-kit/node/path';
 import {AbortError} from '@shopify/cli-kit/node/error';
 
 // Do not import code synchronously from this dependency, it must be patched first
 import type {LoadCodegenConfigResult} from '@graphql-codegen/cli';
+import type {GraphQLConfig} from 'graphql-config';
 
 const nodePath = process.argv[1];
 const modulePath = fileURLToPath(import.meta.url);
@@ -167,9 +168,20 @@ async function generateDefaultConfig(
   {rootDirectory, appDirectory}: ProjectDirs,
   forceSfapiVersion?: string,
 ): Promise<LoadCodegenConfigResult> {
-  const {schema, preset, pluckConfig} = await import(
+  const {getSchema, preset, pluckConfig} = await import(
     '@shopify/hydrogen-codegen'
   );
+
+  const {loadConfig} = await import('graphql-config');
+  const gqlConfig = await loadConfig({
+    rootDir: rootDirectory,
+    throwOnEmpty: false,
+    throwOnMissing: false,
+    legacy: false,
+  }).catch(() => undefined);
+
+  const sfapiSchema = getSchema('storefront');
+  const sfapiProject = findGqlProject(sfapiSchema, gqlConfig);
 
   const defaultGlob = '*!(*.d).{ts,tsx,js,jsx}'; // No d.ts files
   const appDirRelative = relativePath(rootDirectory, appDirectory);
@@ -182,8 +194,8 @@ async function generateDefaultConfig(
       generates: {
         ['storefrontapi.generated.d.ts']: {
           preset,
-          schema,
-          documents: [
+          schema: sfapiSchema,
+          documents: sfapiProject?.documents ?? [
             defaultGlob, // E.g. ./server.(t|j)s
             joinPath(appDirRelative, '**', defaultGlob), // E.g. app/routes/_index.(t|j)sx
           ],
@@ -211,6 +223,17 @@ async function generateDefaultConfig(
       },
     },
   };
+}
+
+function findGqlProject(schemaFilepath: string, gqlConfig?: GraphQLConfig) {
+  if (!gqlConfig) return;
+
+  const schemaFilename = basename(schemaFilepath);
+  return Object.values(gqlConfig.projects || {}).find(
+    (project) =>
+      typeof project.schema === 'string' &&
+      project.schema.endsWith(schemaFilename),
+  ) as GraphQLConfig['projects'][number];
 }
 
 async function addHooksToHydrogenOptions(
