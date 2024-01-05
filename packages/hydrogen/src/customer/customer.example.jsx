94509b750afefd686971198ed86277e2c70f3176
Michelle Chen <michelle.chen@shopify.com>
2024-01-05 17:25:41 -0500

    ✨ Make customer client utility official & edit examples + docs (#1606)

--- packages/hydrogen/src/customer/customer.example.jsx
+++ packages/hydrogen/src/customer/customer.example.jsx
@@ -1,4 +1,4 @@
-import {createCustomerClient__unstable} from '@shopify/hydrogen';
+import {createCustomerClient} from '@shopify/hydrogen';
 import * as remixBuild from '@remix-run/dev/server-build';
 import {
   createRequestHandler,
@@ -10,7 +10,7 @@ export default {
     const session = await AppSession.init(request, [env.SESSION_SECRET]);
 
     /* Create a Customer API client with your credentials and options */
-    const customer = createCustomerClient__unstable({
+    const customer = createCustomerClient({
       /* Runtime utility in serverless environments */
       waitUntil: (p) => executionContext.waitUntil(p),
       /* Public Customer Account API token for your store */
