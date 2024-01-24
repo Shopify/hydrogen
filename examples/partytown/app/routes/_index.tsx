fe75741f8eedbb2fbb4dfa9ea69429313bf9981d
Juan P. Prieto <jp@calltheguys.co>
2024-01-24 15:58:31 -0800

    Initial

---
+++ examples/partytown/app/routes/_index.tsx
@@ -0,0 +1,24 @@
+import {useEffect} from 'react';
+import {useLocation} from '@remix-run/react';
+
+export default function Index() {
+  return (
+    <>
+      <h1>Partytown + Google GTM example</h1>
+      <TrackPageView />
+    </>
+  );
+}
+
+function TrackPageView() {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.gtag({
+      event: 'pageView',
+      data: {path: location.pathname, title: document.title},
+    });
+  }, []);
+
+  return <pre>pageView event tracked</pre>;
+}
