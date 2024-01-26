9313032d11bc75147bf081d05466f4fc310379b6
Michelle Chen <michelle.chen@shopify.com>
2024-01-26 10:48:57 -0500

    fix type

--- examples/legacy-customer-account-flow/app/routes/cart.tsx
+++ examples/legacy-customer-account-flow/app/routes/cart.tsx
@@ -1,6 +1,6 @@
 import {Await, type MetaFunction} from '@remix-run/react';
 import {Suspense} from 'react';
-import type {CartQueryData} from '@shopify/hydrogen';
+import type {CartQueryDataReturn} from '@shopify/hydrogen';
 import {CartForm} from '@shopify/hydrogen';
 import {json, type ActionFunctionArgs} from '@shopify/remix-oxygen';
 import {CartMain} from '~/components/Cart';
@@ -25,7 +25,7 @@ export async function action({request, context}: ActionFunctionArgs) {
   }
 
   let status = 200;
-  let result: CartQueryData;
+  let result: CartQueryDataReturn;
 
   switch (action) {
     case CartForm.ACTIONS.LinesAdd:
@@ -71,6 +71,8 @@ export async function action({request, context}: ActionFunctionArgs) {
     status = 303;
     headers.set('Location', redirectTo);
   }
+
+  headers.append('Set-Cookie', await context.session.commit());
 
   return json(
     {
