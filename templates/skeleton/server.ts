bb5c741352bdf4bb8a0b3de0e803f6b368be3df2
Graham F. Scott <gfscott@gmail.com>
2024-02-14 17:12:49 -0500

    Test on a file that's not .env

--- templates/skeleton/server.ts
+++ templates/skeleton/server.ts
@@ -39,19 +39,21 @@ export default {
         AppSession.init(request, [env.SESSION_SECRET]),
       ]);
 
+      // [ START two-col-test ]
       /**
        * Create Hydrogen's Storefront client.
-       */
-      const {storefront} = createStorefrontClient({
-        cache,
-        waitUntil,
-        i18n: {language: 'EN', country: 'US'},
-        publicStorefrontToken: env.PUBLIC_STOREFRONT_API_TOKEN,
-        privateStorefrontToken: env.PRIVATE_STOREFRONT_API_TOKEN,
-        storeDomain: env.PUBLIC_STORE_DOMAIN,
-        storefrontId: env.PUBLIC_STOREFRONT_ID,
-        storefrontHeaders: getStorefrontHeaders(request),
+      */
+     const {storefront} = createStorefrontClient({
+       cache,
+       waitUntil,
+       i18n: {language: 'EN', country: 'US'},
+       publicStorefrontToken: env.PUBLIC_STOREFRONT_API_TOKEN,
+       privateStorefrontToken: env.PRIVATE_STOREFRONT_API_TOKEN,
+       storeDomain: env.PUBLIC_STORE_DOMAIN,
+       storefrontId: env.PUBLIC_STOREFRONT_ID,
+       storefrontHeaders: getStorefrontHeaders(request),
       });
+      // [ END two-col-test ]
 
       /**
        * Create a client for Customer Account API.
