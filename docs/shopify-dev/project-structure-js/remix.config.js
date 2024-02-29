f1d79d4f591e5b8c097c44c741039e1656a3c548
Graham F. Scott <gfscott@gmail.com>
2024-02-29 15:29:09 -0500

    prettier

--- docs/shopify-dev/project-structure-js/remix.config.js
+++ docs/shopify-dev/project-structure-js/remix.config.js
@@ -9,20 +9,20 @@ module.exports = {
   // [START project-structure.config-hydrogen]
   /**
    * The following settings are required to deploy Hydrogen apps to Oxygen:
-  */
+   */
   publicPath: (process.env.HYDROGEN_ASSET_BASE_URL ?? '/') + 'build/',
- assetsBuildDirectory: 'dist/client/build',
- serverBuildPath: 'dist/worker/index.js',
- serverMainFields: ['browser', 'module', 'main'],
- serverConditions: ['worker', process.env.NODE_ENV],
- serverDependenciesToBundle: 'all',
- serverModuleFormat: 'esm',
- serverPlatform: 'neutral',
- serverMinify: process.env.NODE_ENV === 'production',
- future: {
-   v3_fetcherPersist: true,
-   v3_relativeSplatpath: true,
-   v3_throwAbortReason: true,
+  assetsBuildDirectory: 'dist/client/build',
+  serverBuildPath: 'dist/worker/index.js',
+  serverMainFields: ['browser', 'module', 'main'],
+  serverConditions: ['worker', process.env.NODE_ENV],
+  serverDependenciesToBundle: 'all',
+  serverModuleFormat: 'esm',
+  serverPlatform: 'neutral',
+  serverMinify: process.env.NODE_ENV === 'production',
+  future: {
+    v3_fetcherPersist: true,
+    v3_relativeSplatpath: true,
+    v3_throwAbortReason: true,
   },
   // [END project-structure.config-hydrogen]
 };
