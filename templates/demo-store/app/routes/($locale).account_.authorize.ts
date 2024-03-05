8e874f18650213cadddccc4f407ab8aab9d02ab6
Bret Little <bret.little@shopify.com>
2024-03-05 11:58:56 -0500

    Move the demo-store to https://github.com/Shopify/hydrogen-demo-store (#1769)
    Also add deployments for the skeleton template and examples.

--- templates/demo-store/app/routes/($locale).account_.authorize.ts
+++
@@ -1,5 +0,0 @@
-import type {LoaderFunctionArgs} from '@shopify/remix-oxygen';
-
-export async function loader({context, params}: LoaderFunctionArgs) {
-  return context.customerAccount.authorize();
-}
