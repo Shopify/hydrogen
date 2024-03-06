6ec748a9b0d49a2207ceb4a7c78aef2933a78182
Fran Dios <frankdiox@gmail.com>
2024-03-06 20:26:50 +0900

    Extract codegen into `@shopify/graphql-codegen` (#1799)
    * Refactor package to use extracted logic
    
    * Changesets
    
    * Update to public dep
    
    * Smaller workaround for dts problem
    
    * Fix dts bundling

--- packages/hydrogen-codegen/tsup.config.ts
+++ packages/hydrogen-codegen/tsup.config.ts
@@ -1,6 +1,7 @@
 import path from 'node:path';
 import fs from 'node:fs/promises';
 import {defineConfig} from 'tsup';
+import {generateDtsBundle} from 'dts-bundle-generator';
 
 const commonConfig = {
   minify: false,
@@ -14,16 +15,39 @@ export default defineConfig([
   {
     ...commonConfig,
     format: 'esm',
-    // Force bundling types in files here so that they can later be
-    // bundled in Hydrogen. Otherwise, TSUP fails to bundle them later.
-    dts: {entry: ['src/index.ts', 'src/patch.ts']},
     entry: ['src/**/*.ts'],
     outDir: 'dist/esm',
+    // TSUP does not bundle types properly for this package,
+    // use dts-bundle-generator instead.
+    dts: false,
     async onSuccess() {
       const schemaFile = 'dist/esm/schema.js';
       const content = await fs.readFile(schemaFile, 'utf8');
       // Uncomment createRequire for ESM:
       await fs.writeFile(schemaFile, content.replace(/\/\/!/g, ''));
+
+      const [dts] = await generateDtsBundle([
+        {
+          filePath: './src/index.ts',
+          libraries: {
+            // Bundle types from these libraries to avoid
+            // the need to install them as dependencies in consumers.
+            // Specifically, 'type-fest' might be installed in a consumer
+            // with an older version that doesn't include `IsNever` or
+            // a similar type.
+            inlinedLibraries: ['@shopify/graphql-codegen', 'type-fest'],
+          },
+          output: {noBanner: true},
+        },
+      ]);
+
+      await fs.writeFile(
+        'dist/esm/index.d.ts',
+        // For some reason, dts-bundle-generator exports PresetConfig
+        // from @shopify/graphql-codegen, which conflicts with the
+        // overwritten type in src/preset.ts. This is a workaround.
+        dts.replace('export type PresetConfig', 'type PresetConfig'),
+      );
     },
   },
   {
