3af3078892ce9d3e0443a42f030a52e8c24f0a5e
Fran Dios <fran.dios@shopify.com>
2024-03-26 18:13:17 +0100

    Setup Vite in skeleton

---
+++ templates/skeleton/vite.config.ts
@@ -0,0 +1,20 @@
+import {defineConfig} from 'vite';
+import {hydrogen, oxygen} from '@shopify/cli-hydrogen/experimental-vite';
+import {vitePlugin as remix} from '@remix-run/dev';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+export default defineConfig({
+  plugins: [
+    hydrogen(),
+    oxygen(),
+    remix({
+      buildDirectory: 'dist',
+      future: {
+        v3_fetcherPersist: true,
+        v3_relativeSplatPath: true,
+        v3_throwAbortReason: true,
+      },
+    }),
+    tsconfigPaths(),
+  ],
+});
