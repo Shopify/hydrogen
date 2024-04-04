aa989d4ac6acd67ee2df22fd48e4b5db9f7137bf
Fran Dios <fran.dios@shopify.com>
2024-04-04 14:18:16 +0200

    Update projects to new plugin paths

--- templates/skeleton/vite.config.ts
+++ templates/skeleton/vite.config.ts
@@ -1,5 +1,6 @@
 import {defineConfig} from 'vite';
-import {hydrogen, oxygen} from '@shopify/cli-hydrogen/experimental-vite';
+import {hydrogen} from '@shopify/hydrogen/vite';
+import {oxygen} from '@shopify/mini-oxygen/vite';
 import {vitePlugin as remix} from '@remix-run/dev';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
