2178b90b7733f5105e6c4aebbd94abf03b4d0589
Graham F. Scott <gfscott@gmail.com>
2024-05-01 13:23:12 -0400

    Add code examples for 2 column Analytics tutorial

---
+++ docs/shopify-dev/analytics/js/app/routes/cart.jsx
@@ -0,0 +1,112 @@
+import {Await} from '@remix-run/react';
+import {Suspense} from 'react';
+import {CartForm} from '@shopify/hydrogen';
+import {json} from '@shopify/remix-oxygen';
+import {CartMain} from '~/components/Cart';
+import {useRootLoaderData} from '~/lib/root-data';
+
+/**
+ * @type {MetaFunction}
+ */
+export const meta = () => {
+  return [{title: `Hydrogen | Cart`}];
+};
+
+/**
+ * @param {ActionFunctionArgs}
+ */
+export async function action({request, context}) {
+  const {cart} = context;
+
+  const formData = await request.formData();
+
+  const {action, inputs} = CartForm.getFormInput(formData);
+
+  if (!action) {
+    throw new Error('No action provided');
+  }
+
+  let status = 200;
+  let result;
+
+  switch (action) {
+    case CartForm.ACTIONS.LinesAdd:
+      result = await cart.addLines(inputs.lines);
+      break;
+    case CartForm.ACTIONS.LinesUpdate:
+      result = await cart.updateLines(inputs.lines);
+      break;
+    case CartForm.ACTIONS.LinesRemove:
+      result = await cart.removeLines(inputs.lineIds);
+      break;
+    case CartForm.ACTIONS.DiscountCodesUpdate: {
+      const formDiscountCode = inputs.discountCode;
+
+      // User inputted discount code
+      const discountCodes = formDiscountCode ? [formDiscountCode] : [];
+
+      // Combine discount codes already applied on cart
+      discountCodes.push(...inputs.discountCodes);
+
+      result = await cart.updateDiscountCodes(discountCodes);
+      break;
+    }
+    case CartForm.ACTIONS.BuyerIdentityUpdate: {
+      result = await cart.updateBuyerIdentity({
+        ...inputs.buyerIdentity,
+      });
+      break;
+    }
+    default:
+      throw new Error(`${action} cart action is not defined`);
+  }
+
+  const cartId = result.cart.id;
+  const headers = cart.setCartId(result.cart.id);
+  const {cart: cartResult, errors} = result;
+
+  const redirectTo = formData.get('redirectTo') ?? null;
+  if (typeof redirectTo === 'string') {
+    status = 303;
+    headers.set('Location', redirectTo);
+  }
+
+  headers.append('Set-Cookie', await context.session.commit());
+
+  return json(
+    {
+      cart: cartResult,
+      errors,
+      analytics: {
+        cartId,
+      },
+    },
+    {status, headers},
+  );
+}
+
+export default function Cart() {
+  const rootData = useRootLoaderData();
+  const cartPromise = rootData.cart;
+
+  return (
+    <div className="cart">
+      <h1>Cart</h1>
+      <Suspense fallback={<p>Loading cart ...</p>}>
+        <Await
+          resolve={cartPromise}
+          errorElement={<div>An error occurred</div>}
+        >
+          {(cart) => {
+            return <CartMain layout="page" cart={cart} />;
+          }}
+        </Await>
+      </Suspense>
+    </div>
+  );
+}
+
+/** @template T @typedef {import('@remix-run/react').MetaFunction<T>} MetaFunction */
+/** @typedef {import('@shopify/hydrogen').CartQueryDataReturn} CartQueryDataReturn */
+/** @typedef {import('@shopify/remix-oxygen').ActionFunctionArgs} ActionFunctionArgs */
+/** @typedef {import('@shopify/remix-oxygen').SerializeFrom<typeof action>} ActionReturnData */
