b19043640eb588f85210b7420a515230de6cd8dd
Michelle Chen <michelle.chen@shopify.com>
2024-05-07 11:17:25 -0400

    sync server file with skeleton more closetly

--- examples/b2b/server.ts
+++ examples/b2b/server.ts
@@ -17,7 +17,6 @@ import {
 import {AppSession} from '~/lib/session';
 import {CART_QUERY_FRAGMENT} from '~/lib/fragments';
 
-
 /**
  * Export a fetch handler in module format.
  */
@@ -42,6 +41,20 @@ export default {
       ]);
 
       /**
+       * Create Hydrogen's Storefront client.
+       */
+      const {storefront} = createStorefrontClient({
+        cache,
+        waitUntil,
+        i18n: {language: 'EN', country: 'US'},
+        publicStorefrontToken: env.PUBLIC_STOREFRONT_API_TOKEN,
+        privateStorefrontToken: env.PRIVATE_STOREFRONT_API_TOKEN,
+        storeDomain: env.PUBLIC_STORE_DOMAIN,
+        storefrontId: env.PUBLIC_STOREFRONT_ID,
+        storefrontHeaders: getStorefrontHeaders(request),
+      });
+
+      /**
        * Create a client for Customer Account API.
        */
       const customerAccount = createCustomerAccountClient({
@@ -55,20 +68,6 @@ export default {
         unstableB2b: true,
         /**********   EXAMPLE UPDATE END   ************/
         /***********************************************/
-      });
-
-      /**
-       * Create Hydrogen's Storefront client.
-       */
-      const {storefront} = createStorefrontClient({
-        cache,
-        waitUntil,
-        i18n: {language: 'EN', country: 'US'},
-        publicStorefrontToken: env.PUBLIC_STOREFRONT_API_TOKEN,
-        privateStorefrontToken: env.PRIVATE_STOREFRONT_API_TOKEN,
-        storeDomain: env.PUBLIC_STORE_DOMAIN,
-        storefrontId: env.PUBLIC_STOREFRONT_ID,
-        storefrontHeaders: getStorefrontHeaders(request),
       });
 
       /*
