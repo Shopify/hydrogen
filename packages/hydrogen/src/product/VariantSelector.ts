fd843403e8693b48698ac6d44b276955c067be07
Juan P. Prieto <jp@calltheguys.co>
2024-05-08 08:54:39 -0700

    Cleanup `getSelectedProductOptions` implementation and add JSDocs  (#2089)
    * Cleanup getSelectedProduct options implmentation
    
    * Add JSDocs to getSelectedProductOptions
    
    * Add changeset

--- packages/hydrogen/src/product/VariantSelector.ts
+++ packages/hydrogen/src/product/VariantSelector.ts
@@ -133,6 +133,26 @@ export function VariantSelector({
 
 type GetSelectedProductOptions = (request: Request) => SelectedOptionInput[];
 
+/**
+ * Extract searchParams from a Request instance and return an array of selected options.
+ * @param request - The Request instance to extract searchParams from.
+ * @returns An array of selected options.
+ * @example Basic usage:
+ * ```tsx
+ *
+ * import {getSelectedProductOptions} from '@shopify/hydrogen';
+ *
+ * // Given a request url of `/products/product-handle?color=red&size=large`
+ *
+ * const selectedOptions = getSelectedProductOptions(request);
+ *
+ * // selectedOptions will equal:
+ * // [
+ * //   {name: 'color', value: 'red'},
+ * //   {name: 'size', value: 'large'}
+ * // ]
+ * ```
+ **/
 export const getSelectedProductOptions: GetSelectedProductOptions = (
   request,
 ) => {
