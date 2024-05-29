64a0203c60632e83500816fedf1671b4b4c741d3
Fran Dios <frankdiox@gmail.com>
2024-05-29 13:24:46 +0900

    Update vite setup asset (#2160)

--- packages/cli/src/setup-assets/vite/vite.config.js
+++ packages/cli/src/setup-assets/vite/vite.config.js
@@ -23,4 +23,19 @@ export default defineConfig({
     // withtout inlining assets as base64:
     assetsInlineLimit: 0,
   },
+  ssr: {
+    optimizeDeps: {
+      /**
+       * Include dependencies here if they throw CJS<>ESM errors.
+       * For example, for the following error:
+       *
+       * > ReferenceError: module is not defined
+       * >   at /Users/.../node_modules/example-dep/index.js:1:1
+       *
+       * Include 'example-dep' in the array below.
+       * @see https://vitejs.dev/config/dep-optimization-options
+       */
+      include: [],
+    },
+  },
 });
