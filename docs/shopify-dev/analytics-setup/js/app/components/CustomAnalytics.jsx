e633f068d06ac5a40a7dd8f880ca9960571d761d
Helen Lin <helen.lin@shopify.com>
2024-05-30 10:12:51 -0700

    Example code for shopify-dev docs (#2164)
    Co-authored-by: Graham F. Scott <gfscott@gmail.com>

---
+++ docs/shopify-dev/analytics-setup/js/app/components/CustomAnalytics.jsx
@@ -0,0 +1,41 @@
+// [START import]
+import {useAnalytics} from '@shopify/hydrogen';
+// [END import]
+import {useEffect} from 'react';
+
+// [START export]
+export function CustomAnalytics() {
+  // [START use]
+  const {subscribe} = useAnalytics();
+  // [END use]
+
+  useEffect(() => {
+    // [START subscribe]
+    // Standard events
+    subscribe('page_viewed', (data) => {
+      console.log('CustomAnalytics - Page viewed:', data);
+    });
+    subscribe('product_viewed', (data) => {
+      console.log('CustomAnalytics - Product viewed:', data);
+    });
+    subscribe('collection_viewed', (data) => {
+      console.log('CustomAnalytics - Collection viewed:', data);
+    });
+    subscribe('cart_viewed', (data) => {
+      console.log('CustomAnalytics - Cart viewed:', data);
+    });
+    subscribe('cart_updated', (data) => {
+      console.log('CustomAnalytics - Cart updated:', data);
+    });
+
+    // Custom events
+    subscribe('custom_sidecart_viewed', (data) => {
+      console.log('CustomAnalytics - Custom sidecart opened:', data);
+    });
+    // [END subscribe]
+
+  }, []);
+
+  return null;
+}
+// [END export]
