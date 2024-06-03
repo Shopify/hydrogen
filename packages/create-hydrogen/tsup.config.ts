2f48ca4f5129067c5510371aeb15a41d1a2d6d48
Fran Dios <fran.dios@shopify.com>
2024-06-03 19:01:43 +0900

    Fix bundled code and copy assets

--- packages/create-hydrogen/tsup.config.ts
+++ packages/create-hydrogen/tsup.config.ts
@@ -1,17 +1,38 @@
+import {createRequire} from 'node:module';
+import fs from 'node:fs/promises';
 import {defineConfig} from 'tsup';
 
 export default defineConfig({
   entry: ['src/create-app.ts'],
   outDir: 'dist',
   format: 'esm',
-  outExtension: () => ({js: '.mjs'}),
   clean: true,
-  bundle: true,
-  minify: true,
-  splitting: false,
   sourcemap: false,
   dts: false,
-  // 'react-devtools-core' is not used but breaks the build otherwise:
-  external: ['react-devtools-core', '@ast-grep/napi', '@parcel/watcher'],
+  minify: true,
+  splitting: true, // Async/await breaks without splitting
+
+  // -- Bundle:
+  bundle: true,
+  external: [
+    '@ast-grep/napi', // Required binary
+    '@parcel/watcher', // Not used but but needs to resolve it
+    'react-devtools-core', // Not used but breaks the build otherwise
+  ],
   noExternal: ['@shopify/cli-hydrogen'],
+  // Needed for some CJS dependencies:
+  shims: true,
+  banner: {
+    js: "import { createRequire as __createRequire } from 'module';globalThis.require = __createRequire(import.meta.url);",
+  },
+  async onSuccess() {
+    // Copy assets to the dist folder
+    await fs.cp('../cli/dist/assets', './dist/assets', {recursive: true});
+
+    // This WASM file is used in a dependency, copy it over:
+    await fs.copyFile(
+      createRequire(import.meta.url).resolve('yoga-wasm-web/dist/yoga.wasm'),
+      './dist/yoga.wasm',
+    );
+  },
 });
