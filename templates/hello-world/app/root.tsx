f86128c102d77392635526ef050a94720fc072a5
Michelle Chen <michelle.chen@shopify.com>
2024-06-04 10:33:55 -0400

    Move hello-world template to use vite (#2188)
    * move hello-world template to use vite
    
    * Fix package-lock
    
    ---------
    
    Co-authored-by: Fran Dios <fran.dios@shopify.com>

--- templates/hello-world/app/root.tsx
+++ templates/hello-world/app/root.tsx
@@ -7,13 +7,12 @@ import {
   Meta,
   Outlet,
   Scripts,
-  LiveReload,
   ScrollRestoration,
   useRouteLoaderData,
   type ShouldRevalidateFunction,
 } from '@remix-run/react';
 import type {Shop} from '@shopify/hydrogen/storefront-api-types';
-import appStyles from './styles/app.css';
+import appStyles from './styles/app.css?url';
 import favicon from '../public/favicon.svg';
 import {useNonce} from '@shopify/hydrogen';
 
@@ -82,7 +81,6 @@ export function Layout({children}: {children?: React.ReactNode}) {
         )}
         <ScrollRestoration nonce={nonce} />
         <Scripts nonce={nonce} />
-        <LiveReload nonce={nonce} />
       </body>
     </html>
   );
