6bffcd462982a7c169f38721d00112b91a522d0e
Bret Little <bret.little@shopify.com>
2024-06-05 13:11:45 -0400

    Split the skeleton template loaders to separate primary and secondary data functions (#2130)
    * Split the skeleton template loaders to separate primary and secondary data functions
    
    The goal here is to make the template more self documenting:
    
    1. Force people to consider up front what to defer and what not to
    2. Default to a `Promise.all(`) and a comment within queries to encourage parallel requests
    3. The `secondaryData` function is _not_ async, so people can't await.
    
    * Udpate
    
    * Fix
    
    * Update
    
    * Update examples
    
    * Fixes
    
    * Update to catch errors in the deferred loader
    
    * Fix types
    
    * Add session commit back

--- examples/multipass/app/routes/blogs.$blogHandle._index.tsx
+++ examples/multipass/app/routes/blogs.$blogHandle._index.tsx
@@ -1,4 +1,4 @@
-import {json, type LoaderFunctionArgs} from '@shopify/remix-oxygen';
+import {defer, type LoaderFunctionArgs} from '@shopify/remix-oxygen';
 import {Link, useLoaderData, type MetaFunction} from '@remix-run/react';
 import {Image, Pagination, getPaginationVariables} from '@shopify/hydrogen';
 import type {ArticleItemFragment} from 'storefrontapi.generated';
@@ -7,11 +7,25 @@ export const meta: MetaFunction<typeof loader> = ({data}) => {
   return [{title: `Hydrogen | ${data?.blog.title ?? ''} blog`}];
 };
 
-export const loader = async ({
+export async function loader(args: LoaderFunctionArgs) {
+  // Start fetching non-critical data without blocking time to first byte
+  const deferredData = loadDeferredData(args);
+
+  // Await the critical data required to render initial state of the page
+  const criticalData = await loadCriticalData(args);
+
+  return defer({...deferredData, ...criticalData});
+}
+
+/**
+ * Load data necessary for rendering content above the fold. This is the critical data
+ * needed to render the page. If it's unavailable, the whole page should 400 or 500 error.
+ */
+async function loadCriticalData({
+  context,
   request,
   params,
-  context: {storefront},
-}: LoaderFunctionArgs) => {
+}: LoaderFunctionArgs) {
   const paginationVariables = getPaginationVariables(request, {
     pageBy: 4,
   });
@@ -20,19 +34,31 @@ export const loader = async ({
     throw new Response(`blog not found`, {status: 404});
   }
 
-  const {blog} = await storefront.query(BLOGS_QUERY, {
-    variables: {
-      blogHandle: params.blogHandle,
-      ...paginationVariables,
-    },
-  });
+  const [{blog}] = await Promise.all([
+    context.storefront.query(BLOGS_QUERY, {
+      variables: {
+        blogHandle: params.blogHandle,
+        ...paginationVariables,
+      },
+    }),
+    // Add other queries here, so that they are loaded in parallel
+  ]);
 
   if (!blog?.articles) {
     throw new Response('Not found', {status: 404});
   }
 
-  return json({blog});
-};
+  return {blog};
+}
+
+/**
+ * Load data for rendering content below the fold. This data is deferred and will be
+ * fetched after the initial page load. If it's unavailable, the page should still 200.
+ * Make sure to not throw any errors here, as it will cause the page to 500.
+ */
+function loadDeferredData({context}: LoaderFunctionArgs) {
+  return {};
+}
 
 export default function Blog() {
   const {blog} = useLoaderData<typeof loader>();
