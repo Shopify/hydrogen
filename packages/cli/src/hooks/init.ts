fe31d5007f1c7fde79f1eee71cdbc28b3c494aae
Fran Dios <fran.dios@shopify.com>
2024-06-06 19:49:08 +0900

    Check if directory is a hydrogen project

--- packages/cli/src/hooks/init.ts
+++ packages/cli/src/hooks/init.ts
@@ -1,8 +1,49 @@
+import {createRequire} from 'node:module';
 import {spawnSync} from 'node:child_process';
-import {outputDebug} from '@shopify/cli-kit/node/output';
 import type {Hook} from '@oclif/core';
+import {outputDebug, outputNewline} from '@shopify/cli-kit/node/output';
+import {cwd, joinPath, resolvePath} from '@shopify/cli-kit/node/path';
+import {renderWarning} from '@shopify/cli-kit/node/ui';
 
 const hook: Hook<'init'> = async function (options) {
+  if (options.id === 'hydrogen:init') return;
+
+  let projectPath = cwd();
+  const pathFlagRE = /^--path($|=)/;
+  const pathFlagIndex = options.argv.findIndex((arg) => pathFlagRE.test(arg));
+
+  if (pathFlagIndex !== -1) {
+    const pathFlagValue =
+      options.argv[pathFlagIndex]?.split('=')[1] ??
+      options.argv[pathFlagIndex + 1];
+
+    if (pathFlagValue && !pathFlagValue.startsWith('--')) {
+      projectPath = resolvePath(projectPath, pathFlagValue);
+    }
+  }
+
+  if (!isHydrogenProject(projectPath)) {
+    outputNewline();
+    renderWarning({
+      headline: `Looks like you're trying to run a Hydrogen command outside of a Hydrogen project.`,
+      body: [
+        'Run',
+        {command: 'shopify hydrogen init'},
+        'to create a new Hydrogen project or use the',
+        {command: '--path'},
+        'flag to specify an existing Hydrogen project.\n\n',
+        {subdued: projectPath},
+      ],
+      reference: [
+        'Getting started: https://shopify.dev/docs/storefronts/headless/hydrogen',
+        'CLI commands: https://shopify.dev/docs/api/shopify-cli/hydrogen',
+      ],
+    });
+
+    // Throwing errors here does not end the process:
+    process.exit(1);
+  }
+
   if (
     commandNeedsVM(options.id, options.argv) &&
     !process.execArgv.includes(EXPERIMENTAL_VM_MODULES_FLAG) &&
@@ -31,6 +72,20 @@ function commandNeedsVM(id = '', argv: string[] = []) {
     (['hydrogen:dev', 'hydrogen:preview'].includes(id) &&
       argv.includes('--legacy-runtime'))
   );
+}
+
+function isHydrogenProject(projectPath: string) {
+  try {
+    const require = createRequire(import.meta.url);
+    const {dependencies} = require(joinPath(projectPath, 'package.json'));
+    return (
+      !!dependencies['@shopify/hydrogen'] ||
+      // Diff examples only have this package as a dependency
+      !!dependencies['@shopify/cli-hydrogen']
+    );
+  } catch {
+    return false;
+  }
 }
 
 export default hook;
