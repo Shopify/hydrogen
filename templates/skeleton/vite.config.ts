96f816fc649be0cc5128d9ed84e17320a8f3a1bc
Fran Dios <fran.dios@shopify.com>
2024-06-10 15:23:51 +0900

    Make things in parallel for 30% perf

--- templates/skeleton/vite.config.ts
+++ templates/skeleton/vite.config.ts
@@ -66,6 +66,8 @@ function hydrogenBundleAnalyzer() {
     async generateBundle(options, bundle) {
       if (!config.build.ssr) return;
 
+      const t1 = Date.now();
+
       const {root} = config;
 
       const workerFile = Object.values(bundle).find(
@@ -81,93 +83,88 @@ function hydrogenBundleAnalyzer() {
       }
 
       const renderedSizes = new Map<string, number>();
-      const toAnalyzeMods = new Set(Object.keys(workerFile.modules));
       const modsMeta = new Map<string, any>();
 
-      for (const modId of toAnalyzeMods) {
-        if (isViteHelper(modId)) {
-          continue;
-        }
+      await Promise.all(
+        Object.keys(workerFile.modules).map(async (modId) => {
+          if (isViteHelper(modId)) return;
 
-        const mod = this.getModuleInfo(modId);
-        if (!mod?.id) continue;
+          const mod = this.getModuleInfo(modId);
+          if (!mod?.id) return;
 
-        const modBundleInfo = workerFile.modules[mod.id];
-        const originalCodeBytes =
-          modBundleInfo?.originalLength ?? mod.code?.length ?? 0;
+          const modBundleInfo = workerFile.modules[mod.id];
+          const originalCodeBytes =
+            modBundleInfo?.originalLength ?? mod.code?.length ?? 0;
 
-        let resultingCodeBytes = modBundleInfo?.renderedLength ?? 0;
+          let resultingCodeBytes = modBundleInfo?.renderedLength ?? 0;
 
-        if (config?.build.minify && modBundleInfo?.code) {
-          const result = await transformWithEsbuild(
-            modBundleInfo.code,
-            mod.id,
-            {
-              minify: true,
-              minifyWhitespace: true,
-              minifySyntax: true,
-              minifyIdentifiers: true,
-              sourcemap: false,
-              treeShaking: false, // Tree-shaking would drop most exports in routes
-              legalComments: 'none',
-              target: 'esnext',
-            },
-          ).catch(() => null);
+          if (config?.build.minify && modBundleInfo?.code) {
+            const result = await transformWithEsbuild(
+              modBundleInfo.code,
+              mod.id,
+              {
+                minify: true,
+                minifyWhitespace: true,
+                minifySyntax: true,
+                minifyIdentifiers: true,
+                sourcemap: false,
+                treeShaking: false, // Tree-shaking would drop most exports in routes
+                legalComments: 'none',
+                target: 'esnext',
+              },
+            ).catch(() => null);
 
-          if (result) resultingCodeBytes = result.code.length;
-        }
-
-        renderedSizes.set(path.relative(root, modId), resultingCodeBytes);
-
-        const resolveImportString = (importString: string) =>
-          this.resolve(importString, mod.id);
-
-        let isESM =
-          !mod.code ||
-          /(^\s*export\s+[\w\{]|^\s*import\s+[\w\{]|\bimport\()|\bcreateRequire\(/ms.test(
-            mod.code,
-          ) ||
-          !/((^|\b)exports\b|\brequire\()/.test(mod.code);
-
-        const staticImportsMeta = createImportsMeta(
-          mod.importedIds,
-          'import-statement',
-          root,
-          resolveImportString,
-          mod.code,
-        );
-
-        const dynamicImportsMeta = createImportsMeta(
-          mod.dynamicallyImportedIds,
-          'dynamic-import',
-          root,
-          resolveImportString,
-          mod.code,
-        );
-
-        const importsMeta = (
-          await Promise.all([...staticImportsMeta, ...dynamicImportsMeta])
-        ).reduce((acc, {importedId, ...meta}) => {
-          if (isViteHelper(importedId)) {
-            // Helpers are CJS
-            isESM = false;
-          } else {
-            // Uncomment this to include modules with 0 bytes in the output (tree-shaked)
-            // if (!toAnalyzeMods.has(importedId)) {
-            //   toAnalyzeMods.add(importedId);
-            // }
-            acc.push(meta);
+            if (result) resultingCodeBytes = result.code.length;
           }
 
-          return acc;
-        }, [] as Array<{path: string; kind: string; original: string}>);
+          renderedSizes.set(path.relative(root, modId), resultingCodeBytes);
 
-        modsMeta.set(path.relative(root, modId), {
-          bytes: originalCodeBytes,
-          format: isESM ? 'esm' : 'cjs',
-          imports: importsMeta,
-        });
-      }
+          const resolveImportString = (importString: string) =>
+            this.resolve(importString, mod.id);
+
+          let isESM =
+            !mod.code ||
+            /(^\s*export\s+[\w\{]|^\s*import\s+[\w\{]|\bimport\()|\bcreateRequire\(/ms.test(
+              mod.code,
+            ) ||
+            !/((^|\b)exports\b|\brequire\()/.test(mod.code);
+
+          const staticImportsMeta = createImportsMeta(
+            mod.importedIds,
+            'import-statement',
+            root,
+            resolveImportString,
+            mod.code,
+          );
+
+          const dynamicImportsMeta = createImportsMeta(
+            mod.dynamicallyImportedIds,
+            'dynamic-import',
+            root,
+            resolveImportString,
+            mod.code,
+          );
+
+          const importsMeta = (
+            await Promise.all([...staticImportsMeta, ...dynamicImportsMeta])
+          ).reduce((acc, {importedId, ...meta}) => {
+            if (isViteHelper(importedId)) {
+              // Helpers are CJS
+              isESM = false;
+            } else {
+              acc.push(meta);
+            }
+
+            return acc;
+          }, [] as Array<{path: string; kind: string; original: string}>);
+
+          modsMeta.set(path.relative(root, modId), {
+            bytes: originalCodeBytes,
+            format: isESM ? 'esm' : 'cjs',
+            imports: importsMeta,
+          });
+        }),
+      );
 
       const inputs = Object.fromEntries(modsMeta.entries());
       const metafile = {
