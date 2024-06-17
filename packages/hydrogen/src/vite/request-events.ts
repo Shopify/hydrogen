77cf70cfaa5f684c914402183cfe2754783a228c
Fran Dios <fran.dios@shopify.com>
2024-06-17 15:36:14 +0900

    Add extension to CLI import

--- packages/hydrogen/src/vite/request-events.ts
+++ packages/hydrogen/src/vite/request-events.ts
@@ -2,7 +2,7 @@ import path from 'node:path';
 import {EventEmitter} from 'node:events';
 import type {IncomingMessage, ServerResponse} from 'node:http';
 import {mapSourcePosition} from 'source-map-support';
-import type {WaitUntil} from '../types';
+import type {WaitUntil} from '../types.js';
 
 const IGNORED_ROUTES = new Set([
   '/graphiql',
