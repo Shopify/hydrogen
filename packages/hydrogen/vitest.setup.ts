32e91ab9152fc3172224701d0d36d6685f32fd44
Helen Lin <helen.lin@shopify.com>
2024-07-02 11:30:06 -0700

    set up perfkit in test

--- packages/hydrogen/vitest.setup.ts
+++ packages/hydrogen/vitest.setup.ts
@@ -4,6 +4,13 @@ import {expect} from 'vitest';
 
 expect.extend(matchers);
 
+const perfkit = document.createElement('script');
+perfkit.setAttribute('data-application', 'hydrogen');
+perfkit.setAttribute('data-shop-id', '1');
+perfkit.setAttribute('data-storefront-id', '0');
+perfkit.setAttribute('data-monorail-region', 'global');
+perfkit.setAttribute('data-spa-mode', 'true');
+
 Object.defineProperty(document, 'currentScript', {
-  value: document.createElement('script'),
+  value: perfkit,
 });
