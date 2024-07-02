affed9c67106226282d44c2551fccc6a6a3ba396
Helen Lin <helen.lin@shopify.com>
2024-07-02 11:07:21 -0700

    Fix test

--- packages/hydrogen/vitest.setup.ts
+++ packages/hydrogen/vitest.setup.ts
@@ -3,3 +3,7 @@ import matchers from '@testing-library/jest-dom/matchers';
 import {expect} from 'vitest';
 
 expect.extend(matchers);
+
+Object.defineProperty(document, 'currentScript', {
+  value: document.createElement('script'),
+});
