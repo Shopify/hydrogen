c7c9bb6a5970be793461eee762bf95321d77aca7
Michelle Chen <michelle.chen@shopify.com>
2024-07-04 14:24:02 -0400

    fix test

--- packages/hydrogen-react/src/ShopifyProvider.test.tsx
+++ packages/hydrogen-react/src/ShopifyProvider.test.tsx
@@ -203,7 +203,7 @@ describe('<ShopifyProvider/>', () => {
         result.current.getStorefrontApiUrl({
           storeDomain: 'https://test.myshopify.com',
         }),
-      ).toBe('https://test.myshopify.com/api/2024-04/graphql.json');
+      ).toBe('https://test.myshopify.com/api/2024-07/graphql.json');
     });
 
     it(`handles when a '/' is at the end of the url and doesn't add an extra one`, () => {
