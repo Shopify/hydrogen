4faf0c7a5baabf6f4503cfffdcbd5ee17641d082
Fran Dios <fran.dios@shopify.com>
2024-07-12 16:30:11 +0900

    Rework analytics tests to use act and mock fetch

--- packages/hydrogen/vitest.setup.ts
+++ packages/hydrogen/vitest.setup.ts
@@ -3,8 +3,3 @@ import matchers from '@testing-library/jest-dom/matchers';
 import {expect} from 'vitest';
 
 expect.extend(matchers);
-
-// Defining `document.currentScript` to avoid errors in tests
-Object.defineProperty(document, 'currentScript', {
-  value: document.createElement('script'),
-});
