7172f50f16c73c49da7ed608c68cfc780c6378c5
Bret Little <bret.little@shopify.com>
2024-07-22 16:24:55 -0400

    Fix the docs

--- packages/hydrogen/src/product/useOptimisticVariant.doc.ts
+++ packages/hydrogen/src/product/useOptimisticVariant.doc.ts
@@ -39,7 +39,7 @@ const data: ReferenceEntityTemplateSchema = {
   definitions: [
     {
       title: 'Props',
-      type: 'useOptimisticVariantGeneratedType',
+      type: 'UseOptimisticVariantGeneratedType',
       description: '',
     },
   ],
