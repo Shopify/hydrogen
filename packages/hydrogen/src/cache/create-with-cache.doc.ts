6e057af0ccdd6e34bf5a2cc0d0388cca0e4228f1
Bret Little <bret.little@shopify.com>
2024-07-29 10:40:14 -0400

    Update the `createWithCache` docs to warn about caching errors (#2373)
    * Update the `createWithCache` docs to warn about caching errors
    
    * Feedback updates

--- packages/hydrogen/src/cache/create-with-cache.doc.ts
+++ packages/hydrogen/src/cache/create-with-cache.doc.ts
@@ -6,7 +6,7 @@ const data: ReferenceEntityTemplateSchema = {
   subCategory: 'caching',
   isVisualComponent: false,
   related: [],
-  description: `Creates a utility function that executes an asynchronous operation \n like \`fetch\` and caches the result according to the strategy provided.\nUse this to call any third-party APIs from loaders or actions.`,
+  description: `Creates a utility function that executes an asynchronous operation \n like \`fetch\` and caches the result according to the strategy provided.\nUse this to call any third-party APIs from loaders or actions.\n > Note:\n > Sometimes a request to a third-party API might fail, so you shouldn't cache the result. To prevent caching, throw when a request fails. If you don't throw, then the result is cached.`,
   type: 'utility',
   defaultExample: {
     description: 'I am the default example',
