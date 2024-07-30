7a81ea458d8094a86c00b5df2246db2ed34a8323
Bret Little <bret.little@shopify.com>
2024-07-30 10:19:36 -0400

    Add a google cache check to hydration (#2334)

---
+++ .changeset/witty-planes-tickle.md
@@ -0,0 +1,20 @@
+---
+'skeleton': patch
+---
+
+Add a hydration check for google web cache. This prevents an infinite redirect when viewing the cached version of a hydrogen site on Google.
+
+Update your entry.server.jsx file to include this check:
+
+```diff
++ if (!window.location.origin.includes("webcache.googleusercontent.com")) {
+   startTransition(() => {
+     hydrateRoot(
+       document,
+       <StrictMode>
+         <RemixBrowser />
+       </StrictMode>
+     );
+   });
++ }
+```
--- templates/skeleton/app/entry.client.tsx
+++ templates/skeleton/app/entry.client.tsx
@@ -2,11 +2,13 @@ import {RemixBrowser} from '@remix-run/react';
 import {startTransition, StrictMode} from 'react';
 import {hydrateRoot} from 'react-dom/client';
 
-startTransition(() => {
-  hydrateRoot(
-    document,
-    <StrictMode>
-      <RemixBrowser />
-    </StrictMode>,
-  );
-});
+if (!window.location.origin.includes('webcache.googleusercontent.com')) {
+  startTransition(() => {
+    hydrateRoot(
+      document,
+      <StrictMode>
+        <RemixBrowser />
+      </StrictMode>,
+    );
+  });
+}
