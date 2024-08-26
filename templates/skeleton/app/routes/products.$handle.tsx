1f198bb06040d745f7c6421c946f8d64dae9cdc3
Bret Little <bret.little@shopify.com>
2024-08-26 15:04:53 -0400

    Test

--- templates/skeleton/app/routes/products.$handle.tsx
+++ templates/skeleton/app/routes/products.$handle.tsx
@@ -46,8 +46,8 @@ async function loadCriticalData({
 
   console.log('wow');
 
-  promiseWithTimeout({
-    promise: new Promise((resolve) => setTimeout(resolve, 6000)),
+  await promiseWithTimeout({
+    promise: new Promise((resolve) => setTimeout(resolve, 4000)),
   });
 
   const [{product}] = await Promise.all([
