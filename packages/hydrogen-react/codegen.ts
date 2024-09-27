887452b056498114aeab66a95304059a85eb2c23
Helen Lin <helen.lin@shopify.com>
2024-09-27 12:09:22 -0700

    SFAPI 2024-10 update

--- packages/hydrogen-react/codegen.ts
+++ packages/hydrogen-react/codegen.ts
@@ -4,8 +4,8 @@ import {
   customerAccountApiCustomScalars,
 } from './src/codegen.helpers';
 
-const SF_API_VERSION = '2024-07';
-const CA_API_VERSION = '2024-07';
+const SF_API_VERSION = '2024-10';
+const CA_API_VERSION = '2024-10';
 
 const storefrontAPISchema: CodegenConfig['schema'] = {
   [`https://hydrogen-preview.myshopify.com/api/${SF_API_VERSION}/graphql.json`]:
