c14c813a75bab1019ee65ede3841891816a18e04
coryagami <ca11h@my.fsu.edu>
2024-10-24 12:24:38 -0400

    Add optional headers param for logout redirect (#2602)

--- packages/hydrogen/src/customer/customer.ts
+++ packages/hydrogen/src/customer/customer.ts
@@ -430,7 +430,7 @@ export function createCustomerAccountClient({
 
       clearSession(session);
 
-      return redirect(logoutUrl);
+      return redirect(logoutUrl, {headers: options?.headers || {}});
     },
     isLoggedIn,
     handleAuthStatus,
