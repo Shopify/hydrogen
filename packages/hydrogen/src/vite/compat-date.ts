6387a5b3dc0916f35acc28153089880ed3600b48
Helen Lin <helen.lin@shopify.com>
2024-10-25 09:40:56 -0700

    format

--- packages/hydrogen/src/vite/compat-date.ts
+++ packages/hydrogen/src/vite/compat-date.ts
@@ -1,5 +1,5 @@
 // This cannot use LIB_VERSION as it changes when deploy on next tag
-const COMPAT_DATE = '2024-10-01'
+const COMPAT_DATE = '2024-10-01';
 export function getCompatDate() {
   // E.g. '2024-07-01'
   return COMPAT_DATE;
