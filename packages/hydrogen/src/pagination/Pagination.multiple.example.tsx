031156e566be25d8c4073fdb460c0f62b44795fa
Scott Dixon <scott.dixon@shopify.com>
2024-11-22 15:25:01 +1000

    Add examples to the docs

---
+++ packages/hydrogen/src/pagination/Pagination.multiple.example.tsx
@@ -0,0 +1,117 @@
+import {json, type LoaderArgs} from '@shopify/remix-oxygen';
+import {useLoaderData, Link} from '@remix-run/react';
+import {getPaginationVariables, Pagination} from '@shopify/hydrogen';
+
+export async function loader({request, context: {storefront}}: LoaderArgs) {
+  const womensPaginationVariables = getPaginationVariables(request, {
+    pageBy: 2,
+    namespace: 'womens', // Specify a unique namespace for the pagination parameters
+  });
+  const mensPaginationVariables = getPaginationVariables(request, {
+    pageBy: 2,
+    namespace: 'mens', // Specify a unique namespace for the pagination parameters
+  });
+
+  const [womensProducts, mensProducts] = await Promise.all([
+    storefront.query(COLLECTION_PRODUCTS_QUERY, {
+      variables: {...womensPaginationVariables, handle: 'women'},
+    }),
+    storefront.query(COLLECTION_PRODUCTS_QUERY, {
+      variables: {...mensPaginationVariables, handle: 'men'},
+    }),
+  ]);
+
+  return json({womensProducts, mensProducts});
+}
+
+export default function Collection() {
+  const {womensProducts, mensProducts} = useLoaderData<typeof loader>();
+  return (
+    <div className="collection">
+      <h1>Womens</h1>
+
+      <Pagination
+        connection={womensProducts?.collection?.products}
+        // Specify a unique namespace for the pagination links
+        namespace="womens"
+      >
+        {({nodes, isLoading, PreviousLink, NextLink}) => {
+          return (
+            <div>
+              <PreviousLink>
+                {isLoading ? 'Loading...' : <span>↑ Load previous</span>}
+              </PreviousLink>
+              <div>
+                {nodes.map((product) => (
+                  <div key={product.id}>
+                    <Link to={`/products/${product.handle}`}>
+                      {product.title}
+                    </Link>
+                  </div>
+                ))}
+              </div>
+              <NextLink>
+                {isLoading ? 'Loading...' : <span>Load more ↓</span>}
+              </NextLink>
+            </div>
+          );
+        }}
+      </Pagination>
+
+      <h1>Mens</h1>
+      <Pagination
+        connection={mensProducts?.collection?.products}
+        // Specify a unique namespace for the pagination links
+        namespace="mens"
+      >
+        {({nodes, isLoading, PreviousLink, NextLink}) => {
+          return (
+            <div>
+              <PreviousLink>
+                {isLoading ? 'Loading...' : <span>↑ Load previous</span>}
+              </PreviousLink>
+              <div>
+                {nodes.map((product) => (
+                  <div key={product.id}>
+                    <Link to={`/products/${product.handle}`}>
+                      {product.title}
+                    </Link>
+                  </div>
+                ))}
+              </div>
+              <NextLink>
+                {isLoading ? 'Loading...' : <span>Load more ↓</span>}
+              </NextLink>
+            </div>
+          );
+        }}
+      </Pagination>
+    </div>
+  );
+}
+
+const COLLECTION_PRODUCTS_QUERY = `#graphql
+  query CollectionProducts(
+    $first: Int
+    $last: Int
+    $startCursor: String
+    $endCursor: String
+    $handle: String!
+  ) {
+    collection(handle: $handle) {
+      products(first: $first, last: $last, before: $startCursor, after: $endCursor) {
+        nodes {
+          id
+          handle
+          title
+        }
+        pageInfo {
+          hasPreviousPage
+          hasNextPage
+          startCursor
+          endCursor
+        }
+      }
+    }
+  }
+` as const;
