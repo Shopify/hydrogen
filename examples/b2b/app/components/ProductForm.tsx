bc740e91360d417b02f0cf818696ef95a591e4c9
Helen Lin <helen.lin@shopify.com>
2024-12-09 18:44:48 -0800

    don't display single options

--- examples/b2b/app/components/ProductForm.tsx
+++ examples/b2b/app/components/ProductForm.tsx
@@ -25,82 +25,87 @@ export function ProductForm({
   const {open} = useAside();
   return (
     <div className="product-form">
-      {productOptions.map((option) => (
-        <div className="product-options" key={option.name}>
-          <h5>{option.name}</h5>
-          <div className="product-options-grid">
-            {option.optionValues.map((value) => {
-              const {
-                name,
-                handle,
-                variantUriQuery,
-                selected,
-                available,
-                exists,
-                isDifferentProduct,
-                swatch,
-              } = value;
+      {productOptions.map((option) => {
+        // If there is only a single value in the option values, don't display the option
+        if (option.optionValues.length === 1) return null;
 
-              if (isDifferentProduct) {
-                // SEO
-                // When the variant is a combined listing child product
-                // that leads to a different url, we need to render it
-                // as an anchor tag
-                return (
-                  <Link
-                    className="product-options-item"
-                    key={option.name + name}
-                    prefetch="intent"
-                    preventScrollReset
-                    replace
-                    to={`/products/${handle}?${variantUriQuery}`}
-                    style={{
-                      border: selected
-                        ? '1px solid black'
-                        : '1px solid transparent',
-                      opacity: available ? 1 : 0.3,
-                    }}
-                  >
-                    <ProductOptionSwatch swatch={swatch} name={name} />
-                  </Link>
-                );
-              } else {
-                // SEO
-                // When the variant is an update to the search param,
-                // render it as a button with javascript navigating to
-                // the variant so that SEO bots do not index these as
-                // duplicated links
-                return (
-                  <button
-                    type="button"
-                    className={`product-options-item${
-                      exists && !selected ? ' link' : ''
-                    }`}
-                    key={option.name + name}
-                    style={{
-                      border: selected
-                        ? '1px solid black'
-                        : '1px solid transparent',
-                      opacity: available ? 1 : 0.3,
-                    }}
-                    disabled={!exists}
-                    onClick={() => {
-                      if (!selected) {
-                        navigate(`?${variantUriQuery}`, {
-                          replace: true,
-                        });
-                      }
-                    }}
-                  >
-                    <ProductOptionSwatch swatch={swatch} name={name} />
-                  </button>
-                );
-              }
-            })}
+        return (
+          <div className="product-options" key={option.name}>
+            <h5>{option.name}</h5>
+            <div className="product-options-grid">
+              {option.optionValues.map((value) => {
+                const {
+                  name,
+                  handle,
+                  variantUriQuery,
+                  selected,
+                  available,
+                  exists,
+                  isDifferentProduct,
+                  swatch,
+                } = value;
+
+                if (isDifferentProduct) {
+                  // SEO
+                  // When the variant is a combined listing child product
+                  // that leads to a different url, we need to render it
+                  // as an anchor tag
+                  return (
+                    <Link
+                      className="product-options-item"
+                      key={option.name + name}
+                      prefetch="intent"
+                      preventScrollReset
+                      replace
+                      to={`/products/${handle}?${variantUriQuery}`}
+                      style={{
+                        border: selected
+                          ? '1px solid black'
+                          : '1px solid transparent',
+                        opacity: available ? 1 : 0.3,
+                      }}
+                    >
+                      <ProductOptionSwatch swatch={swatch} name={name} />
+                    </Link>
+                  );
+                } else {
+                  // SEO
+                  // When the variant is an update to the search param,
+                  // render it as a button with javascript navigating to
+                  // the variant so that SEO bots do not index these as
+                  // duplicated links
+                  return (
+                    <button
+                      type="button"
+                      className={`product-options-item${
+                        exists && !selected ? ' link' : ''
+                      }`}
+                      key={option.name + name}
+                      style={{
+                        border: selected
+                          ? '1px solid black'
+                          : '1px solid transparent',
+                        opacity: available ? 1 : 0.3,
+                      }}
+                      disabled={!exists}
+                      onClick={() => {
+                        if (!selected) {
+                          navigate(`?${variantUriQuery}`, {
+                            replace: true,
+                          });
+                        }
+                      }}
+                    >
+                      <ProductOptionSwatch swatch={swatch} name={name} />
+                    </button>
+                  );
+                }
+              })}
+            </div>
+            <br />
           </div>
-          <br />
-        </div>
-      ))}
+        )
+      })}
       <br />
       <AddToCartButton
         disabled={!selectedVariant || !selectedVariant.availableForSale}
