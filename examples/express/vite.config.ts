ee5a82194b42839884a9b904edbf86d7231fe01d
Helen Lin <helen.lin@shopify.com>
2025-01-09 13:27:16 -0800

    Turn on v3_lazyRouteDiscovery flag

--- examples/express/vite.config.ts
+++ examples/express/vite.config.ts
@@ -12,6 +12,7 @@ export default defineConfig({
         v3_fetcherPersist: true,
         v3_relativeSplatPath: true,
         v3_throwAbortReason: true,
+        v3_lazyRouteDiscovery: true,
       },
     }),
     tsconfigPaths(),
