666d502e10464a202eaf41a7f8e8a2d436a44499
Helen Lin <helen.lin@shopify.com>
2025-01-15 13:02:01 -0800

    Fix typescript error

--- examples/multipass/vite.config.ts
+++ examples/multipass/vite.config.ts
@@ -4,6 +4,12 @@ import {oxygen} from '@shopify/mini-oxygen/vite';
 import {vitePlugin as remix} from '@remix-run/dev';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
+declare module "@remix-run/server-runtime" {
+  interface Future {
+    v3_singleFetch: true;
+  }
+}
+
 export default defineConfig({
   plugins: [
     hydrogen(),
