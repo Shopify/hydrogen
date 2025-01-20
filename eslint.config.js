d88c0c3f3d5ab47f9d90ca37ddd3b16ca8727b65
Liad Yosef <liad.yosef@gmail.com>
2025-01-20 23:12:50 +0200

    chore(eslint): upgrade root package.json to eslint@9

---
+++ eslint.config.js
@@ -0,0 +1,39 @@
+const typescriptParser = require('@typescript-eslint/parser');
+const typescriptPlugin = require('@typescript-eslint/eslint-plugin');
+
+/** @type {import('eslint').Flat.Config[]} */
+module.exports = [
+  {
+    ignores: [
+      'build/',
+      'node_modules/',
+      'bin/',
+      '*.d.ts',
+      'dist/',
+      'coverage/',
+      'packages/hydrogen-react/codegen.ts',
+      'packages/hydrogen-react/vite.config.ts',
+      'packages/hydrogen-react/vitest.setup.ts',
+    ],
+  },
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    languageOptions: {
+      parser: typescriptParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': typescriptPlugin,
+    },
+    rules: {
+      '@typescript-eslint/ban-ts-comment': 'off',
+      '@typescript-eslint/naming-convention': 'off',
+      '@typescript-eslint/no-non-null-asserted-optional-chain': 'off',
+      'no-useless-escape': 'off',
+      'no-case-declarations': 'off',
+    },
+  },
+];
