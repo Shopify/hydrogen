53d1dc299ac1ff14417ae078b0ce30a193824809
Helen Lin <helen.lin@shopify.com>
2025-02-12 11:15:18 -0800

    Fix doc

--- packages/hydrogen/src/cache/CacheNone.example.js
+++ packages/hydrogen/src/cache/CacheNone.example.js
@@ -3,12 +3,13 @@ import {CacheNone} from '@shopify/hydrogen';
 export async function loader({context}) {
   const data = await context.storefront.query(
     `#grahpql
-  {
-    shop {
-      name
-      description
-    }
-  }`,
+      {
+        shop {
+          name
+          description
+        }
+      }
+    `,
     {
       cache: CacheNone(),
     },
