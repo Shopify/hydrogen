732b52981078654330b90e39bc5a2be4c6094f43
Juan P. Prieto <jp@calltheguys.co>
2025-02-13 10:28:10 -0800

    Add comments on root loader data revalidation

--- templates/skeleton/app/root.tsx
+++ templates/skeleton/app/root.tsx
@@ -33,6 +33,11 @@ export const shouldRevalidate: ShouldRevalidateFunction = ({
   // revalidate when manually revalidating via useRevalidator
   if (currentUrl.toString() === nextUrl.toString()) return true;
 
+  // Defaulting to no revalidation for root loader data to improve performance. 
+  // When using this feature, you risk your UI getting out of sync with your server. 
+  // Use with caution. If you are uncomfortable with this optimization, update the 
+  // line below to `return defaultShouldRevalidate` instead.
+  // For more details see: https://remix.run/docs/en/main/route/should-revalidate
   return false;
 };
 
