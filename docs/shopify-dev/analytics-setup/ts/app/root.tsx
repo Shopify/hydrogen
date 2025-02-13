bdee42ce85cffcce13e2d9fd1b88ab3339981247
Helen Lin <helen.lin@shopify.com>
2025-02-13 10:52:22 -0800

    Fix shouldRevalidate on single fetch (#2752)
    Co-authored-by: Juan P. Prieto <jp@calltheguys.co>

--- docs/shopify-dev/analytics-setup/ts/app/root.tsx
+++ docs/shopify-dev/analytics-setup/ts/app/root.tsx
@@ -46,6 +46,11 @@ export const shouldRevalidate: ShouldRevalidateFunction = ({
     return true;
   }
 
+  // Defaulting to no revalidation for root loader data to improve performance.
+  // When using this feature, you risk your UI getting out of sync with your server.
+  // Use with caution. If you are uncomfortable with this optimization, update the
+  // line below to `return defaultShouldRevalidate` instead.
+  // For more details see: https://remix.run/docs/en/main/route/should-revalidate
   return false;
 };
 
