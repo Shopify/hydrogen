8a69fa9031a94fb083eb32bcf3bde48a0cfd11c5
Sean Parsons <sean.parsons@shopify.com>
2025-03-12 16:03:21 +0000

    Slight tweak to reduce changes.

--- templates/skeleton/app/routes.ts
+++ templates/skeleton/app/routes.ts
@@ -1,5 +1,5 @@
-import {layout, type RouteConfig} from '@remix-run/route-config';
 import {flatRoutes} from '@remix-run/fs-routes';
+import {layout, type RouteConfig} from '@remix-run/route-config';
 import {hydrogenRoutes} from '@shopify/hydrogen';
 
 export default hydrogenRoutes([
