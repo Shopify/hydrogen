865fb4ff4bbb4c17797075458a242ef51eece63c
Balazs Bajorics <balazs.bajorics@shopify.com>
2025-04-01 16:35:44 +0200

    adding a dummy collection query

--- templates/spike/app/routes/home.tsx
+++ templates/spike/app/routes/home.tsx
@@ -1,5 +1,5 @@
 import type {Route} from './+types/home';
-import {getShopDetails} from '../lib/shopify';
+import {createClient, getShopDetails} from '../lib/shopify';
 
 // eslint-disable-next-line no-empty-pattern
 export function meta({}: Route.MetaArgs) {
@@ -11,18 +11,50 @@ export function meta({}: Route.MetaArgs) {
 
 export async function loader({context}: Route.LoaderArgs) {
   const shopData = await getShopDetails(context.cloudflare.env);
+  const featuredCollection = await getFeaturedCollection(
+    context.cloudflare.env,
+  );
   return {
     message: context.cloudflare.env.VALUE_FROM_CLOUDFLARE,
     shopName: shopData.shop.name,
+    featuredCollection,
   };
 }
 
 export default function Home({loaderData}: Route.ComponentProps) {
+  console.log(loaderData);
   return (
     <div>
       <div className="text-center mt-4">
         <h2 className="text-xl font-bold">Shop Name: {loaderData.shopName}</h2>
+        <h3>Featured Collection</h3>
+        <p>{loaderData.featuredCollection.data.collection.title}</p>
+        <img
+          src={loaderData.featuredCollection.data.collection.image.url}
+          alt={loaderData.featuredCollection.data.collection.title}
+        />
       </div>
     </div>
   );
+}
+
+async function getFeaturedCollection(env: CloudflareEnvironment) {
+  const client = createClient(env);
+  const featuredCollection = await client.request(
+    `#graphql
+      {
+        collection(handle: "featured") {
+          id
+          handle
+          title
+          description
+          image {
+            id
+            url
+          }
+        }
+      }
+    `,
+  );
+  return featuredCollection;
 }
