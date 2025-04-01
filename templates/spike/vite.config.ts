d07e8a284d06455d6b99b6e242c15f3ce613d34a
Balazs Bajorics <balazs.bajorics@shopify.com>
2025-04-01 14:47:16 +0200

    adding hydrogen and oxygen plugins

--- templates/spike/vite.config.ts
+++ templates/spike/vite.config.ts
@@ -3,9 +3,13 @@ import {cloudflare} from '@cloudflare/vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 import {defineConfig} from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import {hydrogen} from '@shopify/hydrogen/vite';
+import {oxygen} from '@shopify/mini-oxygen/vite';
 
 export default defineConfig({
   plugins: [
+    hydrogen(),
+    oxygen(),
     cloudflare({viteEnvironment: {name: 'ssr'}}),
     tailwindcss(),
     reactRouter(),
