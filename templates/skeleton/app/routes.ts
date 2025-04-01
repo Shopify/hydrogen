6a1c8fdd7d1c5f29a7f910aae6cc0e31c34b1f66
Federico Ruggi <federico.ruggi@shopify.com>
2025-04-01 17:19:21 +0200

    add comment to hydrogenRoutes

--- templates/skeleton/app/routes.ts
+++ templates/skeleton/app/routes.ts
@@ -2,4 +2,8 @@ import {flatRoutes} from '@remix-run/fs-routes';
 import {type RouteConfig} from '@remix-run/route-config';
 import {hydrogenRoutes} from '@shopify/hydrogen';
 
-export default hydrogenRoutes(await flatRoutes()) satisfies RouteConfig;
+export default hydrogenRoutes([
+  ...(await flatRoutes()),
+  // Manual route definitions can be added to this array, in addition to or instead of using the `flatRoutes` file-based routing convention.
+  // See https://remix.run/docs/en/main/guides/routing for more details
+]) satisfies RouteConfig;
