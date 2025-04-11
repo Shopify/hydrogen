da4df6e4134d2d280d495571c724570ce4559f43
Fatih Altinok <fatihaltinok@live.com>
2025-04-11 20:10:00 +0300

    Null to false
    Co-authored-by: Bret Little <bret.little@gmail.com>

--- templates/skeleton/app/components/CartMain.tsx
+++ templates/skeleton/app/components/CartMain.tsx
@@ -26,7 +26,7 @@ export function CartMain({layout, cart: originalCart}: CartMainProps) {
     cart &&
     Boolean(cart?.discountCodes?.filter((code) => code.applicable)?.length);
   const className = `cart-main ${withDiscount ? 'with-discount' : ''}`;
-  const cartHasItems = cart?.totalQuantity ? cart.totalQuantity > 0 : null;
+  const cartHasItems = cart?.totalQuantity ? cart.totalQuantity > 0 : false;
 
   return (
     <div className={className}>
