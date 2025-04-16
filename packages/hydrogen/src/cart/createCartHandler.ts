85bf53aa172e3d29588bddd4763dcd46e7b057db
Juan P. Prieto <jp@calltheguys.co>
2025-04-16 09:36:52 -0700

    Add Address mutations support to CartForm and createCartHandler (#2850)
    * initial scaffold
    
    * cleanup UI elements
    
    * Add cart delivery address methods to the cart handler
    
    * Add new cart delivery methods to CartForm
    
    * Fix CartForm and createCartHandler types and methods
    
    * Update example to use the updated CartForm & cart methods
    
    * Add cartDeliveryAddresses tests, docs and examples
    
    * Cleanup example
    
    * Add Select address action and cart aside
    
    * lint
    
    * Fixes
    
    * Fix type issues
    
    * WIP address update form
    
    * Remove example
    
    * remove cart delivery optins from workspace
    
    * Revert changes to CartMAin
    
    * Revert CartMain
    
    * Remove example
    
    * reset package-lock
    
    * Reset package-lock
    
    * typo
    
    * More typos
    
    * Add changeset
    
    * Reset lock
    
    * Update lock
    
    * Fix lock
    
    * Add TODO ts docs

--- packages/hydrogen/src/cart/createCartHandler.ts
+++ packages/hydrogen/src/cart/createCartHandler.ts
@@ -49,6 +49,18 @@ import {
   type CartGiftCardCodesUpdateFunction,
   cartGiftCardCodesUpdateDefault,
 } from './queries/cartGiftCardCodeUpdateDefault';
+import {
+  type CartDeliveryAddressesAddFunction,
+  cartDeliveryAddressesAddDefault,
+} from './queries/cartDeliveryAddressesAddDefault';
+import {
+  type CartDeliveryAddressesRemoveFunction,
+  cartDeliveryAddressesRemoveDefault,
+} from './queries/cartDeliveryAddressesRemoveDefault';
+import {
+  type CartDeliveryAddressesUpdateFunction,
+  cartDeliveryAddressesUpdateDefault,
+} from './queries/cartDeliveryAddressesUpdateDefault';
 
 export type CartHandlerOptions = {
   storefront: Storefront;
@@ -84,6 +96,83 @@ export type HydrogenCart = {
   updateAttributes: ReturnType<typeof cartAttributesUpdateDefault>;
   setMetafields: ReturnType<typeof cartMetafieldsSetDefault>;
   deleteMetafield: ReturnType<typeof cartMetafieldDeleteDefault>;
+  /**
+   * Adds delivery addresses to the cart.
+   *
+   * This function sends a mutation to the storefront API to add one or more delivery addresses to the cart.
+   * It returns the result of the mutation, including any errors that occurred.
+   *
+   * @param {CartQueryOptions} options - The options for the cart query, including the storefront API client and cart fragment.
+   * @returns {ReturnType<typeof cartDeliveryAddressesAddDefault>} - A function that takes an array of addresses and optional parameters, and returns the result of the API call.
+   *
+   * @example
+   * const result = await cart.addDeliveryAddresses(
+   *   [
+   *     {
+   *       address1: '123 Main St',
+   *       city: 'Anytown',
+   *       countryCode: 'US'
+   *     }
+   *   ],
+   *   { someOptionalParam: 'value' }
+   * );
+   */
+  addDeliveryAddresses: ReturnType<typeof cartDeliveryAddressesAddDefault>;
+  /**
+   * Removes delivery addresses from the cart.
+   *
+   * This function sends a mutation to the storefront API to remove one or more delivery addresses from the cart.
+   * It returns the result of the mutation, including any errors that occurred.
+   *
+   * @param {CartQueryOptions} options - The options for the cart query, including the storefront API client and cart fragment.
+   * @returns {CartDeliveryAddressRemoveFunction} - A function that takes an array of address IDs and optional parameters, and returns the result of the API call.
+   *
+   * @example
+   * const result = await cart.removeDeliveryAddresses([
+   *   "gid://shopify/<objectName>/10079785100"
+   * ],
+   * { someOptionalParam: 'value' });
+   */
+
+  removeDeliveryAddresses: ReturnType<
+    typeof cartDeliveryAddressesRemoveDefault
+  >;
+  /**
+  * Updates delivery addresses in the cart.
+  *
+  * This function sends a mutation to the storefront API to update one or more delivery addresses in the cart.
+  * It returns the result of the mutation, including any errors that occurred.
+  *
+  * @param {CartQueryOptions} options - The options for the cart query, including the storefront API client and cart fragment.
+  * @returns {CartDeliveryAddressUpdateFunction} - A function that takes an array of addresses and optional parameters, and returns the result of the API call.
+  *
+  * const result = await cart.updateDeliveryAddresses([
+      {
+        "address": {
+          "copyFromCustomerAddressId": "gid://shopify/<objectName>/10079785100",
+          "deliveryAddress": {
+            "address1": "<your-address1>",
+            "address2": "<your-address2>",
+            "city": "<your-city>",
+            "company": "<your-company>",
+            "countryCode": "AC",
+            "firstName": "<your-firstName>",
+            "lastName": "<your-lastName>",
+            "phone": "<your-phone>",
+            "provinceCode": "<your-provinceCode>",
+            "zip": "<your-zip>"
+          }
+        },
+        "id": "gid://shopify/<objectName>/10079785100",
+        "oneTimeUse": true,
+        "selected": true,
+        "validationStrategy": "COUNTRY_CODE_ONLY"
+      }
+    ],{ someOptionalParam: 'value' });
+  */
+  updateDeliveryAddresses: ReturnType<
+    typeof cartDeliveryAddressesUpdateDefault
+  >;
 };
 
 export type HydrogenCartCustom<
@@ -202,6 +291,9 @@ export function createCartHandler<TCustomMethods extends CustomMethodsBase>(
         : await cartCreate({metafields}, optionalParams);
     },
     deleteMetafield: cartMetafieldDeleteDefault(mutateOptions),
+    addDeliveryAddresses: cartDeliveryAddressesAddDefault(mutateOptions),
+    removeDeliveryAddresses: cartDeliveryAddressesRemoveDefault(mutateOptions),
+    updateDeliveryAddresses: cartDeliveryAddressesUpdateDefault(mutateOptions),
   };
 
   if ('customMethods' in options) {
@@ -253,6 +345,10 @@ export type HydrogenCartForDocs = {
    */
   addLines?: CartLinesAddFunction;
   /**
+   * Adds a delivery address to the cart.
+   */
+  addDeliveryAddresses?: CartDeliveryAddressesAddFunction;
+  /**
    * Creates a new cart.
    */
   create?: CartCreateFunction;
@@ -270,6 +366,10 @@ export type HydrogenCartForDocs = {
    */
   getCartId?: () => string | undefined;
   /**
+   * Removes a delivery address from the cart
+   */
+  removeDeliveryAddresses?: CartDeliveryAddressesRemoveFunction;
+  /**
    * Removes items from the cart.
    */
   removeLines?: CartLinesRemoveFunction;
@@ -283,6 +383,10 @@ export type HydrogenCartForDocs = {
    * If the cart doesn't exist, a new one will be created.
    */
   setMetafields?: CartMetafieldsSetFunction;
+  /**
+   * Update cart delivery addresses.
+   */
+  updateDeliveryAddresses?: CartDeliveryAddressesUpdateFunction;
   /**
    * Updates additional information (attributes) in the cart.
    */
