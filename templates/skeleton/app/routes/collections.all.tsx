0c52bcb7c4c2849ad939d92b5bf53d498976d795
Juan P. Prieto <jp@calltheguys.co>
2025-04-18 11:56:51 -0700

    Cleanup

--- templates/skeleton/app/routes/collections.all.tsx
+++ templates/skeleton/app/routes/collections.all.tsx
@@ -69,38 +69,6 @@ export default function Collection() {
   );
 }
 
-function CollectionItem({
-  product,
-  loading,
-}: {
-  product: CollectionItemFragment;
-  loading?: 'eager' | 'lazy';
-}) {
-  const variantUrl = useVariantUrl(product.handle);
-  return (
-    <Link
-      className="product-item"
-      key={product.id}
-      prefetch="intent"
-      to={variantUrl}
-    >
-      {product.featuredImage && (
-        <Image
-          alt={product.featuredImage.altText || product.title}
-          aspectRatio="1/1"
-          data={product.featuredImage}
-          loading={loading}
-          sizes="(min-width: 45em) 400px, 100vw"
-        />
-      )}
-      <h4>{product.title}</h4>
-      <small>
-        <Money data={product.priceRange.minVariantPrice} />
-      </small>
-    </Link>
-  );
-}
-
 const COLLECTION_ITEM_FRAGMENT = `#graphql
   fragment MoneyProductItem on MoneyV2 {
     amount
