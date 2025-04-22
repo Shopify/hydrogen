39f0929e670bab79e87b932d8a358af44b898bee
Balazs Bajorics <balazs.bajorics@shopify.com>
2025-04-22 15:26:56 +0200

    it seems like the skeleton's build directory should be 'dist' according to CLI tests

--- templates/skeleton/react-router.config.ts
+++ templates/skeleton/react-router.config.ts
@@ -1,7 +1,7 @@
-import type { Config } from "@react-router/dev/config";
+import type {Config} from '@react-router/dev/config';
 
 export default {
   appDirectory: 'app',
-  buildDirectory: 'build',
+  buildDirectory: 'dist',
   ssr: true,
-} satisfies Config
+} satisfies Config;
