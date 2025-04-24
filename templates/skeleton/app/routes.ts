13f21f9dd1ad31c44818e4d9fbb194385a9980f7
Balazs Bajorics <balazs.bajorics@shopify.com>
2025-04-24 15:30:05 +0200

    prettier change

--- templates/skeleton/app/routes.ts
+++ templates/skeleton/app/routes.ts
@@ -1,5 +1,5 @@
-import { flatRoutes } from '@react-router/fs-routes';
-import { type RouteConfig } from '@react-router/dev/routes';
+import {flatRoutes} from '@react-router/fs-routes';
+import {type RouteConfig} from '@react-router/dev/routes';
 import {hydrogenRoutes} from '@shopify/hydrogen';
 
 export default hydrogenRoutes([
