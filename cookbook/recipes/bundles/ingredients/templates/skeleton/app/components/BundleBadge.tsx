5e2cdd749757ba05cee7b184981f65cc56f9b623
Juan P. Prieto <jp@calltheguys.co>
2025-05-09 12:30:56 +0100

    Cookbook recipe: Bundles (#2873)
    * Add bundles recipe boilerplate
    
    * update
    
    * Add missing ingredients and render
    
    * Update recipe to latest skeleton
    
    * regenerate
    
    * Fix typos
    
    * Add step titles
    
    * regen
    
    * update copy
    
    * Copyedits
    
    * render README.md
    
    * regen
    
    ---------
    
    Co-authored-by: Federico Ruggi <federico.ruggi@shopify.com>
    Co-authored-by: Rory Tanner <rory.tanner@shopify.com>

---
+++ cookbook/recipes/bundles/ingredients/templates/skeleton/app/components/BundleBadge.tsx
@@ -0,0 +1,17 @@
+export function BundleBadge() {
+  return (
+    <div
+      style={{
+        position: 'absolute',
+        padding: '.5rem .75rem',
+        fontSize: '11px',
+        backgroundColor: '#10804c',
+        color: 'white',
+        top: '1rem',
+        right: '1rem',
+      }}
+    >
+      BUNDLE
+    </div>
+  );
+}
