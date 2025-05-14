8b9feeac9c84467e35bf88cb155505b28ded6bab
Federico Ruggi <federico.ruggi@shopify.com>
2025-05-14 19:07:25 +0200

    update buyer identity

--- cookbook/recipes/markets/ingredients/templates/skeleton/app/components/CountrySelector.tsx
+++ cookbook/recipes/markets/ingredients/templates/skeleton/app/components/CountrySelector.tsx
@@ -1,5 +1,7 @@
-import {Link} from '@remix-run/react';
+import {Link, useFetcher} from '@remix-run/react';
 import {DEFAULT_LOCALE, Locale, useSelectedLocale} from '../lib/i18n';
+import {CartForm} from '@shopify/hydrogen';
+import {useCallback, useEffect, useState} from 'react';
 
 export function CountrySelector() {
   const selectedLocale = useSelectedLocale();
@@ -42,9 +44,51 @@ export function CountrySelector() {
 }
 
 const LocaleLink = ({locale}: {locale: Locale}) => {
+  const selectedLocale = useSelectedLocale();
+  const fetcher = useFetcher();
+  const [updating, setUpdating] = useState(false);
+
+  const updateCartBuyerIdentity = useCallback(() => {
+    if (selectedLocale?.pathPrefix === locale.pathPrefix) {
+      return;
+    }
+    setUpdating(true);
+
+    const target = `${locale.pathPrefix.replace(/\/+$/, '')}/cart`;
+
+    const form = new FormData();
+    const variables = {
+      action: CartForm.ACTIONS.BuyerIdentityUpdate,
+      inputs: {
+        buyerIdentity: {
+          countryCode: locale.country.toUpperCase(),
+        },
+        redirectTo: target,
+      },
+    };
+    form.append('cartFormInput', JSON.stringify(variables));
+
+    // update the country code in the cart's buyer identity
+    fetcher.submit(form, {
+      method: 'POST',
+      action: target,
+    });
+  }, [fetcher, locale, selectedLocale]);
+
+  useEffect(() => {
+    if (updating && fetcher.state === 'loading') {
+      setUpdating(false);
+      window.location.replace(locale.pathPrefix);
+    }
+  }, [fetcher, updating, locale]);
+
   return (
-    <Link key={locale.country} reloadDocument={true} to={locale.pathPrefix}>
+    <button
+      key={locale.country}
+      onClick={updateCartBuyerIdentity}
+      disabled={updating}
+    >
       {locale.language}-{locale.country}
-    </Link>
+    </button>
   );
 };
