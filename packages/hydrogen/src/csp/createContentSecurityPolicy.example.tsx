fddab7354a2dcb2555a96b182e80518024de53c4
Balazs Bajorics <balazs.bajorics@shopify.com>
2025-05-19 11:47:25 +0200

    re-enabling createContentSecurityPolicy example

--- packages/hydrogen/src/csp/createContentSecurityPolicy.example.tsx
+++ packages/hydrogen/src/csp/createContentSecurityPolicy.example.tsx
@@ -4,46 +4,44 @@ import {isbot} from 'isbot';
 import {renderToReadableStream} from 'react-dom/server';
 import {createContentSecurityPolicy} from '@shopify/hydrogen';
 
-// TODO BEFORE MERGE: fix EntryContext and re-enable this example
+export default async function handleRequest(
+  request: Request,
+  responseStatusCode: number,
+  responseHeaders: Headers,
+  remixContext: EntryContext,
+) {
+  const {nonce, header, NonceProvider} = createContentSecurityPolicy({
+    // pass a custom directive to load content from a third party domain
+    styleSrc: [
+      "'self'",
+      'https://cdn.shopify.com',
+      'https://some-custom-css.cdn',
+    ],
+  });
+  const body = await renderToReadableStream(
+    <NonceProvider>
+      <ServerRouter context={remixContext} url={request.url} nonce={nonce} />
+    </NonceProvider>,
+    {
+      nonce,
+      signal: request.signal,
+      onError(error) {
+        // eslint-disable-next-line no-console
+        console.error(error);
+        responseStatusCode = 500;
+      },
+    },
+  );
 
-// export default async function handleRequest(
-//   request: Request,
-//   responseStatusCode: number,
-//   responseHeaders: Headers,
-//   remixContext: EntryContext,
-// ) {
-//   const {nonce, header, NonceProvider} = createContentSecurityPolicy({
-//     // pass a custom directive to load content from a third party domain
-//     styleSrc: [
-//       "'self'",
-//       'https://cdn.shopify.com',
-//       'https://some-custom-css.cdn',
-//     ],
-//   });
-//   const body = await renderToReadableStream(
-//     <NonceProvider>
-//       <ServerRouter context={remixContext} url={request.url} nonce={nonce} />
-//     </NonceProvider>,
-//     {
-//       nonce,
-//       signal: request.signal,
-//       onError(error) {
-//         // eslint-disable-next-line no-console
-//         console.error(error);
-//         responseStatusCode = 500;
-//       },
-//     },
-//   );
+  if (isbot(request.headers.get('user-agent'))) {
+    await body.allReady;
+  }
 
-//   if (isbot(request.headers.get('user-agent'))) {
-//     await body.allReady;
-//   }
+  responseHeaders.set('Content-Type', 'text/html');
+  responseHeaders.set('Content-Security-Policy', header);
 
-//   responseHeaders.set('Content-Type', 'text/html');
-//   responseHeaders.set('Content-Security-Policy', header);
-
-//   return new Response(body, {
-//     headers: responseHeaders,
-//     status: responseStatusCode,
-//   });
-// }
+  return new Response(body, {
+    headers: responseHeaders,
+    status: responseStatusCode,
+  });
+}
