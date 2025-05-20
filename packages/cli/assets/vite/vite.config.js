1e1536243b95731e242dcaf7f5adef49dd146149
Balazs Bajorics <balazs.bajorics@shopify.com>
2025-05-20 12:15:59 +0200

    Update vite.config.js (#2944)

--- packages/cli/assets/vite/vite.config.js
+++ packages/cli/assets/vite/vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig({
         v3_fetcherPersist: true,
         v3_relativeSplatPath: true,
         v3_throwAbortReason: true,
-        v3_routeConfig: true,
+        v3_routeConfig: false,
       },
     }),
     tsconfigPaths(),
