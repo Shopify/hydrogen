21b6568e95bde8f4ab32240028b584ce980c8beb
Balazs Bajorics <balazs.bajorics@shopify.com>
2025-05-21 20:09:07 +0200

    Update vite.config.ts

--- templates/skeleton/vite.config.ts
+++ templates/skeleton/vite.config.ts
@@ -23,7 +23,7 @@ export default defineConfig({
        * Include 'example-dep' in the array below.
        * @see https://vitejs.dev/config/dep-optimization-options
        */
-      include: [],
+      include: ['set-cookie-parser', 'cookie', 'react-router'],
     },
   },
 });
