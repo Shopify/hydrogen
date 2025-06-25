ac3784e36b4083f100afe52a7bc0eff2e797a183
Sean Parsons <sean.parsons@shopify.com>
2025-06-25 16:37:56 +0100

    Further change minimalisation.

--- templates/skeleton/server.ts
+++ templates/skeleton/server.ts
@@ -23,10 +23,9 @@ export default {
        * Create a Remix request handler and pass
        * Hydrogen's Storefront client to the loader context.
        */
-      const build = await import('virtual:react-router/server-build'); // eslint-disable-line import/no-unresolved
-
       const handleRequest = createRequestHandler({
-        build,
+        // eslint-disable-next-line import/no-unresolved
+        build: await import('virtual:react-router/server-build'),
         mode: process.env.NODE_ENV,
         getLoadContext: () => appLoadContext,
       });
