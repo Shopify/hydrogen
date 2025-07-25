3037f1571e0fc8b0d16564f65693aa37eb62de5a
Juan P. Prieto <jp@calltheguys.co>
2025-07-25 10:11:12 -0700

    feat(skeleton): pre-optimize @shopify/graphql-client dependency in Vite
    - Add @shopify/graphql-client to optimizeDeps.include in vite.config.ts
    - Eliminates 'new dependencies optimized' warning during development
    - Improves dev server startup performance by pre-bundling the dependency

--- templates/skeleton/vite.config.ts
+++ templates/skeleton/vite.config.ts
@@ -11,6 +11,9 @@ export default defineConfig({
     // withtout inlining assets as base64:
     assetsInlineLimit: 0,
   },
+  optimizeDeps: {
+    include: ['@shopify/graphql-client'],
+  },
   ssr: {
     optimizeDeps: {
       /**
