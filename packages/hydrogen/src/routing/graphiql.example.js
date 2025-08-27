fd511971360e9633351ff6d60c7679f4b4dd84b1
Kara Daviduik <kara.daviduik@shopify.com>
2025-08-27 19:06:03 -0400

    Replace @shopify/remix-oxygen imports with react-router All types and functions that were re-exported from remix-oxygen are now imported directly from react-router since they are available in React Router 7.6.0

--- packages/hydrogen/src/routing/graphiql.example.js
+++ packages/hydrogen/src/routing/graphiql.example.js
@@ -1,5 +1,5 @@
 import {graphiqlLoader} from '@shopify/hydrogen';
-import {redirect} from '@shopify/remix-oxygen';
+import {redirect} from 'react-router';
 
 export async function loader(args) {
   if (process.env.NODE_ENV === 'development') {
