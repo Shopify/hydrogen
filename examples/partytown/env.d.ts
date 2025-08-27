4fbe5b00cbcba0fe8172378b823fd5b88d9fac18
Kara Daviduik <kara.daviduik@shopify.com>
2025-08-27 17:01:47 -0400

    Update TypeScript references from remix-oxygen to oxygen-workers-types
    Updates /// <reference types> directives in env.d.ts files to use
    @shopify/oxygen-workers-types instead of @shopify/remix-oxygen

--- examples/partytown/env.d.ts
+++ examples/partytown/env.d.ts
@@ -1,5 +1,5 @@
 /// <reference types="vite/client" />
-/// <reference types="@shopify/remix-oxygen" />
+/// <reference types="@shopify/oxygen-workers-types" />
 /// <reference types="@shopify/oxygen-workers-types" />
 
 // Enhance TypeScript's built-in typings.
