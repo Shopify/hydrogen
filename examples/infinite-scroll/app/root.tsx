eba9ceea1a34e6bb79968d2713c340b03bf56c77
Juan P. Prieto <jp@calltheguys.co>
2025-09-12 11:43:51 -0700

    delete infinite-scroll

--- examples/infinite-scroll/app/root.tsx
+++
@@ -1,5 +0,0 @@
-// This stub file is required for React Router type generation
-// The actual root implementation is inherited from the skeleton template
-export default function App() {
-  return null;
-}
