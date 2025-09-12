4daf37ea291334b23bd543fdad5673ab7c9a6133
andrew-cottage <andrew.cottage@shopify.com>
2025-09-12 17:00:08 -0400

    update login function to support countryCode argument (#3148)

--- templates/skeleton/app/routes/account_.login.tsx
+++ templates/skeleton/app/routes/account_.login.tsx
@@ -1,5 +1,7 @@
 import type {LoaderFunctionArgs} from '@shopify/remix-oxygen';
 
 export async function loader({request, context}: LoaderFunctionArgs) {
-  return context.customerAccount.login();
+  return context.customerAccount.login({
+    countryCode: context.storefront.i18n.country,
+  });
 }
