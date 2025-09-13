7004f3e27184a89817b9a1a12734b25f0b82d0a5
Juan P. Prieto <jp@calltheguys.co>
2025-09-12 19:41:59 -0700

    delete multipass example

--- examples/multipass/app/routes/account.$.tsx
+++
@@ -1,9 +0,0 @@
-import {redirect} from 'react-router';
-import type {Route} from './+types/account.$';
-
-export async function loader({context}: Route.LoaderArgs) {
-  if (await context.session.get('customerAccessToken')) {
-    return redirect('/account');
-  }
-  return redirect('/account/login');
-}
