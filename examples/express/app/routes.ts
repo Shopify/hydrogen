ad0948b9c9d1f3e6bf43e952f256065729b585ee
Juan P. Prieto <jp@calltheguys.co>
2025-09-12 20:30:14 -0700

    delete final express example

--- examples/express/app/routes.ts
+++
@@ -1,8 +0,0 @@
-import {flatRoutes} from '@react-router/fs-routes';
-import {type RouteConfig} from '@react-router/dev/routes';
-
-export default (async () => {
-  const {hydrogenRoutes} = await import('@shopify/hydrogen');
-  const routes = await flatRoutes();
-  return hydrogenRoutes([...routes]);
-})() satisfies Promise<RouteConfig>;
