e610200b5c943eebda8ab95c153e32248387b9b2
Juan P. Prieto <jp@calltheguys.co>
2025-10-22 10:21:28 -0700

    [2025-10] Update Storefront API and Customer Account API
    Updated both APIs from version 2025-07 to 2025-10.
    
    Changes:
    - Updated version constants in all packages
    - Regenerated GraphQL types and schemas
    - Updated documentation and examples
    - Updated test fixtures
    - Created 11 tracking issues for API changes
    
    Related: #3281

--- packages/hydrogen-react/src/ShopPayButton2.example.jsx
+++ packages/hydrogen-react/src/ShopPayButton2.example.jsx
@@ -5,7 +5,7 @@ export default function App() {
     <ShopifyProvider
       storeDomain="my-store"
       storefrontToken="abc123"
-      storefrontApiVersion="2025-07"
+      storefrontApiVersion="2025-10"
       countryIsoCode="CA"
       languageIsoCode="EN"
     >
