cd653456fbd1e7e1ab1f6fecff04c89a74b6cad9
freddie <freddie.batista@shopify.com>
2026-01-08 12:42:04 +0000

    chore: bump SFAPI and CAAPI

--- packages/hydrogen/src/cart/queries/cartDeliveryAddressesUpdateDefault.doc.ts
+++ packages/hydrogen/src/cart/queries/cartDeliveryAddressesUpdateDefault.doc.ts
@@ -7,7 +7,7 @@ const data: ReferenceEntityTemplateSchema = {
   isVisualComponent: false,
   related: [],
   description:
-    'Creates a function that accepts an array of selectable delivery addresses [CartSelectableAddressUpdateInput](/docs/api/storefront/2025-07/input-objects/CartSelectableAddressUpdateInput) to update in a cart',
+    'Creates a function that accepts an array of selectable delivery addresses [CartSelectableAddressUpdateInput](/docs/api/storefront/2025-10/input-objects/CartSelectableAddressUpdateInput) to update in a cart',
   type: 'utility',
   defaultExample: {
     description: 'This is the default example',
