23f1ea22c435dc7cec93e819d75b3ee49103056b
Fran Dios <frankdiox@gmail.com>
2026-01-08 05:14:18 +0100

    Run e2e tests on CI (#3330)
    * Run e2e tests on CI
    
    * test: comment out checkout URl assertions until backend has updated
    
    ---------
    
    Co-authored-by: Kara Daviduik <kara.daviduik@shopify.com>

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,12 +1,20 @@
 import {defineConfig} from '@playwright/test';
 
+const isCI = !!process.env.CI;
+
 export default defineConfig({
   testMatch: /\.spec\.ts$/,
-  retries: 0,
-  reporter: 'list',
+  retries: isCI ? 1 : 0,
+  reporter: isCI ? 'html' : 'list',
   workers: 1,
   fullyParallel: true,
   timeout: 60 * 1000,
+  use: {
+    // Capture screenshot on failure
+    screenshot: 'only-on-failure',
+    // Record trace on first retry (helps debug flaky tests)
+    trace: 'on-first-retry',
+  },
   projects: [
     {
       name: 'smoke',
