e642d7c5cdaf1b512a5eef02ab9ea45922a586de
Hao LI <lihaokx@gmail.com>
2026-01-13 21:08:26 -0500

    Add acrValues and loginHint to customerAccount.login() (#3363)

--- templates/skeleton/app/routes/account_.login.tsx
+++ templates/skeleton/app/routes/account_.login.tsx
@@ -1,7 +1,13 @@
 import type {Route} from './+types/account_.login';
 
 export async function loader({request, context}: Route.LoaderArgs) {
+  const url = new URL(request.url);
+  const acrValues = url.searchParams.get('acr_values') || undefined;
+  const loginHint = url.searchParams.get('login_hint') || undefined;
+
   return context.customerAccount.login({
     countryCode: context.storefront.i18n.country,
+    acrValues,
+    loginHint,
   });
 }
