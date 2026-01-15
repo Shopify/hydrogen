a733d62bbec7596c9f4ef3d9265cef2f88d7af3a
Hao Li <lihaokx@gmail.com>
2026-01-15 12:33:35 -0500

    Add locale and loginHintMode parameter to Customer Account login URL

--- templates/skeleton/app/routes/account_.login.tsx
+++ templates/skeleton/app/routes/account_.login.tsx
@@ -4,10 +4,14 @@ export async function loader({request, context}: Route.LoaderArgs) {
   const url = new URL(request.url);
   const acrValues = url.searchParams.get('acr_values') || undefined;
   const loginHint = url.searchParams.get('login_hint') || undefined;
+  const loginHintMode = url.searchParams.get('login_hint_mode') || undefined;
+  const locale = url.searchParams.get('locale') || undefined;
 
   return context.customerAccount.login({
     countryCode: context.storefront.i18n.country,
     acrValues,
     loginHint,
+    loginHintMode,
+    locale,
   });
 }
