4ae97b4b47c5676acb0e88fd8b4fcf5f4a465386
freddie <freddie.batista@shopify.com>
2026-02-06 13:03:24 +0000

    style: notice

--- templates/skeleton/app/components/MockShopNotice.tsx
+++ templates/skeleton/app/components/MockShopNotice.tsx
@@ -4,29 +4,31 @@ export function MockShopNotice() {
       className="mock-shop-notice"
       aria-labelledby="mock-shop-notice-heading"
     >
-      <p id="mock-shop-notice-heading">
-        Welcome to hydrogen! This is a mock shop.
-      </p>
-      <p>
-        You&rsquo;re seeing mocked products because no store is connected to
-        this project yet.
-      </p>
-      <p>
-        Link a store by running <code>npx shopify hydrogen link</code> in your
-        project.
-      </p>
-      <p>
-        Need help? Learn more about{' '}
-        <a
-          className="link"
-          target="_blank"
-          rel="noreferrer noopener"
-          href="https://shopify.dev/docs/custom-storefronts/hydrogen/environment-variables"
-        >
-          editing environment variables
-        </a>
-        .
-      </p>
+      <div className="inner">
+        <h2 id="mock-shop-notice-heading">Welcome to Hydrogen!</h2>
+        <p>
+          You&rsquo;re seeing mocked products because no store is connected to
+          this project yet.
+        </p>
+        <p>
+          Link a store by running <code>npx shopify hydrogen link</code> in your
+          terminal.
+        </p>
+      </div>
+      <footer>
+        <p className="small">
+          Learn more about{' '}
+          <a
+            className="link"
+            target="_blank"
+            rel="noreferrer noopener"
+            href="https://shopify.dev/docs/custom-storefronts/hydrogen/environment-variables"
+          >
+            editing environment variables
+          </a>
+          .
+        </p>
+      </footer>
     </section>
   );
 }
