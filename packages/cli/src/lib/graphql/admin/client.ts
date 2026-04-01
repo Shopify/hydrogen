c4aa7e327a3e65fe1ff9bba7cfbb5a15a92b38e8
Lucy <sam.lucy.xiang@gmail.com>
2026-04-01 17:29:05 -0400

    Don't send user hydrogenStorefronts access denial error to observe (#3654)
    * Add error handling for hydrogenStorefronts access denial
    
    * Update twenty-rice-give.md

--- packages/cli/src/lib/graphql/admin/client.ts
+++ packages/cli/src/lib/graphql/admin/client.ts
@@ -23,6 +23,54 @@ interface GraphqlError {
  * @param variables - GraphQL variables to pass to the query.
  * @returns The response of the query of generic type <T>.
  */
+const KNOWN_USER_ERRORS: {
+  pattern: string;
+  abort: ConstructorParameters<typeof AbortError>;
+}[] = [
+  {
+    pattern: 'app is not installed',
+    abort: [
+      "Hydrogen sales channel isn't installed",
+      'Install the Hydrogen sales channel on your store to start creating and linking Hydrogen storefronts: https://apps.shopify.com/hydrogen',
+    ],
+  },
+  {
+    pattern: 'Access denied for hydrogenStorefrontCreate field',
+    abort: [
+      "Couldn't connect storefront to Shopify",
+      [
+        'Common reasons for this error include:',
+        {
+          list: {
+            items: [
+              "The Hydrogen sales channel isn't installed on the store.",
+              "You don't have the required account permission to manage apps or channels.",
+              "You're trying to connect to an ineligible store type (Trial, Development store)",
+            ],
+          },
+        },
+      ],
+    ],
+  },
+  {
+    pattern: 'Access denied for hydrogenStorefronts field',
+    abort: [
+      "Couldn't access Hydrogen storefronts",
+      [
+        'Common reasons for this error include:',
+        {
+          list: {
+            items: [
+              "You don't have full access to apps or access to the Hydrogen channel.",
+              "The Hydrogen sales channel isn't installed on the store.",
+            ],
+          },
+        },
+      ],
+    ],
+  },
+];
+
 export async function adminRequest<T>(
   query: string,
   session: AdminSession,
@@ -42,34 +90,10 @@ export async function adminRequest<T>(
   } catch (error: any) {
     const errors: GraphqlError[] = error.errors;
 
-    if (
-      errors?.some?.((error) => error.message.includes('app is not installed'))
-    ) {
-      throw new AbortError(
-        "Hydrogen sales channel isn't installed",
-        'Install the Hydrogen sales channel on your store to start creating and linking Hydrogen storefronts: https://apps.shopify.com/hydrogen',
-      );
-    }
-
-    if (
-      errors?.some?.((error) =>
-        error.message.includes(
-          'Access denied for hydrogenStorefrontCreate field',
-        ),
-      )
-    ) {
-      throw new AbortError("Couldn't connect storefront to Shopify", [
-        'Common reasons for this error include:',
-        {
-          list: {
-            items: [
-              "The Hydrogen sales channel isn't installed on the store.",
-              "You don't have the required account permission to manage apps or channels.",
-              "You're trying to connect to an ineligible store type (Trial, Development store)",
-            ],
-          },
-        },
-      ]);
+    for (const {pattern, abort} of KNOWN_USER_ERRORS) {
+      if (errors?.some?.((e) => e.message.includes(pattern))) {
+        throw new AbortError(...abort);
+      }
     }
 
     throw error;
