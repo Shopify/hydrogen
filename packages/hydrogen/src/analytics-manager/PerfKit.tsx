3ddc3aa6440b68ed838c48a484a811e836d899c3
✦ freddie <45042736+fredericoo@users.noreply.github.com>
2026-06-15 16:40:07 +0100

    fix: sampling to 10

--- packages/hydrogen/src/analytics-manager/PerfKit.tsx
+++ packages/hydrogen/src/analytics-manager/PerfKit.tsx
@@ -32,7 +32,7 @@ export function PerfKit({shop}: {shop: ShopAnalytics}) {
       'data-storefront-id': shop.hydrogenSubchannelId,
       'data-monorail-region': 'global',
       'data-spa-mode': 'true',
-      'data-resource-timing-sampling-rate': '100',
+      'data-resource-timing-sampling-rate': '10',
     }),
     [shop.shopId, shop.hydrogenSubchannelId],
   );
